0105e5604ac86eec206df5a9f8657cf33359ffcc
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-03-02 09:29:46 +0300

    Renamed PO-before/after methods and added some logic

--- src/org/bandahealth/idempiere/base/modelevent/ProductModelEvent.java
+++ src/org/bandahealth/idempiere/base/modelevent/ProductModelEvent.java
@@ -1,9 +1,11 @@
 package org.bandahealth.idempiere.base.modelevent;
 
+import java.math.BigDecimal;
 import java.util.logging.Level;
 
 import org.adempiere.base.event.AbstractEventHandler;
 import org.adempiere.base.event.IEventTopics;
+import org.compiere.model.I_M_Product;
 import org.compiere.model.MPriceList;
 import org.compiere.model.MPriceListVersion;
 import org.compiere.model.MProduct;
@@ -18,14 +20,13 @@ public class ProductModelEvent extends AbstractEventHandler {
 	
 	private CLogger logger = CLogger.getCLogger(ProductModelEvent.class);
 	private MProduct product;
-	private MPriceList priceList;
 	private PO persistentObject;
 	private int clientId;
 	private int orgId;
 	
 	@Override
 	protected void doHandleEvent(Event event) {
-		logger.log(Level.ALL, "Event trigered");
+		System.out.println("Event trigered: " + event.getTopic());
 		persistentObject = getPO(event);
 		if(persistentObject instanceof MProduct) {
 			product = (MProduct)persistentObject;
@@ -37,10 +38,12 @@ public class ProductModelEvent extends AbstractEventHandler {
 		
 		if(event.getTopic().equals(IEventTopics.PO_BEFORE_NEW)) {
 			//stuff to do before save
-			setBeforeHeaderSaveDefaults();
+			System.out.println("Executing beforeSaveRequest()");
+			beforeSaveRequest();
 		}else if(event.getTopic().equals(IEventTopics.PO_AFTER_NEW)){
 			//do this after save
-			setAfterHeaderSaveDefaults();
+			System.out.println("Executing afterSaveRequest()");
+			afterSaveRequest();
 		}
 		
 	}
@@ -49,12 +52,12 @@ public class ProductModelEvent extends AbstractEventHandler {
 	@Override
 	protected void initialize() {
 		logger.log(Level.ALL, "Initializing event listener");
-		registerTableEvent(IEventTopics.PO_BEFORE_NEW, MProduct.Table_Name);
-		registerTableEvent(IEventTopics.PO_AFTER_NEW, MProduct.Table_Name);
+		registerTableEvent(IEventTopics.PO_BEFORE_NEW, I_M_Product.Table_Name);
+		registerTableEvent(IEventTopics.PO_AFTER_NEW, I_M_Product.Table_Name);
 	}
 	
 	/*Set defaults to fields */
-	private void setBeforeHeaderSaveDefaults() {
+	private void beforeSaveRequest() {
 
 		logger.log(Level.INFO, "saving header defaults!");
 		//set required check-boxes
@@ -65,13 +68,43 @@ public class ProductModelEvent extends AbstractEventHandler {
 	}
 
 	
-	private void setAfterHeaderSaveDefaults() {
-		//set the sales and purchase price-lists for this product
-		Query query = new Query(Env.getCtx(),MPriceList.Table_Name,"isactive='Y' and isdefault='Y'",null);
-		priceList = query.first();
-		System.out.println("Query: " + query.getSQL());
-		MPriceListVersion priceListVersion = new MPriceListVersion(priceList);
-//		MProductPrice prodPrice =  new MProductPrice(priceListVersion,product.get_ID(),priceList(),new PriceList)
+	private void afterSaveRequest() {
+		
+		//setting the sales pricing for the product
+		Query query = null;
+		MPriceList priceList = null;
+		MPriceListVersion plVersion = null;
+		
+		//get existing sales price-list
+		query = new Query(Env.getCtx(),MPriceList.Table_Name,"isactive='Y' and isdefault='Y'",null); 
+		if (query.count() > 0) {
+			priceList = query.first();
+			System.out.println(priceList.toString());
+		}
+		
+		// get a version of the price-list set as the default
+		query = new Query(Env.getCtx(),MPriceListVersion.Table_Name,"m_pricelist_id="+priceList.get_ID(),null);
+		if(query.count() > 0) {
+			plVersion = query.first();
+			System.out.println(plVersion.toString());
+		}
+		
+		//create a product price and set default prices (list,standard and limit prices)
+		MProductPrice prodPrice =  
+				new MProductPrice(Env.getCtx(),
+						plVersion.get_ID(), 
+						product.get_ID(), 
+						new BigDecimal(100), 
+						new BigDecimal(100), 
+						new BigDecimal(100), 
+						null);
+		prodPrice.setM_PriceList_Version_ID(plVersion.get_ID());
+		plVersion.setM_PriceList_ID(priceList.get_ID());
+		prodPrice.save();
+//		product
+		System.out.println(prodPrice.toString());
+		
+		//add the product price to the product
 		
 	}
 
