d2cda20138b442d8a5133589cf91d146b3e260cf
Andrew <andrewndungu@gmail.com>
2018-03-07 11:40:42 +0300

    Intentionally break build

--- src/org/bandahealth/idempiere/base/modelevent/PhysicalInventoryModelEvent.java
+++ src/org/bandahealth/idempiere/base/modelevent/PhysicalInventoryModelEvent.java
@@ -21,7 +21,7 @@ public class PhysicalInventoryModelEvent extends AbstractEventHandler{
 
 	@Override
 	protected void doHandleEvent(Event event) {
-		MInventory inventory = null;
+		MInvdentory inventory = null;
 		PO persistantObject = getPO(event);
 		if (persistantObject instanceof MInventory) {
 			inventory = (MInventory) persistantObject;
