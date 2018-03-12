9334ca06f42fdf77ea0392738bfca30353c89f10
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-03-12 23:07:23 +0300

    Moved toolbar button creation functionality into method

--- src/org/bandahealth/bhgo/unitydashboard/DPBandaGoPanel.java
+++ src/org/bandahealth/bhgo/unitydashboard/DPBandaGoPanel.java
@@ -32,89 +32,46 @@ public class DPBandaGoPanel extends DashboardPanel implements EventListener<Even
 	
 	private static final long serialVersionUID = 1L;
 	private CLogger logger = CLogger.getCLogger(DPBandaGoPanel.class);
+	private final String DEFAULT_TOOL_ICON = "Server24.png";
 
 	public DPBandaGoPanel() {
 		super();
-		System.out.println("Inside constructor for custom dashboard");
-		this.setHeight("300px");
 		this.appendChild(createPanel());
-		
 	}
 	
 	public Box createPanel() {
-		System.out.println("Inside createPanel()");
 		Vbox vBox = new Vbox();
-		MInfoWindow win = getProductInfoView();
+		MInfoWindow productInfoWindow = filterFromViews("Product Info");
+			if(productInfoWindow != null) {
+				ToolBarButton button = createPanelButton(productInfoWindow.getName(),
+						"link",
+						productInfoWindow.get_Translation("Name"),
+						productInfoWindow.getImageURL());
+				button.addEventListener(Events.ON_CLICK, this);
+				vBox.appendChild(button);
+				}
+			//add links to BH custom windows
+			List<MWindow> bhCustomWindows = getBHCustomWindows();
+			for (MWindow mWindow : bhCustomWindows) {
+				ToolBarButton winBtn = createPanelButton(mWindow.getName(),
+						"link",
+						mWindow.getName(),
+						DEFAULT_TOOL_ICON);
+				winBtn.addEventListener(Events.ON_CLICK, this);
+				vBox.appendChild(winBtn);
+			}
 		
-		ToolBarButton btnViewItem = new ToolBarButton(win.getName());
-		btnViewItem.setSclass("link");
-		btnViewItem.setLabel(win.get_Translation("Name"));
-		btnViewItem.setImage(ThemeManager.getThemeResource("images/" + (Util.isEmpty(win.getImageURL()) ? "Server24.png" : "Server24.png")));
-		btnViewItem.addEventListener(Events.ON_CLICK, this);
-		vBox.appendChild(btnViewItem);
-		System.out.println("Added product info link to panel");
-		
-		List<MWindow> bhCustomWindows = getBHCustomWindows();
-		for (MWindow mWindow : bhCustomWindows) {
-			int count = 0;
-			ToolBarButton winBtn = new ToolBarButton(mWindow.getName());
-			winBtn.setSclass("link");
-			winBtn.setLabel(mWindow.get_Translation("Name"));
-			winBtn.setImage(ThemeManager.getThemeResource("images/" + (Util.isEmpty(win.getImageURL()) ? "Info16.png" :"Server24.png")));
-			winBtn.addEventListener(Events.ON_CLICK, this);
-			System.out.println("Window link "+count+"  added");
-			vBox.appendChild(winBtn);
-			count++;
-		}
 		return vBox;
 	}
 	
-	
-	
-	/*
-	 * Get listing of dashboard views and filter only 
-	 * the product info view
-	 */
-	private MInfoWindow getProductInfoView() {
-		
-		MInfoWindow  productInfoWin = null;
-		
-		List<MInfoWindow> list = new Query(Env.getCtx(), MInfoWindow.Table_Name, "IsValid='Y' AND IsShowInDashboard='Y'", null)
-				.setOnlyActiveRecords(true)
-				.setOrderBy(MInfoWindow.COLUMNNAME_SeqNo)
-				.list();
-		
-		for (MInfoWindow currentWindow : list) {
-			if (currentWindow.getName().contains("Product Info")) {
-				productInfoWin = currentWindow;
-			}
-		}
-		return productInfoWin;
-	}
-	
-	/* Get all custom BH windows 
-	 * Assumes every custom table needed is prefixed with BH
-	 * 
-	 */
-	private List<MWindow> getBHCustomWindows(){
-		List<MWindow> list = new Query(Env.getCtx(), MWindow.Table_Name, "name like 'BH_%'", null)
-				.setOnlyActiveRecords(true)
-				.list();
-		return list;
-	}
-	
-	
 	@Override
 	public void onEvent(Event event) throws Exception {
-		logger.info(event.toString());
 		Component component = event.getTarget();
 		String eventName = event.getName();
 		int displayId = -1;
 		if(eventName.equals(Events.ON_CLICK)) {
 			ToolBarButton button = (ToolBarButton) component;
 			String windowName = button.getName();
-			
-			System.out.println("Display ID = " + displayId);
 			if(windowName.equals("Product Info")) {
 				displayId = getWindowId(windowName,MInfoWindow.Table_Name);
 				SessionManager.getAppDesktop().openInfo(displayId);
@@ -126,13 +83,58 @@ public class DPBandaGoPanel extends DashboardPanel implements EventListener<Even
 		}
 	}
 	
-	/*Get the ID of the window to be displayed*/
-	private int getWindowId(String windowName, String windowType) {
-		int windowToDisplay = new Query(Env.getCtx(), windowType, "Name = ?", null)
-				.setParameters(windowName)
+	/*
+	 * Get listing of dashboard views and filter only 
+	 * the product info view
+	 */
+	private MInfoWindow filterFromViews(String viewName) {
+		MInfoWindow  productInfoWin = null;
+		List<MInfoWindow> list = new Query(
+				Env.getCtx(), 
+				MInfoWindow.Table_Name, 
+				"IsValid='Y' AND IsShowInDashboard='Y'",
+				null).setOnlyActiveRecords(true).list();
+		for (MInfoWindow currentWindow : list) {
+			if (currentWindow.getName().contains(viewName)) {
+				productInfoWin = currentWindow;
+			}
+		}
+		return productInfoWin;
+	}
+	
+	/* Get all custom BH windows 
+	 * Assumes every custom table needed is prefixed with BH
+	 */
+	private List<MWindow> getBHCustomWindows(){
+		List<MWindow> list = new Query(Env.getCtx(), 
+				MWindow.Table_Name, 
+				"name like 'BH_%'", 
+				null)
 				.setOnlyActiveRecords(true)
-				.firstIdOnly();
+				.list();
+		return list;
+	}
+	
+	/*
+	 * Get the ID of the window to be displayed
+	 */
+	private int getWindowId(String windowName, String windowType) {
+		int windowToDisplay = new Query(Env.getCtx(), 
+				windowType, 
+				"Name = ?",
+				null).setParameters(windowName).setOnlyActiveRecords(true).firstIdOnly();
 		return windowToDisplay;
+	}
+	
+	/*Create toolbar buttons for the panel*/
+	private ToolBarButton createPanelButton(String name, String styleClass, String label, String image) {
+		ToolBarButton itemLink = new ToolBarButton(name);
+		itemLink.setSclass(styleClass);
+		itemLink.setLabel(label);
+		itemLink.setImage(
+				ThemeManager.getThemeResource("images/" + 
+						(Util.isEmpty(image) ? image : DEFAULT_TOOL_ICON)));
+		return itemLink;
 	}
 
 }
