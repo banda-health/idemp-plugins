8c9ff52f39685eca60a81327a2ea13d0a583c647
Andrew <andrewndungu@gmail.com>
2018-03-14 15:33:44 +0300

    Receive goods model event

--- src/org/bandahealth/idempiere/base/modelevent/ReceiveGoodsModelEvent.java
+++ src/org/bandahealth/idempiere/base/modelevent/ReceiveGoodsModelEvent.java
@@ -1,19 +1,22 @@
 package org.bandahealth.idempiere.base.modelevent;
 
+import java.math.BigDecimal;
+
 import org.adempiere.base.event.AbstractEventHandler;
 import org.adempiere.base.event.IEventTopics;
 import org.compiere.model.MInOut;
+import org.compiere.model.MInOutLine;
 import org.compiere.model.MOrder;
+import org.compiere.model.MOrderLine;
+import org.compiere.model.MWarehouse;
 import org.compiere.model.PO;
+import org.compiere.util.Env;
 import org.osgi.service.event.Event;
 
 public class ReceiveGoodsModelEvent extends AbstractEventHandler {
 
 	@Override
 	protected void initialize() {
-		registerTableEvent(IEventTopics.PO_AFTER_NEW, MOrder.Table_Name);
-		registerTableEvent(IEventTopics.PO_AFTER_CHANGE, MOrder.Table_Name);
-		
 		registerEvent(IEventTopics.DOC_AFTER_COMPLETE);
 	}
 
@@ -23,6 +26,9 @@ public class ReceiveGoodsModelEvent extends AbstractEventHandler {
 		PO persistantObject = getPO(event);
 		if (persistantObject instanceof MOrder) {
 			order = (MOrder) persistantObject;
+			if (order.isSOTrx()) {
+				return;
+			}
 		} else {
 			return;
 		}
@@ -33,11 +39,29 @@ public class ReceiveGoodsModelEvent extends AbstractEventHandler {
 	}
 
 	private void createMaterialReceiptFromOrder(MOrder order) {
-		if (order.isSOTrx()) {
-			return;
+		// Create Material Receipt header
+		MInOut mReceipt = new MInOut(order, 0, order.getDateOrdered());
+		mReceipt.setMovementType(MInOut.MOVEMENTTYPE_VendorReceipts);
+		mReceipt.setDocAction(MInOut.DOCACTION_Complete);
+		mReceipt.save();
+
+		// add lines if any
+		MOrderLine[] oLines = order.getLines(true, "M_Product_ID");
+		if (oLines.length > 0) {
+			MWarehouse mWarehouse = new MWarehouse(Env.getCtx(), order.getM_Warehouse_ID(), order.get_TrxName());
+			for (MOrderLine oLine : oLines) {
+				MInOutLine line = new MInOutLine(mReceipt);
+				line.setOrderLine(oLine, mWarehouse.getDefaultLocator().get_ID(), Env.ZERO);
+				// need to find out why the available/reserved quantity is doubled under product info 
+				line.setQty(oLine.getQtyOrdered().divide(new BigDecimal(2)));
+				line.saveEx(order.get_TrxName());
+			}
 		}
 
-		MInOut mReceipt = MInOut.createFrom(order, order.getDateOrdered(), false, false, null, true, order.get_TrxName());
-		System.out.println("ID = " + mReceipt.get_ID());
+		// complete operation
+		mReceipt.completeIt();
+		String m_status = mReceipt.completeIt();
+		mReceipt.setDocStatus(m_status);
+		mReceipt.save();
 	}
 }
