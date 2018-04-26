c99a6b919966a150abe057389dda850fd8690054
Andrew <andrewndungu@gmail.com>
2018-04-26 08:54:26 +0300

    idemp-36 Create a Stock Take window that shows stocks with expirations, and ability to edit quantities.

--- src/org/bandahealth/idempiere/base/factory/BHProcessFactory.java
+++ src/org/bandahealth/idempiere/base/factory/BHProcessFactory.java
@@ -2,6 +2,7 @@ package org.bandahealth.idempiere.base.factory;
 
 import org.adempiere.base.IProcessFactory;
 import org.bandahealth.idempiere.base.process.SalesProcess;
+import org.bandahealth.idempiere.base.process.StockTakeProcess;
 import org.compiere.process.ProcessCall;
 import org.compiere.util.CLogger;
 
@@ -14,6 +15,8 @@ public class BHProcessFactory implements IProcessFactory {
 
 		if (className.equals(SalesProcess.class.getName())) {
 			return new SalesProcess();
+		} else if (className.equals(StockTakeProcess.class.getName())) {
+			return new StockTakeProcess();
 		}
 
 		return null;
