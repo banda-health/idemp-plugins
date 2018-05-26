f1bb88c935bebc2bc3a6b2cc6ef874fdba4ffff8
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-05-26 08:42:35 +0300

    Modify query to use mstorage in checking for product quanties

--- src/org/bandahealth/idempiere/base/callout/InventoryQuantity.java
+++ src/org/bandahealth/idempiere/base/callout/InventoryQuantity.java
@@ -1,13 +1,14 @@
 package org.bandahealth.idempiere.base.callout;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.adempiere.base.IColumnCallout;
 import org.bandahealth.idempiere.base.model.MOrderLine_BH;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
-import org.compiere.model.MInventoryLine;
+import org.compiere.model.MStorageOnHand;
 import org.compiere.model.Query;
 import org.compiere.util.CLogger;
 import org.compiere.util.Env;
@@ -15,19 +16,26 @@ import org.compiere.util.Env;
 public class InventoryQuantity implements IColumnCallout {
 
 	CLogger logger = CLogger.getCLogger(this.getClass());
-	
-	
+
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
 		String errorMessage = null;
-		Integer productId = (Integer) value;
-		
-		String whereClause = MOrderLine_BH.COLUMNNAME_M_Product_ID+"=?";
-		Query inventoryAvailabeForProduct = new Query(Env.getCtx(),MInventoryLine.Table_Name,
-				whereClause, null);
-		Integer quantityCount = inventoryAvailabeForProduct.setParameters(productId).first().get_ValueAsInt("qtycount");
-		mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyAvailable, BigDecimal.valueOf(quantityCount));
-		
+		if (value != null) {
+
+			Integer productId = (Integer) value;
+			Integer locatorId = Env.getContextAsInt(ctx, "M_Warehouse_ID");
+			String whereClause = MStorageOnHand.COLUMNNAME_M_Product_ID + "=? AND "
+					+ MStorageOnHand.COLUMNNAME_M_Locator_ID + "=?";
+			logger.info("Selected Warehouse: " + locatorId);
+			Query getAvailableQtyInStorage = new Query(Env.getCtx(), MStorageOnHand.Table_Name, whereClause, null);
+			logger.info(getAvailableQtyInStorage.setParameters(Arrays.asList(productId, locatorId)).getSQL());
+			int quantity = getAvailableQtyInStorage.setParameters(Arrays.asList(productId, locatorId)).first()
+					.get_ValueAsInt("qtyonhand");
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyAvailable, String.valueOf(quantity));
+		} else {
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyAvailable, BigDecimal.ZERO);
+		}
+
 		return errorMessage;
 	}
 
