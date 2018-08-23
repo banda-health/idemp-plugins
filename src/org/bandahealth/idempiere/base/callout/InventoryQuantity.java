c3560d6879cd81e79c20389063422b29f9dce389
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-08-23 16:01:28 +0300

    Update inventory quantity available when different expiration date for product selected  in a SO

--- src/org/bandahealth/idempiere/base/callout/InventoryQuantity.java
+++ src/org/bandahealth/idempiere/base/callout/InventoryQuantity.java
@@ -1,7 +1,9 @@
 package org.bandahealth.idempiere.base.callout;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.adempiere.base.IColumnCallout;
@@ -25,17 +27,23 @@ public class InventoryQuantity implements IColumnCallout {
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
 
 		String errorMessage = null;
+		Query queryQtyInStorage = null;
 		Integer warehouseId = Env.getContextAsInt(ctx, WindowNo + "|M_Warehouse_ID");
 		Integer orderId = Env.getContextAsInt(ctx, WindowNo + "|C_Order_ID");
-
+		Integer attributeSetId = Env.getContextAsInt(ctx, WindowNo + "|M_AttributeSetInstance_ID");
+		Integer productId = null;
+		
 		if (value != null) {
 
 			MOrder order = new MOrder(ctx, orderId, null);
 			if (!order.isSOTrx()) {
 				return errorMessage;
 			}
-
-			Integer productId = (Integer) value;
+			if(attributeSetId > 0) {
+				productId = Env.getContextAsInt(ctx, WindowNo + "|M_Product_ID");
+			}else {
+				productId = (Integer) value;
+			}
 
 			// ignore billed services
 			MProduct product = new MProduct(ctx, productId, null);
@@ -45,12 +53,19 @@ public class InventoryQuantity implements IColumnCallout {
 
 			MLocator locator = new MWarehouse(ctx, warehouseId, null).getDefaultLocator();
 			Integer locatorId = locator.get_ID();
-
 			String whereClause = MStorageOnHand.COLUMNNAME_M_Product_ID + "=? AND "
 					+ MStorageOnHand.COLUMNNAME_M_Locator_ID + "=?";
-			Query availableQtyInStorage = new Query(Env.getCtx(), MStorageOnHand.Table_Name, whereClause, null);
-			BigDecimal quantity = (BigDecimal) availableQtyInStorage.setParameters(Arrays.asList(productId, locatorId))
-					.first().get_Value("qtyonhand");
+			ArrayList<Object> parameters = new ArrayList<>();
+			parameters.add(productId);
+			parameters.add(locatorId);
+			if(attributeSetId > 0) {
+				logger.info("Inside check!");
+				whereClause += " AND " + MStorageOnHand.COLUMNNAME_M_AttributeSetInstance_ID+"=?";
+				parameters.add(attributeSetId);
+			}
+			queryQtyInStorage = new Query(Env.getCtx(), MStorageOnHand.Table_Name, whereClause, null);
+			queryQtyInStorage.setParameters(parameters);
+			BigDecimal quantity  = queryQtyInStorage.aggregate("qtyonhand", Query.AGGREGATE_SUM);
 			mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyAvailable, quantity);
 		} else {
 			mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyAvailable, BigDecimal.ZERO);
