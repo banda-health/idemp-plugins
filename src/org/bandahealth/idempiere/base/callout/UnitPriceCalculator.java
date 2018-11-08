1644ea9a699a91048b88b7fd09f4ff484a397873
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-11-08 14:35:22 +0300

    Refactor and format method

--- src/org/bandahealth/idempiere/base/callout/UnitPriceCalculator.java
+++ src/org/bandahealth/idempiere/base/callout/UnitPriceCalculator.java
@@ -5,37 +5,28 @@ import java.math.RoundingMode;
 import java.util.Properties;
 
 import org.adempiere.base.IColumnCallout;
+import org.bandahealth.idempiere.base.model.MOrderLine_BH;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
-import org.compiere.model.MField;
 import org.compiere.model.MOrderLine;
 import org.compiere.util.CLogger;
 
-public class UnitPriceCalculator implements IColumnCallout{
+public class UnitPriceCalculator implements IColumnCallout {
 
 	CLogger log = CLogger.getCLogger(ProductTypeCheck.class);
 	private String errorMessage = null;
-	
+
 	@Override
 	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
-		log.info("Inside callout");
-		//get the qty field q
-		GridField qtyField = mTab.getField(MOrderLine.COLUMNNAME_QtyEntered);
-		BigDecimal productQuantity = (BigDecimal) qtyField.getValue();
-		//get the product total field T
-		BigDecimal totalLineAmount = (BigDecimal)mField.getValue();
-		//check if any are null/zero
-		if(productQuantity == null || totalLineAmount == null ) {
-			errorMessage = "Fields are null";
+		BigDecimal productQuantity = (BigDecimal) mTab.getField(MOrderLine.COLUMNNAME_QtyEntered).getValue();
+		BigDecimal totalLineAmount = (BigDecimal) mField.getValue();
+		if (productQuantity == null || totalLineAmount == null) {
+			errorMessage = "product Qty or total line aamount is empty";
 			return errorMessage;
 		}
-		//calculate the unit price p by T/q
-		BigDecimal unitPrice = totalLineAmount.divide(productQuantity,2,RoundingMode.HALF_UP);
-		//add p to price field
-		GridField unitPriceField = mTab.getField(MOrderLine.COLUMNNAME_PriceEntered);
-		unitPriceField.setValue(unitPrice, true);
+		BigDecimal unitPrice = totalLineAmount.divide(productQuantity, 2, RoundingMode.HALF_UP);
+		mTab.setValue(MOrderLine_BH.COLUMNNAME_PriceEntered, unitPrice);
 		return errorMessage;
 	}
 
-	
 }
