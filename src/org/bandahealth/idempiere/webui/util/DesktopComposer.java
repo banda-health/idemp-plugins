d59524750c9a8d45fa1ded269aa58a76ffd27341
Kelly Muhindi <kmuhindi@bandahealth.org>
2018-12-22 13:38:15 +0300

    Remove some expirimental code

--- src/org/bandahealth/idempiere/webui/util/DesktopComposer.java
+++ src/org/bandahealth/idempiere/webui/util/DesktopComposer.java
@@ -1,21 +1,18 @@
 package org.bandahealth.idempiere.webui.util;
 
-import java.awt.Event;
-
-import org.adempiere.webui.component.Messagebox;
 import org.bandahealth.idempiere.webui.TermsOfAgreementService;
 import org.compiere.util.CLogger;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Borderlayout;
 import org.zkoss.zul.Center;
 import org.zkoss.zul.Html;
 import org.zkoss.zul.West;
+import org.zkoss.zul.Window;
 
-public class DesktopComposer extends SelectorComposer<Borderlayout> {
+public class DesktopComposer extends SelectorComposer<Borderlayout>{
 
 	private CLogger logger = CLogger.getCLogger(this.getClass());
 	private static final long serialVersionUID = 1L;
@@ -25,15 +22,18 @@ public class DesktopComposer extends SelectorComposer<Borderlayout> {
 
 	@Wire("#windowArea")
 	private Center windowArea;
-	
+
+	@Wire("#tosWindow")
+	private Window window;
+
 	private TermsOfAgreementService termsOfAgreementService = new TermsOfAgreementService();
 
 	public void doAfterCompose(Borderlayout layout) {
 		try {
 			super.doAfterCompose(layout);
-			if(termsOfAgreementService.isAccepted())
+			if (termsOfAgreementService.isAccepted())
 				return;
-			Component termsOfServiceComponent = Executions.createComponents("zul/TermsOfService.zul",null,null);
+			Component termsOfServiceComponent = Executions.createComponents("zul/TermsOfService.zul", null, null);
 			Html termsOfServiceText = new Html(termsOfAgreementService.getTermsAndConditionsContent());
 			termsOfServiceComponent.getFellow("tosText", true).appendChild(termsOfServiceText);
 			windowArea.appendChild(termsOfServiceComponent);
--- src/org/bandahealth/idempiere/webui/util/TermsOfServiceComposer.java
+++ src/org/bandahealth/idempiere/webui/util/TermsOfServiceComposer.java
@@ -1,22 +1,25 @@
 package org.bandahealth.idempiere.webui.util;
 
-
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.adempiere.webui.AdempiereWebUI;
 import org.adempiere.webui.component.Messagebox;
 import org.adempiere.webui.theme.ThemeManager;
 import org.bandahealth.idempiere.webui.TermsOfAgreementService;
 import org.compiere.util.CLogger;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.Execution;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.metainfo.PageDefinition;
+import org.zkoss.zk.ui.metainfo.PageDefinitions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Window;
 
 public class TermsOfServiceComposer extends SelectorComposer<Window> {
@@ -28,30 +31,25 @@ public class TermsOfServiceComposer extends SelectorComposer<Window> {
 
 	@Wire("#tosWindow")
 	private Window window;
-	
+
 	private CLogger logger = CLogger.getCLogger(this.getClass());
 	private TermsOfAgreementService termsOfAgreementService = new TermsOfAgreementService();
-	
+
 	public void doAfterCompose(Window window) {
 		try {
 			super.doAfterCompose(window);
-			logger.info("doAfterCompose on " + String.valueOf(window.getId()) +"-> " + getClass().getName());
-		}catch (Exception e) {
+			logger.info("doAfterCompose on " + String.valueOf(window.getId()) + "-> " + getClass().getName());
+		} catch (Exception e) {
 		}
 	}
-	
+
 	@Listen("onClick = button#acceptBtn")
 	public void acceptTermsOfService(Event event) {
 		logger.info("Accepted terms!...");
-		termsOfAgreementService.acceptTermsOfUse(window);
-		String url = ThemeManager.getThemeResource("zul/desktop/desktop.zul");
-		Executions.getCurrent().sendRedirect(url);
 	}
-	
+
 	@Listen("onClick = button#rejectBtn")
 	public void rejectTermsOfService(Event event) {
 		logger.info("Rejecting terms!...");
-		String url = ThemeManager.getThemeResource("zul/login/login.zul");
-			Executions.getCurrent().sendRedirect(url);
 	}
 }
