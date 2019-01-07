3d9a88e73aba088dc759021e4d1720b755fd5f77
Andrew <andrewndungu@gmail.com>
2019-01-07 16:20:55 +0300

    idemp-328 Receive goods page to reflect paid X for Y# of a supply

--- src/org/bandahealth/idempiere/base/callout/UnitPriceCalculator.java
+++ src/org/bandahealth/idempiere/base/callout/UnitPriceCalculator.java
@@ -4,34 +4,58 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Properties;
 
-import org.adempiere.base.IColumnCallout;
 import org.bandahealth.idempiere.base.model.MOrderLine_BH;
+import org.compiere.model.CalloutEngine;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
-import org.compiere.model.MOrderLine;
-import org.compiere.util.CLogger;
+import org.compiere.util.Env;
 
-public class UnitPriceCalculator implements IColumnCallout {
+public class UnitPriceCalculator extends CalloutEngine {
 
-	CLogger log = CLogger.getCLogger(ProductTypeCheck.class);
 	private String errorMessage = null;
-	BigDecimal productQuantity, totalLineAmount, persistedUnitPrice, calculatedUnitPrice;
+	private BigDecimal productQuantity, totalLineAmount, unitPrice;
 
-	@Override
-	public String start(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
-		if (value != null) {
-			productQuantity = (BigDecimal) mTab.getField(MOrderLine.COLUMNNAME_QtyEntered).getValue();
-			totalLineAmount = (BigDecimal) mField.getValue();
-			persistedUnitPrice = (BigDecimal)mTab.getField(MOrderLine.COLUMNNAME_PriceActual).getValue();
-			calculatedUnitPrice = totalLineAmount.divide(productQuantity, 2, RoundingMode.HALF_UP);
-			
-			//since lineTotal is calculated from table values (priceActual * Qty)...
-			//set the calculated unit price as actual price
-			if (persistedUnitPrice != calculatedUnitPrice) {
-				mTab.setValue(MOrderLine.COLUMNNAME_PriceActual, calculatedUnitPrice);
-			}
-			mTab.setValue(MOrderLine_BH.COLUMNNAME_PriceEntered, calculatedUnitPrice);
+	public String validate(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value, Object oldValue) {
+		// don't touch SO line items.
+		String isSoTrx = Env.getContext(ctx, WindowNo, 0, "IsSOTrx");
+		if (isSoTrx != null && isSoTrx.equalsIgnoreCase("Y")) {
+			return errorMessage;
 		}
+		
+		if (value != null) {
+			if (mField.getColumnName().equals(MOrderLine_BH.COLUMNNAME_PriceEntered)){
+				unitPrice = (BigDecimal) mField.getValue();
+				mTab.setValue(MOrderLine_BH.COLUMNNAME_PriceActual, unitPrice);
+				
+				productQuantity = (BigDecimal) mTab.getField(MOrderLine_BH.COLUMNNAME_QtyEntered).getValue();
+			} else if (mField.getColumnName().equals(MOrderLine_BH.COLUMNNAME_QtyEntered)){
+				productQuantity = (BigDecimal) mField.getValue();
+				
+				unitPrice = (BigDecimal)mTab.getField(MOrderLine_BH.COLUMNNAME_PriceActual).getValue();
+				//totalLineAmount = (BigDecimal) mTab.getField(MOrderLine_BH.COLUMNNAME_LineNetAmt).getValue();
+			} else {
+				totalLineAmount = (BigDecimal) mField.getValue();
+				
+				productQuantity = (BigDecimal) mTab.getField(MOrderLine_BH.COLUMNNAME_QtyEntered).getValue();
+			}
+			
+			if (unitPrice == null) {
+				unitPrice = totalLineAmount.divide(productQuantity, 2, RoundingMode.CEILING);
+			}
+			
+			if (totalLineAmount == null) {
+				totalLineAmount = productQuantity.multiply(unitPrice);
+			}
+			
+			// save qty, unitprice, totallineamount
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_LineNetAmt, totalLineAmount);
+			
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_QtyEntered, productQuantity);
+			
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_PriceEntered, unitPrice);
+			mTab.setValue(MOrderLine_BH.COLUMNNAME_PriceActual, unitPrice);
+		}
+
 		return errorMessage;
 	}
 
