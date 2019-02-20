894a18f912126cb0fed7e3e97199c55b651868b7
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-02-20 09:24:03 +0300

    Fix bug in hasAllRolesAssigned() method

--- src/org/bandahealth/idempiere/webui/DashboardMenu.java
+++ src/org/bandahealth/idempiere/webui/DashboardMenu.java
@@ -68,6 +68,7 @@ public class DashboardMenu extends DashboardPanel implements EventListener<Event
 	private Integer unclosedSOCount = 0;
 	private static Integer MAX_RESULTS_SIZE = 20;
 	private boolean userHasAllRoles = false;
+	private final static String ALL_SUBROLES_INCLUDED = "allRoles"; 
 	
 	int userId = Env.getContextAsInt(Env.getCtx(), "#AD_User_ID");
 	int roleId = Env.getContextAsInt(Env.getCtx(), "#AD_Role_ID");
@@ -126,15 +127,15 @@ public class DashboardMenu extends DashboardPanel implements EventListener<Event
 	}
 	
 	private boolean hasAllRolesAssigned() {
-		List<MRoleIncluded> subRolesIncludedInRole = new Query(Env.getCtx(), MRoleIncluded.Table_Name, 
-				MUserRoles.Table_Name+"."+MUserRoles.COLUMNNAME_AD_Role_ID+"="+ roleId+" AND " + MUserRoles.COLUMNNAME_AD_User_ID+"="+userId, null)
-				.addJoinClause("JOIN " + MRole.Table_Name+" ON " +MRoleIncluded.Table_Name+"." + MRoleIncluded.COLUMNNAME_AD_Role_ID 
-						+ "=" + MRole.Table_Name + "." + MRole.COLUMNNAME_AD_Role_ID)
-				.addJoinClause("JOIN " + MUserRoles.Table_Name+" ON " +MRoleIncluded.Table_Name+"."+MRoleIncluded.COLUMNNAME_AD_Role_ID
-						+ "="+MUserRoles.Table_Name+"."+MUserRoles.COLUMNNAME_AD_Role_ID).list();
 		
-		for (MRoleIncluded subRole : subRolesIncludedInRole) {
-			if(subRole.get_ValueAsString(MRoleIncluded.COLUMNNAME_AD_Role_Included_UU).equals("de4c10f1-ba56-47eb-b274-365c983bff97")) {
+		
+		List<MRole> subRolesIncludedInRole = new Query(Env.getCtx(), MRole.Table_Name, 
+				MRoleIncluded.Table_Name+"."+MRoleIncluded.COLUMNNAME_AD_Role_ID+"="+ roleId, null)
+				.addJoinClause("JOIN " + MRoleIncluded.Table_Name+" ON " +MRole.Table_Name+"." + MRole.COLUMNNAME_AD_Role_ID 
+						+ "=" + MRoleIncluded.Table_Name + "." + MRoleIncluded.COLUMNNAME_Included_Role_ID).list();
+		
+		for (MRole subRole : subRolesIncludedInRole) {
+			if(subRole.get_ValueAsString(MRole.COLUMNNAME_Name).equals(ALL_SUBROLES_INCLUDED)) {
 				userHasAllRoles = true;
 			} 
 		}
