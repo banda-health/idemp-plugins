a353abfe64d536e9e21285f048d5e27a6fb62e9c
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-04-15 18:21:57 +0300

    Resolve Mockito issues and add tests for menu buttons using mock objects

--- src/main/java/org/bandahealth/idempiere/webui/util/dashboardmenu/DashboardMenuButtonCreation.java
+++ src/main/java/org/bandahealth/idempiere/webui/util/dashboardmenu/DashboardMenuButtonCreation.java
@@ -13,72 +13,74 @@ import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
 
 public class DashboardMenuButtonCreation implements EventListener<Event> {
-	
+
 	public static String INFO_WINDOW_ATTRIBUTE = "data-infowindow";
 	public static String SPECIAL_FORM_ATTRIBUTE = "special-form";
 	public static String REPORT_OR_PROCESS_ATTRIBUTE = "data-process";
 	public static String TERMS_OF_USE_ATTRIBUTE = "data-terms-of-use";
 
+	boolean isInfoWindow = false;
+	boolean isReportOrProcess = false;
+	boolean isSpecialForm = false;
+	int windowOrInfoWindowId;
 
-	public Grid createButton(MHomeScreenButton mHomeScreenButton) {
-		
+	public void setButtonType(MHomeScreenButton mHomeScreenButton) {
 		int windowOrInfoWindowId = mHomeScreenButton.getAD_Window_ID();
 
-		boolean isInfoWindow = false;
+		isInfoWindow = false;
 		if (windowOrInfoWindowId == 0) {
 			windowOrInfoWindowId = mHomeScreenButton.getAD_InfoWindow_ID();
 			isInfoWindow = true;
 		}
-		boolean isReportOrProcess = false;
+		isReportOrProcess = false;
 		if (windowOrInfoWindowId == 0) {
 			windowOrInfoWindowId = mHomeScreenButton.getAD_Process_ID();
 			isReportOrProcess = true;
 		}
-		boolean isSpecialForm = false;
+		isSpecialForm = false;
 		if (windowOrInfoWindowId == 0) {
 			windowOrInfoWindowId = mHomeScreenButton.getAD_Form_ID();
 			isSpecialForm = true;
 		}
-		
-		Grid grid = (Grid) Executions.createComponents("/zul/MenuButton.zul", null, null);
-		//set icon and text
-		Div div = (Div) grid.query(".icn");
-		Div div2 = (Div) grid.query(".lbl");
-		Label label = (Label) grid.query("label");
-		div.setSclass(mHomeScreenButton.getIconClassName() + " i");
-		label.setValue(mHomeScreenButton.getButtonText());
-		div2.appendChild(label);
-		//set attributes and id
-		grid.setId(Integer.toString(windowOrInfoWindowId));
-		grid.setAttribute(INFO_WINDOW_ATTRIBUTE, isInfoWindow);
-		grid.setAttribute(REPORT_OR_PROCESS_ATTRIBUTE, isReportOrProcess);
-		grid.setAttribute(SPECIAL_FORM_ATTRIBUTE, isSpecialForm);
-		grid.setTooltiptext(mHomeScreenButton.getButtonHelpText());
-		//set events on button
-		grid.addEventListener(Events.ON_CLICK, this);
-		return grid;
 	}
-	
+
+	public Grid createButton(MHomeScreenButton mHomeScreenButton) {
+		setButtonType(mHomeScreenButton);
+		Grid buttonGrid = (Grid) Executions.createComponents("/zul/MenuButton.zul", null, null);
+		Div menuIcon = (Div) buttonGrid.query(".icn");
+		Div menuText = (Div) buttonGrid.query(".lbl");
+		Label string = (Label) buttonGrid.query("label");
+		menuIcon.setSclass(mHomeScreenButton.getIconClassName() + " i");
+		string.setValue(mHomeScreenButton.getButtonText());
+		menuText.appendChild(string);
+		buttonGrid.setId(Integer.toString(windowOrInfoWindowId));
+		buttonGrid.setAttribute(INFO_WINDOW_ATTRIBUTE, isInfoWindow);
+		buttonGrid.setAttribute(REPORT_OR_PROCESS_ATTRIBUTE, isReportOrProcess);
+		buttonGrid.setAttribute(SPECIAL_FORM_ATTRIBUTE, isSpecialForm);
+		buttonGrid.setTooltiptext(mHomeScreenButton.getButtonHelpText());
+		buttonGrid.addEventListener(Events.ON_CLICK, this);
+		return buttonGrid;
+	}
+
 	@Override
 	public void onEvent(Event event) throws Exception {
 		Component component = event.getTarget();
 		String eventName = event.getName();
-		System.out.println("Event is: " + event.getName());
 		if (eventName.equals(Events.ON_CLICK)) {
 			if (component instanceof Grid) {
 				Grid button = (Grid) component;
-				Boolean termsOfUse = (Boolean) button.getAttribute(UIUtil.TERMS_OF_USE_ATTRIBUTE);
-				if ((Boolean) button.getAttribute(UIUtil.REPORT_OR_PROCESS_ATTRIBUTE)) {
+				Boolean termsOfUse = (Boolean) button.getAttribute(TERMS_OF_USE_ATTRIBUTE);
+				if ((Boolean) button.getAttribute(REPORT_OR_PROCESS_ATTRIBUTE)) {
 					int processId = Integer.parseInt(button.getId());
 					try {
 						SessionManager.getAppDesktop().openForm(processId);
 					} catch (Exception ex) {
 						SessionManager.getAppDesktop().openProcessDialog(processId, false);
 					}
-				} else if ((Boolean) button.getAttribute(UIUtil.INFO_WINDOW_ATTRIBUTE)) {
+				} else if ((Boolean) button.getAttribute(INFO_WINDOW_ATTRIBUTE)) {
 					int infoWindowId = Integer.parseInt(button.getId());
 					SessionManager.getAppDesktop().openInfo(infoWindowId);
-				} else if ((Boolean) button.getAttribute(UIUtil.SPECIAL_FORM_ATTRIBUTE)) {
+				} else if ((Boolean) button.getAttribute(SPECIAL_FORM_ATTRIBUTE)) {
 					int infoWindowId = Integer.parseInt(button.getId());
 					SessionManager.getAppDesktop().openForm(infoWindowId);
 				} else if (termsOfUse != null && termsOfUse == true) {
@@ -90,6 +92,7 @@ public class DashboardMenuButtonCreation implements EventListener<Event> {
 					SessionManager.getAppDesktop().openWindow(windowId, null);
 				}
 			}
+			//TODO Add event handler for open bills widget
 		}
 	}
 }
--- src/test/java/org/bandahealth/idempiere/webui/util/dashboardmenu/DashboardMenuCreationTest.java
+++ src/test/java/org/bandahealth/idempiere/webui/util/dashboardmenu/DashboardMenuCreationTest.java
@@ -1,8 +1,43 @@
 package org.bandahealth.idempiere.webui.util.dashboardmenu;
 
-public class DashboardMenuCreationTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-	public void testCreateButtonReturnsZKGrid() {
-		
+import org.bandahealth.idempiere.base.model.MHomeScreenButton;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import test.AdempiereTestCase;
+
+public class DashboardMenuCreationTest extends AdempiereTestCase{
+
+	MHomeScreenButton button;
+	DashboardMenuButtonCreation menuCreator;
+
+	boolean isInfoWindow = false;
+	boolean isReportOrProcess = false;
+	boolean isSpecialForm = false;
+	int windowOrInfoWindowId;
+
+	@Before
+	public void setUp() throws Exception {
+		button = mock(MHomeScreenButton.class);
+		menuCreator = new DashboardMenuButtonCreation();
+	}
+
+	@Test
+	public void testButtonTypeSetsIsProcessAttributeToTrue() {
+		when(button.getAD_Window_ID()).thenReturn(0);
+		when(button.getAD_Process_ID()).thenReturn(1000006); //revenue report process
+		menuCreator.setButtonType(button);
+		assertThat(isReportOrProcess, is(true));
+	}
+	
+	@Test
+	public void testButtonTypeSetsIsFormAttributeToTrue() {
+		when(button.getAD_Window_ID()).thenReturn(200000);// view products info window id
+		menuCreator.setButtonType(button);
+		assertThat(isInfoWindow, is(true));
 	}
 }
