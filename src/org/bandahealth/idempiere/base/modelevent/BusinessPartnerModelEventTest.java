6268b9ae5982d885f244a0cde6e4038899dc4c81
Andrew <andrewndungu@gmail.com>
2019-04-15 11:58:38 +0300

    Add a test case

--- src/org/bandahealth/idempiere/base/modelevent/BusinessPartnerModelEventTest.java
+++ src/org/bandahealth/idempiere/base/modelevent/BusinessPartnerModelEventTest.java
@@ -1,7 +1,9 @@
 package org.bandahealth.idempiere.base.modelevent;
 
 import org.bandahealth.idempiere.base.model.MBPartner_BH;
+import org.compiere.model.MBPartnerLocation;
 import org.compiere.model.MOrder;
+import org.compiere.model.MUser;
 import org.junit.Test;
 
 import test.AdempiereTestCase;
@@ -13,37 +15,46 @@ public class BusinessPartnerModelEventTest extends AdempiereTestCase {
 		super.setUp();
 
 		assertNotNull("Context should not be null", getCtx());
+
 	}
 
 	@Test
 	public void testCreateNewPatient() {
-		MBPartner_BH  bPartner = new MBPartner_BH(getCtx(), 0, getTrxName());
+		MBPartner_BH bPartner = new MBPartner_BH(getCtx(), 0, getTrxName());
 		bPartner.setName("Joe Blow");
 		bPartner.setBH_PatientID("000001");
 		bPartner.setBH_IsPatient(true);
 
-		boolean successful = bPartner.save();
-		assertEquals("Successfully created patient: ", true, successful);
+		bPartner.saveEx();
 
-		MBPartner_BH savedBPartner = (MBPartner_BH) MBPartner_BH.get(getCtx(), bPartner.get_ID());
+		MBPartner_BH savedBPartner = (MBPartner_BH) MBPartner_BH.get(getCtx(), bPartner.get_ID(), getTrxName());
 		assertEquals("Is Patient? ", true, savedBPartner.isCustomer());
 		assertEquals("Should have an Invoice Rule: ", MOrder.INVOICERULE_Immediate, savedBPartner.getInvoiceRule());
 		assertEquals("Should have a Payment Rule: ", MOrder.PAYMENTRULE_Cash, savedBPartner.getPaymentRule());
 		assertNotNull("Should have a Price List: ", savedBPartner.getM_PriceList_ID());
+
+		rollback();
 	}
 
 	@Test
-	public void testAfterNewEvent() {
-		// assertEquals(true, false);
-	}
+	public void testAfterCreateNewPatientEvent() {
+		MBPartner_BH bPartner = new MBPartner_BH(getCtx(), 0, getTrxName());
+		bPartner.setName("Joe Blow2");
+		bPartner.setBH_PatientID("000002");
+		bPartner.setBH_IsPatient(true);
 
-	@Test
-	public void testBeforeChangeEvent() {
-		// assertEquals(true, false);
-	}
+		bPartner.saveEx();
 
-	@Test
-	public void testAfterChangeEvent() {
-		// assertEquals(true, false);
+		MBPartner_BH savedBPartner = (MBPartner_BH) MBPartner_BH.get(getCtx(), bPartner.get_ID(), getTrxName());
+
+		// should have a user contact
+		MUser user = new MUser(savedBPartner);
+		assertNotNull("Should have a user contact ", user);
+
+		// should have a location
+		MBPartnerLocation businessPartnerLocation = new MBPartnerLocation(savedBPartner);
+		assertNotNull("Should have a location ", businessPartnerLocation);
+
+		rollback();
 	}
 }
