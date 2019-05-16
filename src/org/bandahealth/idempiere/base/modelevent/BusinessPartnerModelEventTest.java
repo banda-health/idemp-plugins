5d86850e7c1628e4009015e0db3fffb5faf8e19d
Andrew <andrewndungu@gmail.com>
2019-05-16 09:49:30 +0300

    Test Case Template

---
+++ src/org/bandahealth/idempiere/base/modelevent/BusinessPartnerModelEventTest.java
@@ -0,0 +1,23 @@
+package org.bandahealth.idempiere.base.modelevent;
+
+import org.junit.Test;
+
+import test.AdempiereTestCase;
+
+public class BusinessPartnerModelEventTest extends AdempiereTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		assertNotNull("Context should not be null", getCtx());
+	}
+
+	@Test
+	public void testCreateNewPatient() {
+	}
+
+	@Test
+	public void testAfterCreateNewPatientEvent() {
+	}
+}
