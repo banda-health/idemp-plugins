eb7c3bfc41dc6747d53b958894cdd9be3eb45ff4
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-07-19 11:07:14 +0300

    Debug method to add DomObserver for tabs resetting.

--- theme/bandahealth/js/bandahealth.js
+++ theme/bandahealth/js/bandahealth.js
@@ -45,7 +45,6 @@ define(
                 hideEastPanel();
                 appendLogoutButton();
                 addDomObservationMethods();
-                addTabCountResetObservation();
                 addDashboardPanelMethods();
                 expandDashboardMenuTabPanels();
                 openInitialWindow();
@@ -130,6 +129,7 @@ define(
                                     return;
                                 }
                                 disableZoomAccrossWindows();
+                               
                                 let bodyTagClasses = document.body.classList;
                                 if (!areAnyTabsVisisble()) {
                                     util
@@ -245,30 +245,17 @@ define(
         /*On adding a new window, clear/close the previous one*/
         function addTabCountResetObservation(){
         	let mainTab = document.querySelector('.desktop-tabbox.z-tabbox.z-tabbox-top > .z-tabs > .z-tabs-content');
-        	 util.executeFunctionWhenElementPresent(mainTab,
-        			 function createTabResetObserver(){
-        		 		let tabObserver = new DomObserver(mainTab, function(mutationsList){
-        		 			mutationsList.forEach(mutation => {
-        		 				var previous = mutation.previousSibling;
-        		 				if(previous != null){
-        		 					previous.remove();
-        		 					console.log("Removed previous");
-        		 				});
-        		 			}
-//        	var config = {childList:true};
-//        	var callback = function(mutationsList){
-//        	    mutationsList.forEach(mutation => {
-//        			var previous = mutation.previousSibling;
-//        			if(previous != null){
-//        				previous.remove();
-//        	        }
-//        	    })};
-//        	    var observer = new MutationObserver(callback);
-//        	    observer.observe(bar,config);
-        	
-//        	var bar = document.querySelector('.desktop-tabbox.z-tabbox.z-tabbox-top > .z-tabs > .z-tabs-content');
+        	var config = {childList:true};
+        	var callback = function(mutationsList){
+        	    mutationsList.forEach(mutation => {
+        			var previous = mutation.previousSibling;
+        			if(previous != null){
+        				previous.remove();
+        	        }
+        	    })};
+        	    var observer = new MutationObserver(callback);
+        	    observer.observe(mainTab,config);
         }
-        	 }}
 
         function addDashboardPanelMethods() {
             util.executeFunctionWhenElementPresent('.desktop-left-column',
@@ -283,6 +270,7 @@ define(
                                 return;
                             }
                             expandDashboardMenuTabPanels();
+                            addTabCountResetObservation();
 
                         });
                 }, maxTimeToWaitUntilDomElementsAppearMS);
@@ -770,7 +758,4 @@ define(
             }
             return activeElement;
         }
-        
-
-        
     });
