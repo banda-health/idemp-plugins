b95e6915893618a4f20021ab733d99fd9703191e
Andrew Moko <amoko@bandahealth.org>
2019-12-10 15:23:27 +0300

    go-817 Expose Show Expenses REST services on iDemp REST

---
+++ WEB-INF/src/org/bandahealth/idempiere/rest/model/Expense.java
@@ -0,0 +1,30 @@
+package org.bandahealth.idempiere.rest.model;
+
+import java.math.BigDecimal;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "expense")
+public class Expense extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+	private BigDecimal amount;
+
+	public Expense(int clientId, int orgId, String uuid, boolean isActive, String created, int createdBy, String name,
+			String description, BigDecimal amount) {
+		super(clientId, orgId, uuid, isActive, created, createdBy, name, description);
+
+		this.amount = amount;
+	}
+
+	@XmlElement
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+}
