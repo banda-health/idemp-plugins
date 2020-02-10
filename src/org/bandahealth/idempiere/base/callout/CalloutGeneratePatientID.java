5f36ec0dd772ac4551b1ab50db5c72edd266341d
Andrew Moko <amoko@bandahealth.org>
2020-02-10 16:04:51 +0300

    Default patient id for the first patient

--- src/org/bandahealth/idempiere/base/callout/CalloutGeneratePatientID.java
+++ src/org/bandahealth/idempiere/base/callout/CalloutGeneratePatientID.java
@@ -23,19 +23,27 @@ public class CalloutGeneratePatientID implements IColumnCallout {
 				return null;
 			}
 
+			String whereClause = MBPartner_BH.COLUMNNAME_BH_IsPatient + "=?";
+
 			// retrieve last entered patient
-			MBPartner_BH lastCreatedPatient = new Query(ctx, MBPartner_BH.Table_Name, null, null)
-					.setOnlyActiveRecords(true).setClient_ID().setOrderBy(MBPartner_BH.COLUMNNAME_Created + " DESC, "
-							+ MBPartner_BH.COLUMNNAME_BH_PatientID + " DESC")
-					.first();
+			MBPartner_BH lastCreatedPatient = new Query(ctx, MBPartner_BH.Table_Name, whereClause, null).setClient_ID()
+					.setOrderBy(MBPartner_BH.COLUMNNAME_Created + " DESC, " + MBPartner_BH.COLUMNNAME_BH_PatientID
+							+ " DESC")
+					.setParameters("Y").first();
+
+			if (lastCreatedPatient == null) {
+				mTab.setValue(MBPartner_BH.COLUMNNAME_BH_PatientID, 100000);
+				return null;
+			}
+
 			String lastCreatedPatientId = lastCreatedPatient.getBH_PatientID();
 			// check if numeric
 			if (isNumeric(lastCreatedPatientId)) {
 				int numericCreatedPatientId = Integer.valueOf(lastCreatedPatientId);
 
 				// check if there is an updated record greater than the last entered patient id.
-				MBPartner_BH lastUpdatedPatient = new Query(ctx, MBPartner_BH.Table_Name, null, null)
-						.setOnlyActiveRecords(true).setClient_ID().setOrderBy(MBPartner_BH.COLUMNNAME_Updated + " DESC")
+				MBPartner_BH lastUpdatedPatient = new Query(ctx, MBPartner_BH.Table_Name, whereClause, null)
+						.setClient_ID().setOrderBy(MBPartner_BH.COLUMNNAME_Updated + " DESC").setParameters("Y")
 						.first();
 				String lastUpdatedPatientId = lastUpdatedPatient.getBH_PatientID();
 				if (isNumeric(lastUpdatedPatientId)) {
