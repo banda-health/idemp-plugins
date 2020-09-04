52241b52cc8733f7a4043aa27a9153c35b45d7dc
Andrew Moko <amoko@bandahealth.org>
2020-09-04 11:40:06 +0300

    go-1283 Hide reports by default

--- WEB-INF/src/org/bandahealth/idempiere/rest/service/db/MenuGroupDBService.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/service/db/MenuGroupDBService.java
@@ -110,6 +110,25 @@ public class MenuGroupDBService {
 		return null;
 	}
 
+	public boolean hasAccessToReports() {
+		// Retrieve Reports Menu
+		MHomeScreenButtonGroup menu = new Query(Env.getCtx(), MHomeScreenButtonGroup.Table_Name,
+				MHomeScreenButtonGroup.COLUMNNAME_Name + "=?", null)
+						.setOrderBy(MHomeScreenButtonGroup.COLUMNNAME_LineNo).setOnlyActiveRecords(true)
+						.setParameters("Reports").first();
+		if (menu == null) {
+			return false;
+		}
+
+		// Get reports
+		List<MHomeScreenButton> reports = getMenuGroupLineItems(menu.get_ID(), hasAdminPrivileges(getRoleId()), null);
+		if (!reports.isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
+
 	/**
 	 * Get group line items
 	 * 
@@ -117,29 +136,52 @@ public class MenuGroupDBService {
 	 * @return
 	 */
 	private List<MenuGroupLineItem> getMenuGroupLineItems(int menuGroupItemId, boolean isAdmin) {
+		List<MenuGroupLineItem> results = new ArrayList<>();
+
+		List<MHomeScreenButton> menuItems = getMenuGroupLineItems(menuGroupItemId, isAdmin, "'Metrics', 'Reports'");
+
+		if (!menuItems.isEmpty()) {
+			for (MHomeScreenButton menuItem : menuItems) {
+				results.add(new MenuGroupLineItem(menuItem.getAD_Client_ID(), menuItem.getAD_Org_ID(),
+						menuItem.getBH_HmScrn_ButtonGroupLine_UU(), menuItem.isActive(),
+						DateUtil.parse(menuItem.getCreated()), menuItem.getCreatedBy(), menuItem.getName(),
+						menuItem.getDescription(), menuItem.getAD_InfoWindow_ID(), menuItem.getAD_Window_ID(),
+						menuItem.getAD_Process_ID(), menuItem.getAD_Form_ID(), menuItem.getIncludedRole_ID(),
+						menuItem.getLineNo(), menuItem.getButtonText(), menuItem.getIconClassName(),
+						menuItem.getButtonClassName()));
+			}
+		}
+
+		return results;
+	}
+
+	private List<MHomeScreenButton> getMenuGroupLineItems(int menuGroupItemId, boolean isAdmin, String exclude) {
 		try {
-			List<MenuGroupLineItem> results = new ArrayList<>();
 			List<Object> parameters = new ArrayList<>();
 
-			String whereClause;
+			StringBuilder whereClause = new StringBuilder();
 			if (menuGroupItemId > 0) {
-				whereClause = MHomeScreenButton.Table_Name + "." + MHomeScreenButton.COLUMNNAME_BH_HmScrn_ButtonGroup_ID
-						+ " =?";
+				whereClause = new StringBuilder(
+						MHomeScreenButton.Table_Name + "." + MHomeScreenButton.COLUMNNAME_BH_HmScrn_ButtonGroup_ID)
+								.append(" =?");
 				parameters.add(menuGroupItemId);
 			} else {
-				// filter out metrics and reports
-				whereClause = MHomeScreenButtonGroup.Table_Name + "." + MHomeScreenButtonGroup.COLUMNNAME_Name
-						+ " NOT IN ('Metrics', 'Reports')";
+				// filter out metrics and reports by default
+				whereClause.append(MHomeScreenButtonGroup.Table_Name + "." + MHomeScreenButtonGroup.COLUMNNAME_Name);
+
+				if (exclude != null) {
+					whereClause.append(" NOT IN (").append(exclude).append(")");
+				}
 			}
 
 			if (!isAdmin) {
-				whereClause += " AND ";
+				whereClause.append(" AND ");
 
-				whereClause += MRoleIncluded.Table_Name + "." + MRoleIncluded.COLUMNNAME_AD_Role_ID + "=?";
+				whereClause.append(MRoleIncluded.Table_Name + "." + MRoleIncluded.COLUMNNAME_AD_Role_ID + "=?");
 				parameters.add(getRoleId());
 			}
 
-			Query query = new Query(Env.getCtx(), MHomeScreenButton.Table_Name, whereClause, null)
+			Query query = new Query(Env.getCtx(), MHomeScreenButton.Table_Name, whereClause.toString(), null)
 					.setOnlyActiveRecords(true)
 					.setOrderBy(MHomeScreenButton.Table_Name + "." + MHomeScreenButton.COLUMNNAME_LineNo);
 
@@ -160,21 +202,7 @@ public class MenuGroupDBService {
 				query = query.setParameters(parameters);
 			}
 
-			List<MHomeScreenButton> menuItems = query.list();
-
-			if (!menuItems.isEmpty()) {
-				for (MHomeScreenButton menuItem : menuItems) {
-					results.add(new MenuGroupLineItem(menuItem.getAD_Client_ID(), menuItem.getAD_Org_ID(),
-							menuItem.getBH_HmScrn_ButtonGroupLine_UU(), menuItem.isActive(),
-							DateUtil.parse(menuItem.getCreated()), menuItem.getCreatedBy(), menuItem.getName(),
-							menuItem.getDescription(), menuItem.getAD_InfoWindow_ID(), menuItem.getAD_Window_ID(),
-							menuItem.getAD_Process_ID(), menuItem.getAD_Form_ID(), menuItem.getIncludedRole_ID(),
-							menuItem.getLineNo(), menuItem.getButtonText(), menuItem.getIconClassName(),
-							menuItem.getButtonClassName()));
-				}
-			}
-
-			return results;
+			return query.list();
 
 		} catch (Exception ex) {
 			log.severe(ex.getMessage());
