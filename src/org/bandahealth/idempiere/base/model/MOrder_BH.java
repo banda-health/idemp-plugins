0753515cb1f78bba52b813b52092ed80a0f7f194
Andrew Moko <amoko@bandahealth.org>
2020-09-24 06:13:46 +0300

    go-1327 Add second diagnosis

--- src/org/bandahealth/idempiere/base/model/MOrder_BH.java
+++ src/org/bandahealth/idempiere/base/model/MOrder_BH.java
@@ -55,6 +55,8 @@ public class MOrder_BH extends MOrder {
 	public static final String COLUMNNAME_BH_HEIGHT = "BH_Height";
 	
 	public static final String COLUMNNAME_BH_WEIGHT = "BH_Weight";
+	
+	public static final String COLUMNNAME_BH_DIAGNOSIS_II = "BH_Diagnosis_II";
 
 	public MOrder_BH(Properties ctx, int C_Order_ID, String trxName) {
 		super(ctx, C_Order_ID, trxName);
@@ -416,5 +418,13 @@ public class MOrder_BH extends MOrder {
 	
 	public void setBH_Weight(String BH_Weight) {
 		set_Value(COLUMNNAME_BH_WEIGHT, BH_Weight);
+	}
+	
+	public String getBH_Diagnosis_II() {
+		return (String)get_Value( COLUMNNAME_BH_DIAGNOSIS_II);
+	}
+	
+	public void setBH_Diagnosis_II(String BH_Diagnosis_II) {
+		set_Value(COLUMNNAME_BH_DIAGNOSIS_II, BH_Diagnosis_II);
 	}
 }
