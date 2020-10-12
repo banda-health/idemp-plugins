5b204367ff38705757c92d2651f1b31db7f7ddbd
Kevin <kwburnett@gmail.com>
2020-10-12 16:15:03 +0100

    updating expiry to be returned on fetch

--- WEB-INF/src/org/bandahealth/idempiere/rest/service/db/ProductDBService.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/service/db/ProductDBService.java
@@ -226,7 +226,8 @@ public class ProductDBService extends BaseDBService<Product, MProduct_BH> {
 		try {
 			return new Product(product.getAD_Client_ID(), product.getAD_Org_ID(), product.getM_Product_UU(),
 					product.isActive(), DateUtil.parseDateOnly(product.getCreated()), product.getCreatedBy(),
-					product.getName(), product.getDescription(), product.getBH_BuyPrice(), product.getBH_SellPrice(), product.getBH_PriceMargin());
+					product.getName(), product.getDescription(), product.getBH_BuyPrice(), product.getBH_SellPrice(),
+					product.getBH_PriceMargin(), product);
 		} catch (Exception ex) {
 			log.severe("Error creating product instance: " + ex);
 			throw new RuntimeException(ex.getLocalizedMessage(), ex);
