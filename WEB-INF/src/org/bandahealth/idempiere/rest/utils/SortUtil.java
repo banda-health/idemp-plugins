2f57589abb815b0a72b9479b89d6994dd09b41e5
Kelly Muhindi <kmuhindi@bandahealth.org>
2020-11-03 10:31:32 +0300

    Add comments on sort method.

--- WEB-INF/src/org/bandahealth/idempiere/rest/utils/SortUtil.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/utils/SortUtil.java
@@ -17,12 +17,14 @@ public class SortUtil {
 	/**
 	 * Get the table join entry from the alias column
 	 * @param sortColumn column name to use as key 
+	 * @param joinClause A map of join columns provided	 
 	 * @param joinColumns A map of join columns provided
 	 * @return a string representing the join clause. 
 	 */
 	public static String getJoinClauseFromAlias(String sortColumn, String joinClause,  Map<String, String> joinColumns) {
 		String tableName = sortColumn.substring(0, sortColumn.indexOf("."));
 		String alias = tableName + ".";
+		//Do not add JOIN if it is already added. 
 		if (joinClause != null && joinClause.toLowerCase().contains(alias))
 			return null;
 		return joinColumns.get(tableName.toString());
