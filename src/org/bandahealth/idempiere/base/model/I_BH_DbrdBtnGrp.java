d6d0bf3a66f744f879ec428f6110907ee1ab1fb7
Kevin <kwburnett@gmail.com>
2020-11-20 17:28:48 +0100

    re-generating models from iDempiere 7.1

--- src/org/bandahealth/idempiere/base/model/I_BH_DbrdBtnGrp.java
+++ src/org/bandahealth/idempiere/base/model/I_BH_DbrdBtnGrp.java
@@ -22,50 +22,50 @@ import org.compiere.model.*;
 import org.compiere.util.KeyNamePair;
 
 /** Generated Interface for BH_DbrdBtnGrp
- *  @author iDempiere (generated)
- *  @version Release 5.1
+ *  @author iDempiere (generated) 
+ *  @version Release 7.1
  */
 @SuppressWarnings("all")
-public interface I_BH_DbrdBtnGrp
+public interface I_BH_DbrdBtnGrp 
 {
 
-	/** TableName=BH_DbrdBtnGrp */
-	public static final String Table_Name = "BH_DbrdBtnGrp";
+    /** TableName=BH_DbrdBtnGrp */
+    public static final String Table_Name = "BH_DbrdBtnGrp";
 
-	/** AD_Table_ID=1000002 */
-	public static final int Table_ID = MTable.getTable_ID(Table_Name);
+    /** AD_Table_ID=1000002 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
 
-	KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
 
-	/** AccessLevel = 4 - System
-	 */
-	BigDecimal accessLevel = BigDecimal.valueOf(4);
+    /** AccessLevel = 4 - System 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(4);
 
-	/** Load Meta Data */
+    /** Load Meta Data */
 
-	/** Column name AD_Client_ID */
-	public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
 
 	/** Get Client.
-	 * Client/Tenant for this installation.
-	 */
+	  * Client/Tenant for this installation.
+	  */
 	public int getAD_Client_ID();
 
-	/** Column name AD_Org_ID */
-	public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
 
 	/** Set Organization.
-	 * Organizational entity within client
-	 */
+	  * Organizational entity within client
+	  */
 	public void setAD_Org_ID (int AD_Org_ID);
 
 	/** Get Organization.
-	 * Organizational entity within client
-	 */
+	  * Organizational entity within client
+	  */
 	public int getAD_Org_ID();
 
-	/** Column name BH_DbrdBtnGrp_ID */
-	public static final String COLUMNNAME_BH_DbrdBtnGrp_ID = "BH_DbrdBtnGrp_ID";
+    /** Column name BH_DbrdBtnGrp_ID */
+    public static final String COLUMNNAME_BH_DbrdBtnGrp_ID = "BH_DbrdBtnGrp_ID";
 
 	/** Set BH_DbrdBtnGrp_ID	  */
 	public void setBH_DbrdBtnGrp_ID (int BH_DbrdBtnGrp_ID);
@@ -73,8 +73,8 @@ public interface I_BH_DbrdBtnGrp
 	/** Get BH_DbrdBtnGrp_ID	  */
 	public int getBH_DbrdBtnGrp_ID();
 
-	/** Column name BH_DbrdBtnGrp_UU */
-	public static final String COLUMNNAME_BH_DbrdBtnGrp_UU = "BH_DbrdBtnGrp_UU";
+    /** Column name BH_DbrdBtnGrp_UU */
+    public static final String COLUMNNAME_BH_DbrdBtnGrp_UU = "BH_DbrdBtnGrp_UU";
 
 	/** Set BH_DbrdBtnGrp_UU	  */
 	public void setBH_DbrdBtnGrp_UU (String BH_DbrdBtnGrp_UU);
@@ -82,87 +82,87 @@ public interface I_BH_DbrdBtnGrp
 	/** Get BH_DbrdBtnGrp_UU	  */
 	public String getBH_DbrdBtnGrp_UU();
 
-	/** Column name Created */
-	public static final String COLUMNNAME_Created = "Created";
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
 
 	/** Get Created.
-	 * Date this record was created
-	 */
+	  * Date this record was created
+	  */
 	public Timestamp getCreated();
 
-	/** Column name CreatedBy */
-	public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
 
 	/** Get Created By.
-	 * User who created this records
-	 */
+	  * User who created this records
+	  */
 	public int getCreatedBy();
 
-	/** Column name Description */
-	public static final String COLUMNNAME_Description = "Description";
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
 
 	/** Set Description.
-	 * Optional short description of the record
-	 */
+	  * Optional short description of the record
+	  */
 	public void setDescription (String Description);
 
 	/** Get Description.
-	 * Optional short description of the record
-	 */
+	  * Optional short description of the record
+	  */
 	public String getDescription();
 
-	/** Column name IsActive */
-	public static final String COLUMNNAME_IsActive = "IsActive";
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
 
 	/** Set Active.
-	 * The record is active in the system
-	 */
+	  * The record is active in the system
+	  */
 	public void setIsActive (boolean IsActive);
 
 	/** Get Active.
-	 * The record is active in the system
-	 */
+	  * The record is active in the system
+	  */
 	public boolean isActive();
 
-	/** Column name LineNo */
-	public static final String COLUMNNAME_LineNo = "LineNo";
+    /** Column name LineNo */
+    public static final String COLUMNNAME_LineNo = "LineNo";
 
 	/** Set Line.
-	 * Line No
-	 */
+	  * Line No
+	  */
 	public void setLineNo (int LineNo);
 
 	/** Get Line.
-	 * Line No
-	 */
+	  * Line No
+	  */
 	public int getLineNo();
 
-	/** Column name Name */
-	public static final String COLUMNNAME_Name = "Name";
+    /** Column name Name */
+    public static final String COLUMNNAME_Name = "Name";
 
 	/** Set Name.
-	 * Alphanumeric identifier of the entity
-	 */
+	  * Alphanumeric identifier of the entity
+	  */
 	public void setName (String Name);
 
 	/** Get Name.
-	 * Alphanumeric identifier of the entity
-	 */
+	  * Alphanumeric identifier of the entity
+	  */
 	public String getName();
 
-	/** Column name Updated */
-	public static final String COLUMNNAME_Updated = "Updated";
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
 
 	/** Get Updated.
-	 * Date this record was updated
-	 */
+	  * Date this record was updated
+	  */
 	public Timestamp getUpdated();
 
-	/** Column name UpdatedBy */
-	public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
 
 	/** Get Updated By.
-	 * User who updated this records
-	 */
+	  * User who updated this records
+	  */
 	public int getUpdatedBy();
 }
--- src/org/bandahealth/idempiere/base/model/X_BH_DbrdBtnGrp.java
+++ src/org/bandahealth/idempiere/base/model/X_BH_DbrdBtnGrp.java
@@ -23,148 +23,148 @@ import org.compiere.model.*;
 import org.compiere.util.KeyNamePair;
 
 /** Generated Model for BH_DbrdBtnGrp
- *  @author iDempiere (generated)
- *  @version Release 5.1 - $Id$ */
-public class X_BH_DbrdBtnGrp extends PO implements I_BH_DbrdBtnGrp, I_Persistent
+ *  @author iDempiere (generated) 
+ *  @version Release 7.1 - $Id$ */
+public class X_BH_DbrdBtnGrp extends PO implements I_BH_DbrdBtnGrp, I_Persistent 
 {
 
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20190306L;
+	private static final long serialVersionUID = 20201120L;
 
-	/** Standard Constructor */
-	public X_BH_DbrdBtnGrp (Properties ctx, int BH_DbrdBtnGrp_ID, String trxName)
-	{
-		super (ctx, BH_DbrdBtnGrp_ID, trxName);
-		/** if (BH_DbrdBtnGrp_ID == 0)
-		 {
-		 setBH_DbrdBtnGrp_ID (0);
-		 setName (null);
-		 } */
-	}
+    /** Standard Constructor */
+    public X_BH_DbrdBtnGrp (Properties ctx, int BH_DbrdBtnGrp_ID, String trxName)
+    {
+      super (ctx, BH_DbrdBtnGrp_ID, trxName);
+      /** if (BH_DbrdBtnGrp_ID == 0)
+        {
+			setBH_DbrdBtnGrp_ID (0);
+			setName (null);
+        } */
+    }
 
-	/** Load Constructor */
-	public X_BH_DbrdBtnGrp (Properties ctx, ResultSet rs, String trxName)
-	{
-		super (ctx, rs, trxName);
-	}
+    /** Load Constructor */
+    public X_BH_DbrdBtnGrp (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
 
-	/** AccessLevel
-	 * @return 4 - System
-	 */
-	protected int get_AccessLevel()
-	{
-		return accessLevel.intValue();
-	}
+    /** AccessLevel
+      * @return 4 - System 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
 
-	/** Load Meta Data */
-	protected POInfo initPO (Properties ctx)
-	{
-		POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
-		return poi;
-	}
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
 
-	public String toString()
-	{
-		StringBuffer sb = new StringBuffer ("X_BH_DbrdBtnGrp[")
-				.append(get_ID()).append("]");
-		return sb.toString();
-	}
+    public String toString()
+    {
+      StringBuilder sb = new StringBuilder ("X_BH_DbrdBtnGrp[")
+        .append(get_ID()).append(",Name=").append(getName()).append("]");
+      return sb.toString();
+    }
 
 	/** Set BH_DbrdBtnGrp_ID.
-	 @param BH_DbrdBtnGrp_ID BH_DbrdBtnGrp_ID	  */
+		@param BH_DbrdBtnGrp_ID BH_DbrdBtnGrp_ID	  */
 	public void setBH_DbrdBtnGrp_ID (int BH_DbrdBtnGrp_ID)
 	{
-		if (BH_DbrdBtnGrp_ID < 1)
+		if (BH_DbrdBtnGrp_ID < 1) 
 			set_ValueNoCheck (COLUMNNAME_BH_DbrdBtnGrp_ID, null);
-		else
+		else 
 			set_ValueNoCheck (COLUMNNAME_BH_DbrdBtnGrp_ID, Integer.valueOf(BH_DbrdBtnGrp_ID));
 	}
 
 	/** Get BH_DbrdBtnGrp_ID.
-	 @return BH_DbrdBtnGrp_ID	  */
-	public int getBH_DbrdBtnGrp_ID ()
+		@return BH_DbrdBtnGrp_ID	  */
+	public int getBH_DbrdBtnGrp_ID () 
 	{
 		Integer ii = (Integer)get_Value(COLUMNNAME_BH_DbrdBtnGrp_ID);
 		if (ii == null)
-			return 0;
+			 return 0;
 		return ii.intValue();
 	}
 
 	/** Set BH_DbrdBtnGrp_UU.
-	 @param BH_DbrdBtnGrp_UU BH_DbrdBtnGrp_UU	  */
+		@param BH_DbrdBtnGrp_UU BH_DbrdBtnGrp_UU	  */
 	public void setBH_DbrdBtnGrp_UU (String BH_DbrdBtnGrp_UU)
 	{
 		set_Value (COLUMNNAME_BH_DbrdBtnGrp_UU, BH_DbrdBtnGrp_UU);
 	}
 
 	/** Get BH_DbrdBtnGrp_UU.
-	 @return BH_DbrdBtnGrp_UU	  */
-	public String getBH_DbrdBtnGrp_UU ()
+		@return BH_DbrdBtnGrp_UU	  */
+	public String getBH_DbrdBtnGrp_UU () 
 	{
 		return (String)get_Value(COLUMNNAME_BH_DbrdBtnGrp_UU);
 	}
 
 	/** Set Description.
-	 @param Description
-	 Optional short description of the record
-	 */
+		@param Description 
+		Optional short description of the record
+	  */
 	public void setDescription (String Description)
 	{
 		set_Value (COLUMNNAME_Description, Description);
 	}
 
 	/** Get Description.
-	 @return Optional short description of the record
-	 */
-	public String getDescription ()
+		@return Optional short description of the record
+	  */
+	public String getDescription () 
 	{
 		return (String)get_Value(COLUMNNAME_Description);
 	}
 
 	/** Set Line.
-	 @param LineNo
-	 Line No
-	 */
+		@param LineNo 
+		Line No
+	  */
 	public void setLineNo (int LineNo)
 	{
 		set_Value (COLUMNNAME_LineNo, Integer.valueOf(LineNo));
 	}
 
 	/** Get Line.
-	 @return Line No
-	 */
-	public int getLineNo ()
+		@return Line No
+	  */
+	public int getLineNo () 
 	{
 		Integer ii = (Integer)get_Value(COLUMNNAME_LineNo);
 		if (ii == null)
-			return 0;
+			 return 0;
 		return ii.intValue();
 	}
 
 	/** Set Name.
-	 @param Name
-	 Alphanumeric identifier of the entity
-	 */
+		@param Name 
+		Alphanumeric identifier of the entity
+	  */
 	public void setName (String Name)
 	{
 		set_Value (COLUMNNAME_Name, Name);
 	}
 
 	/** Get Name.
-	 @return Alphanumeric identifier of the entity
-	 */
-	public String getName ()
+		@return Alphanumeric identifier of the entity
+	  */
+	public String getName () 
 	{
 		return (String)get_Value(COLUMNNAME_Name);
 	}
 
-	/** Get Record ID/ColumnName
-	 @return ID/ColumnName pair
-	 */
-	public KeyNamePair getKeyNamePair()
-	{
-		return new KeyNamePair(get_ID(), getName());
-	}
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), getName());
+    }
 }
