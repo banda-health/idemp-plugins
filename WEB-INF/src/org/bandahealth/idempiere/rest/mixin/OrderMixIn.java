0d16aca1ea73801ef501ee5bdeab7d1118d637ad
Kevin <kwburnett@gmail.com>
2020-12-28 20:15:31 +0100

    removing some unneeded properties to increase speed

--- WEB-INF/src/org/bandahealth/idempiere/rest/mixin/OrderMixIn.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/mixin/OrderMixIn.java
@@ -8,6 +8,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 		"c_CashLine", "c_Charge", "c_ConversionType", "c_Currency", "c_DocType", "c_Opportunity", "c_OrderSource",
 		"c_Payment", "c_POS", "c_Project", "dropShip_Location", "dropShip_User", "link_Order", "m_FreightCategory",
 		"m_Shipper", "m_Warehouse", "quotationOrder", "ref_Order", "salesRep", "user1", "user2", "c_Invoice_ID",
-		"summary"})
+		"summary", "doc_User_ID", "currencyISO", "c_POS_ID", "pay_BPartner_ID", "link_Order_ID", "ref_Order_ID",
+		"bill_User_ID", "user2_ID", "user1_ID", "selfService", "copyFrom", "payScheduleValid", "printed", "freightAmt",
+		"m_Shipper_ID", "c_CashPlanLine_ID", "bill_Location_ID", "bill_BPartner_ID", "quotationOrder_ID",
+		"dropShip_User_ID", "dropShip_BPartner_ID", "m_FreightCategory_ID", "dropShip_Location_ID", "salesRep_ID",
+		"sendEMail", "ad_OrgTrx_ID", "c_Campaign_ID", "c_Charge_ID", "chargeAmt", "taxIncluded", "c_ConversionType_ID"})
 public abstract class OrderMixIn extends POMixIn {
 }
