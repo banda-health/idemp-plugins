baa1dae2440e76dddb39503cf2ad508cb1011fb6
Kevin <kwburnett@gmail.com>
2021-02-02 17:14:42 +0100

    removing Jackson mapping configuration

--- WEB-INF/src/org/bandahealth/idempiere/rest/mixin/LanguageMixIn.java
+++
@@ -1,7 +0,0 @@
-package org.bandahealth.idempiere.rest.mixin;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(value = {"locale", "dateFormat", "ad_PrintPaper"})
-public abstract class LanguageMixIn implements POMixIn {
-}
