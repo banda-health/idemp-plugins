ff3e070a1f757298f74debd4bacff0767d266fa0
Kevin <kwburnett@gmail.com>
2021-02-12 05:43:16 +0100

    leveraging SQL Util

--- WEB-INF/src/org/bandahealth/idempiere/rest/service/db/ReferenceListDBService.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/service/db/ReferenceListDBService.java
@@ -4,6 +4,7 @@ import org.bandahealth.idempiere.base.model.MInvoice_BH;
 import org.bandahealth.idempiere.base.model.MOrder_BH;
 import org.bandahealth.idempiere.base.model.MPayment_BH;
 import org.bandahealth.idempiere.base.utils.QueryUtil;
+import org.bandahealth.idempiere.rest.utils.SqlUtil;
 import org.compiere.model.MDocType;
 import org.compiere.model.MRefList;
 import org.compiere.model.MRole;
@@ -155,9 +156,6 @@ public class ReferenceListDBService {
 		final List<Object> optionParams = new ArrayList<>();
 		optionParams.add(clientId);
 
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
 		// Get all roles assigned to this user
 		MRole usersRole = MRole.get(Env.getCtx(), roleId);
 		List<MRole> allUsersRoles = usersRole.getIncludedRoles(true);
@@ -168,35 +166,27 @@ public class ReferenceListDBService {
 		String docTypeInClause = QueryUtil.getWhereClauseAndSetParametersForSet(new HashSet<>(docTypeIds), optionParams);
 		String roleInClause = QueryUtil.getWhereClauseAndSetParametersForSet(new HashSet<>(roleIds), optionParams);
 
-		String sql = null;
+		// Copied (with some modification) from MRole.java, method checkActionAccess
+		String sql = "SELECT DISTINCT ty.c_doctype_id, rl.ad_ref_list_id"
+				+ " FROM AD_Document_Action_Access a"
+				+ " INNER JOIN AD_Ref_List rl ON (rl.AD_Reference_ID=135 and rl.AD_Ref_List_ID=a.AD_Ref_List_ID)"
+				+ " INNER JOIN C_DocType ty ON (ty.C_DocType_ID=a.C_DocType_ID)"
+				+ " WHERE a.AD_Client_ID=? AND a.C_DocType_ID IN (" + docTypeInClause + ")"
+				+ " AND a.AD_Role_ID IN (" + roleInClause + ") AND a.IsActive=?";
+		optionParams.add("Y");
 		Map<Integer, List<Integer>> documentActionAccess = new HashMap<>();
-		try {
-			sql = "SELECT DISTINCT ty.c_doctype_id, rl.ad_ref_list_id"
-					+ " FROM AD_Document_Action_Access a"
-					+ " INNER JOIN AD_Ref_List rl ON (rl.AD_Reference_ID=135 and rl.AD_Ref_List_ID=a.AD_Ref_List_ID)"
-					+ " INNER JOIN C_DocType ty ON (ty.C_DocType_ID=a.C_DocType_ID)"
-					+ " WHERE a.AD_Client_ID=? AND a.C_DocType_ID IN (" + docTypeInClause + ")"
-					+ " AND a.AD_Role_ID IN (" + roleInClause + ") AND a.IsActive=?";
-			optionParams.add("Y");
-			pstmt = DB.prepareStatement(sql, null);
-			DB.setParameters(pstmt, optionParams);
-
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
+		SqlUtil.executeQuery(sql, optionParams, null, rs -> {
+			try {
 				int docTypeId = rs.getInt(1);
 				int documentActionId = rs.getInt(2);
 				if (!documentActionAccess.containsKey(docTypeId)) {
 					documentActionAccess.put(docTypeId, new ArrayList<>());
 				}
 				documentActionAccess.get(docTypeId).add(documentActionId);
+			} catch (SQLException e) {
+				log.log(Level.SEVERE, sql, e);
 			}
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, sql, e);
-		} finally {
-			DB.close(rs, pstmt);
-			rs = null;
-			pstmt = null;
-		}
+		});
 
 		return documentActionAccess;
 	}
