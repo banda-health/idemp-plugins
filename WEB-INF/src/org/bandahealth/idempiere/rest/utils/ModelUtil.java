739300ce96a478a74860496e13b9b08b7c0b59fa
Kevin <kwburnett@gmail.com>
2021-02-22 11:24:29 +0100

    ensuring text only gets translated if there's something present

---
+++ WEB-INF/src/org/bandahealth/idempiere/rest/utils/ModelUtil.java
@@ -0,0 +1,30 @@
+package org.bandahealth.idempiere.rest.utils;
+
+import org.bandahealth.idempiere.rest.function.VoidFunction;
+
+import java.util.List;
+
+/**
+ * A utility class to work with iDempiere models
+ */
+public class ModelUtil {
+	/**
+	 * A simplified method to set a property on an entity if it's present on another entity, typically an input model
+	 *
+	 * @param propertyValue  The property value to set, if it's not null
+	 * @param propertySetter The setter method on the entity, if the property is present
+	 * @param <T>            The type of the property
+	 */
+	public static <T> void setPropertyIfPresent(T propertyValue, VoidFunction<T> propertySetter) {
+		if (propertyValue == null) {
+			return;
+		}
+		if (propertyValue instanceof String && !StringUtil.isNullOrEmpty(propertyValue.toString())) {
+			propertySetter.apply(propertyValue);
+		} else if (propertyValue instanceof List<?> && !((List<?>) propertyValue).isEmpty()) {
+			propertySetter.apply(propertyValue);
+		} else {
+			propertySetter.apply(propertyValue);
+		}
+	}
+}
