f944bdc21faf7955112c9ae99141625236e6c781
Kevin <kwburnett@gmail.com>
2021-03-10 06:55:08 +0100

    ensuring visit dates get updated for past visits

---
+++ local_sql/postgresql/202103100650_GO-1546.sql
@@ -0,0 +1,6 @@
+ALTER TABLE c_order
+  ADD IF NOT EXISTS bh_visitdate timestamp;
+
+UPDATE c_order SET bh_visitdate = dateordered WHERE bh_visitdate IS NULL;
+
+SELECT register_migration_script('202103100650_GO-1546.sql') FROM dual;
