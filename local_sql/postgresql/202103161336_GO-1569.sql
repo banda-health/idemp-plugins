81f675848c4db9588f55eafe6e22f3f9f35ad95a
Kevin <kwburnett@gmail.com>
2021-03-16 16:29:08 +0100

    ensuring new roles get added to each client for the new ones we're creating

--- local_sql/postgresql/202103161336_GO-1569.sql
+++ local_sql/postgresql/202103161336_GO-1569.sql
@@ -658,13 +658,155 @@ INSERT INTO ad_document_action_access (ad_client_id, ad_org_id, isactive, create
 /**********************************************************************************************************/
 -- Update default roles functionality
 /**********************************************************************************************************/
+-- Clinic admin already exists, so add the others
+UPDATE ad_ref_list SET name = 'Clinician/Nurse' WHERE ad_ref_list_uu = '2a444310-ac8b-4e40-a0b9-fc6d1e66dc41';
+
+-- Now add the others
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-03-16 16:02:09.670000', 100, '2021-03-16 16:02:09.670000', 100, 'R', 'Cashier/Registration', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'), null, null, 'U', '132c218a-a6d8-4166-a8a1-36d934864ffd', 'N', 'N') ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-03-16 16:02:19.665000', 100, '2021-03-16 16:02:19.665000', 100, 'I', 'Inventory/Pharmacy', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'), null, null, 'U', '97001a3c-26e0-46b3-b0d7-b718f7c1a775', 'N', 'N') ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-03-16 16:02:30.192000', 100, '2021-03-16 16:02:30.192000', 100, 'L', 'Lab/Radiology', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'), null, null, 'U', 'bc7e156d-ddf2-4377-b915-d78b7222f942', 'N', 'N') ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-03-16 16:02:55.020000', 100, '2021-03-16 16:02:55.020000', 100, 'X', 'Accounting', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'), null, null, 'U', 'c3db36cc-c0a2-4314-9d22-8ded879870e2', 'N', 'N') ON CONFLICT DO NOTHING;
 
 /**********************************************************************************************************/
 -- Add the new base roles to each client
 /**********************************************************************************************************/
--- Make sure these are manual
--- May want to get rid of existing roles that resemble these new ones
+-- Some views depend on ad_role's fieldlength, so those have to be dropped and recreated
+DROP TABLE IF EXISTS tmp_views;
+CREATE TEMP TABLE tmp_views(
+	viewname varchar,
+	viewscript varchar
+);
+INSERT INTO tmp_views SELECT 'ad_allroles_v', pg_get_viewdef('ad_allroles_v');
+DROP VIEW ad_allroles_v;
+INSERT INTO tmp_views SELECT 'ad_alluserroles_v', pg_get_viewdef('ad_alluserroles_v');
+DROP VIEW ad_alluserroles_v;
+INSERT INTO tmp_views SELECT 'ad_sessioninfo_v', pg_get_viewdef('ad_sessioninfo_v');
+DROP VIEW ad_sessioninfo_v;
+INSERT INTO tmp_views SELECT 'ad_user_roles_v', pg_get_viewdef('ad_user_roles_v');
+DROP VIEW ad_user_roles_v;
 
+-- Some of the role names are now getting very long, so update the length of the role's name field
+alter table ad_role alter column name type varchar(200) using name::varchar(200);
+UPDATE ad_column SET fieldlength = 200 WHERE ad_column_uu = '8be89244-01ff-44dd-89b2-5b6f9c30adce';
+
+-- Now re-create the views
+DO
+LANGUAGE plpgsql
+$$
+DECLARE
+  stmt text;
+BEGIN
+  FOR stmt IN
+    SELECT 'CREATE VIEW ' || viewname || ' AS ' || viewscript FROM tmp_views
+  LOOP
+    EXECUTE stmt;
+  END LOOP;
+END;
+$$;
+
+-- Update the name of the clinician user
+UPDATE ad_role r
+SET name = c.name || ' ' || rl.name
+FROM ad_ref_list rl
+JOIN ad_reference ref
+	ON ref.ad_reference_id = rl.ad_reference_id
+CROSS JOIN ad_client c
+WHERE ref.ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'
+	AND c.ad_client_id = r.ad_client_id
+	AND r.name = c.name || ' Clinician User'
+	AND rl.ad_ref_list_uu = '2a444310-ac8b-4e40-a0b9-fc6d1e66dc41';
+
+-- Add the new roles
+DROP TABLE IF EXISTS tmp_ad_role;
+CREATE TEMP TABLE tmp_ad_role
+(
+	ad_role_id serial not null,
+	ad_client_id numeric(10) not null,
+	ad_org_id numeric(10) not null,
+	isactive char default 'Y'::bpchar not null,
+	created timestamp default now() not null,
+	createdby numeric(10) not null,
+	updated timestamp default now() not null,
+	name varchar(200) not null,
+	updatedby numeric(10) not null,
+	description varchar(255),
+	userlevel char(3) default '  O'::bpchar not null,
+	c_currency_id numeric(10),
+	amtapproval numeric default 0,
+	ad_tree_menu_id numeric(10),
+	ismanual char default 'Y'::bpchar not null,
+	isshowacct char default 'Y'::bpchar not null,
+	ispersonallock char default 'N'::bpchar not null,
+	ispersonalaccess char default 'N'::bpchar not null,
+	iscanexport char default 'Y'::bpchar not null,
+	iscanreport char default 'Y'::bpchar not null,
+	supervisor_id numeric(10),
+	iscanapproveowndoc char default 'Y'::bpchar not null,
+	isaccessallorgs char default 'N'::bpchar not null,
+	ischangelog char default 'N'::bpchar not null,
+	preferencetype char default 'C'::bpchar not null,
+	overwritepricelimit char default 'N'::bpchar not null,
+	isuseuserorgaccess char default 'N'::bpchar not null,
+	ad_tree_org_id numeric(10),
+	confirmqueryrecords numeric(10) default 0 not null,
+	maxqueryrecords numeric(10) default 0 not null,
+	connectionprofile char,
+	allow_info_account char default 'Y'::bpchar not null,
+	allow_info_asset char default 'Y'::bpchar not null,
+	allow_info_bpartner char default 'Y'::bpchar not null,
+	allow_info_cashjournal char default 'N'::bpchar not null,
+	allow_info_inout char default 'Y'::bpchar not null,
+	allow_info_invoice char default 'Y'::bpchar not null,
+	allow_info_order char default 'Y'::bpchar not null,
+	allow_info_payment char default 'Y'::bpchar not null,
+	allow_info_product char default 'Y'::bpchar not null,
+	allow_info_resource char default 'Y'::bpchar not null,
+	allow_info_schedule char default 'Y'::bpchar not null,
+	userdiscount numeric(22,2),
+	allow_info_mrp char default 'N'::bpchar not null,
+	allow_info_crp char default 'N'::bpchar not null,
+	isdiscountuptolimitprice char default 'N'::bpchar not null,
+	isdiscountallowedontotal char default 'N'::bpchar not null,
+	amtapprovalaccum numeric,
+	daysapprovalaccum numeric(10),
+	ad_role_uu uuid NOT NULL DEFAULT uuid_generate_v4(),
+	ismenuautoexpand char default 'N'::bpchar not null,
+	ismasterrole char default 'N'::bpchar not null,
+	isaccessadvanced char default 'N'::bpchar,
+	roletype varchar(2) default NULL::character varying
+);
+
+SELECT setval(
+	'tmp_ad_role_ad_role_id_seq',
+	(
+		SELECT currentnext
+		FROM ad_sequence
+		WHERE name = 'AD_Role'
+		LIMIT 1
+	)::INT,
+	false
+);
+
+INSERT INTO tmp_ad_role (ad_client_id, ad_org_id, isactive, createdby, name, updatedby, description, userlevel, c_currency_id, amtapproval, ad_tree_menu_id, ismanual, isshowacct, ispersonallock, ispersonalaccess, iscanexport, iscanreport, supervisor_id, iscanapproveowndoc, isaccessallorgs, ischangelog, preferencetype, overwritepricelimit, isuseuserorgaccess, ad_tree_org_id, confirmqueryrecords, maxqueryrecords, connectionprofile, allow_info_account, allow_info_asset, allow_info_bpartner, allow_info_cashjournal, allow_info_inout, allow_info_invoice, allow_info_order, allow_info_payment, allow_info_product, allow_info_resource, allow_info_schedule, userdiscount, allow_info_mrp, allow_info_crp, isdiscountuptolimitprice, isdiscountallowedontotal, amtapprovalaccum, daysapprovalaccum, ad_role_uu, ismenuautoexpand, ismasterrole, isaccessadvanced, roletype)
+SELECT c.ad_client_id, 0, 'Y', 100, c.name || ' ' || rl.name, 100, null, '  O', null, 0, null, 'Y', 'N', 'N', 'N', 'Y', 'Y', null, 'Y', 'N', 'N', 'O', 'N', 'N', null, 0, 0, null, 'Y', 'Y', 'Y', 'N', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', null, 'N', 'N', 'N', 'N', null, null, uuid_generate_v4(), 'N', 'N', 'N', null
+FROM ad_client c
+CROSS JOIN ad_ref_list rl
+JOIN ad_reference ref
+	ON ref.ad_reference_id = rl.ad_reference_id
+		AND ref.ad_reference_uu = '5b41f508-5ce5-4b42-80de-713e10580d51'
+WHERE c.ad_client_id > 999999
+	AND rl.ad_ref_list_uu IN ('c3db36cc-c0a2-4314-9d22-8ded879870e2','132c218a-a6d8-4166-a8a1-36d934864ffd','bc7e156d-ddf2-4377-b915-d78b7222f942','97001a3c-26e0-46b3-b0d7-b718f7c1a775');
+
+INSERT INTO ad_role (ad_role_id, ad_client_id, ad_org_id, isactive, createdby, name, updatedby, description, userlevel, c_currency_id, amtapproval, ad_tree_menu_id, ismanual, isshowacct, ispersonallock, ispersonalaccess, iscanexport, iscanreport, supervisor_id, iscanapproveowndoc, isaccessallorgs, ischangelog, preferencetype, overwritepricelimit, isuseuserorgaccess, ad_tree_org_id, confirmqueryrecords, maxqueryrecords, connectionprofile, allow_info_account, allow_info_asset, allow_info_bpartner, allow_info_cashjournal, allow_info_inout, allow_info_invoice, allow_info_order, allow_info_payment, allow_info_product, allow_info_resource, allow_info_schedule, userdiscount, allow_info_mrp, allow_info_crp, isdiscountuptolimitprice, isdiscountallowedontotal, amtapprovalaccum, daysapprovalaccum, ad_role_uu, ismenuautoexpand, ismasterrole, isaccessadvanced, roletype)
+SELECT ad_role_id, ad_client_id, ad_org_id, isactive, createdby, name, updatedby, description, userlevel, c_currency_id, amtapproval, ad_tree_menu_id, ismanual, isshowacct, ispersonallock, ispersonalaccess, iscanexport, iscanreport, supervisor_id, iscanapproveowndoc, isaccessallorgs, ischangelog, preferencetype, overwritepricelimit, isuseuserorgaccess, ad_tree_org_id, confirmqueryrecords, maxqueryrecords, connectionprofile, allow_info_account, allow_info_asset, allow_info_bpartner, allow_info_cashjournal, allow_info_inout, allow_info_invoice, allow_info_order, allow_info_payment, allow_info_product, allow_info_resource, allow_info_schedule, userdiscount, allow_info_mrp, allow_info_crp, isdiscountuptolimitprice, isdiscountallowedontotal, amtapprovalaccum, daysapprovalaccum, ad_role_uu, ismenuautoexpand, ismasterrole, isaccessadvanced, roletype
+FROM tmp_ad_role;
+
+-- Set the correct org access
+INSERT INTO ad_role_orgaccess (ad_role_id, ad_org_id, ad_client_id, isactive, createdby, updatedby, isreadonly, ad_role_orgaccess_uu)
+SELECT tar.ad_role_id, tar.ad_client_id, ao.ad_org_id, 'Y', 100, 100, 'N', uuid_generate_v4()
+FROM tmp_ad_role tar
+JOIN ad_org ao
+	ON ao.ad_client_id = tar.ad_client_id;
 
 /**********************************************************************************************************/
 -- Map existing users to the new roles
