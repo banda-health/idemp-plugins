54a155374d6f9448c99bf37e13b7f814be9bdecc
Kevin <kwburnett@gmail.com>
2021-03-16 16:31:35 +0100

    adding new fields for use elsewhere

--- src/org/bandahealth/idempiere/base/model/MReference_BH.java
+++ src/org/bandahealth/idempiere/base/model/MReference_BH.java
@@ -6,12 +6,25 @@ import java.sql.ResultSet;
 import java.util.Properties;
 
 public class MReference_BH extends MReference {
-	private static final long serialVersionUID = 1L;
 	public static final String USER_TYPE_AD_REFERENCE_UU = "5b41f508-5ce5-4b42-80de-713e10580d51";
 	/**
 	 * This represents the reference IDs for all document actions in the system
 	 */
 	public static final int DOCUMENT_ACTION_AD_REFERENCE_ID = 135;
+	/**
+	 * This represents the reference ID for date types in the system
+	 */
+	public static final int DATE_AD_REFERENCE_ID = 15;
+	/**
+	 * This represents the reference ID for list types in the system
+	 */
+	public static final int LIST_AD_REFERENCE_ID = 17;
+	/**
+	 * Reference suffix for IDs
+	 * TODO: Remove this when all reports use UUIDs instead of IDs
+	 */
+	public static final String SUFFIX_ID = "id";
+	private static final long serialVersionUID = 1L;
 
 	public MReference_BH(Properties ctx, int AD_Reference_ID, String trxName) {
 		super(ctx, AD_Reference_ID, trxName);
