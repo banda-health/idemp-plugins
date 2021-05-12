a7cfb5a413f2b286b379387c548d6d9ee2b4fd54
Kevin <kwburnett@gmail.com>
2021-05-12 12:02:34 +0100

    fix: override UUID column name generation method

--- src/org/bandahealth/idempiere/base/model/MBHChargeInfoValueSuggestion.java
+++ src/org/bandahealth/idempiere/base/model/MBHChargeInfoValueSuggestion.java
@@ -12,4 +12,13 @@ public class MBHChargeInfoValueSuggestion extends X_BH_Charge_Info_Values_Sugges
 	public MBHChargeInfoValueSuggestion(Properties ctx, ResultSet rs, String trxName) {
 		super(ctx, rs, trxName);
 	}
+
+	@Override
+	/**
+	 * Get the UUID column name for this entity. The base class truncates this due to some past constraints in the DB,
+	 * but we've removed that limitation. So, we're overriding the method here.
+	 */
+	public String getUUIDColumnName() {
+		return get_TableName() + "_UU";
+	}
 }
