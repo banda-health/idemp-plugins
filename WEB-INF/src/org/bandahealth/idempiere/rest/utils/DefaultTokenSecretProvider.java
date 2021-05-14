c3bab7ec55ac51deb144935b9c08e91801b16d0e
Kevin <kwburnett@gmail.com>
2021-05-14 17:02:37 +0100

    fix: undo dev-only file commit

--- WEB-INF/src/org/bandahealth/idempiere/rest/utils/DefaultTokenSecretProvider.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/utils/DefaultTokenSecretProvider.java
@@ -50,8 +50,7 @@ public class DefaultTokenSecretProvider implements ITokenSecretProvider {
 				keyList = new ArrayList<>();
 			}
 
-//			keyList.add(UUID.randomUUID().toString());
-			keyList.add("c758bacb-503c-44d0-b35c-baa0119b69e0"); // This is so I don't have to keep logging in...
+			keyList.add(UUID.randomUUID().toString());
 		}
 	}
 
