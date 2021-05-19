64a86838515c7a4b124c4b79ffe1f31b1cee229f
Andrew Moko <amoko@bandahealth.org>
2021-05-19 09:56:55 +0300

    add coded diagnosis fields to c_order

---
+++ local_sql/postgresql/202105181817_GO_1674.sql
@@ -0,0 +1,15 @@
+-- ADD NEW DIAGNOSIS COLUMNS
+ALTER TABLE c_order ADD IF NOT EXISTS BH_PrimaryCodedDiagnosis_ID int;
+ALTER TABLE c_order ADD IF NOT EXISTS BH_SecondaryCodedDiagnosis_ID int;
+ALTER TABLE c_order ADD IF NOT EXISTS BH_PrimaryUnCodedDiagnosis text;
+ALTER TABLE c_order ADD IF NOT EXISTS BH_SecondaryUnCodedDiagnosis text;
+
+-- populate bh_primaryuncodeddiagnosis
+UPDATE c_order SET BH_PrimaryUncodedDiagnosis = (SELECT c.description FROM c_order c WHERE c_order.c_order_id = c.c_order_id AND c.description IS NOT NULL);
+
+-- populate bh_secondaryuncodeddiagnosis
+UPDATE c_order SET BH_SecondaryUncodedDiagnosis = (SELECT c.bh_seconddiagnosis FROM c_order c WHERE c_order.c_order_id = c.c_order_id AND c.bh_seconddiagnosis IS NOT NULL);
+
+-- we don't delete `c_order.bh_seconddiagnosis` column until all dependencies and reports are updated accordingly.
+
+SELECT register_migration_script('202105181817_GO-1674.sql') FROM dual;
