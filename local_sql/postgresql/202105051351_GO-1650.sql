44608037d39d64798327063bace265e37ec28fb2
Kevin <kwburnett@gmail.com>
2021-05-31 17:17:20 +0100

    feat: add reference lists and the 2-pack

--- local_sql/postgresql/202105051351_GO-1650.sql
+++ local_sql/postgresql/202105051351_GO-1650.sql
@@ -192,40 +192,42 @@ create table if not exists bh_charge_info_values_suggestion
 		check (isactive = ANY (ARRAY['Y'::bpchar, 'N'::bpchar]))
 );
 
-create table if not exists bh_orderline_info
+create table if not exists bh_orderline_charge_info
 (
 	ad_client_id numeric(10) not null,
 	ad_org_id numeric(10) not null,
 	bh_charge_info_id numeric(10) not null,
-	bh_orderline_info_id numeric(10) not null,
-	bh_orderline_info_uu varchar(36) default NULL::character varying,
+	bh_orderline_charge_info_id numeric(10) not null,
+	bh_orderline_charge_info_uu varchar(36) default NULL::character varying,
 	c_orderline_id numeric(10) not null,
 	created timestamp default statement_timestamp() not null,
 	createdby numeric(10) not null,
 	description varchar(255) default NULL::character varying,
 	isactive char default 'Y'::bpchar not null,
-	name varchar(60) not null,
+	name varchar(60),
 	updated timestamp default statement_timestamp() not null,
 	updatedby numeric(10) not null,
-	constraint bh_orderline_info_key
-		primary key (bh_orderline_info_id),
-	constraint bh_orderline_info_uu_idx
-		unique (bh_orderline_info_uu),
-	constraint adclient_bhorderlineinfo
+	constraint bh_orderline_charge_info_key
+		primary key (bh_orderline_charge_info_id),
+	constraint bh_orderline_charge_info_uuidx
+		unique (bh_orderline_charge_info_uu),
+	constraint adclient_bhorderlinechargeinfo
 		foreign key (ad_client_id) references ad_client
 			deferrable initially deferred,
-	constraint adorg_bhorderlineinfo
+	constraint adorg_bhorderlinechargeinfo
 		foreign key (ad_org_id) references ad_org
 			deferrable initially deferred,
-	constraint bhchargeinfo_bhorderlineinfo
+	constraint bhchargeinfo_bhorderlinecharge
 		foreign key (bh_charge_info_id) references bh_charge_info
 			deferrable initially deferred,
-	constraint corderline_bhorderlineinfo
+	constraint corderline_bhorderlinechargein
 		foreign key (c_orderline_id) references c_orderline
-			deferrable initially deferred,
-	constraint bh_orderline_info_isactive_check
-		check (isactive = ANY (ARRAY['Y'::bpchar, 'N'::bpchar]))
+			deferrable initially deferred
 );
+
+alter table bh_orderline_charge_info
+	add constraint bh_orderline_charge_info_isactive_check
+		check (isactive = ANY (ARRAY['Y'::bpchar, 'N'::bpchar]));
 
 create table if not exists bh_bpartner_charge
 (
@@ -326,5 +328,40 @@ SELECT
 	uuid_generate_v4()
 FROM c_chargetype
 WHERE name = 'Default Expense Category - DO NOT CHANGE';
+
+-- Insert the new reference lists
+INSERT INTO ad_reference (ad_reference_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, validationtype, vformat, entitytype, isorderbyvalue, ad_reference_uu, ad_element_id) VALUES ((SELECT MAX(ad_reference_id) + 1 FROM ad_reference), 0, 0, 'Y', '2021-05-05 15:11:46.386000', 100, '2021-05-05 15:11:46.386000', 100, 'Non-Patient Payment Category', 'A category to help differentiate non-patient payment charges', null, 'L', null, 'U', 'N', 'b313a870-0826-4c1d-a9af-f9ec990b4375', null) ON CONFLICT DO NOTHING;
+INSERT INTO ad_reference (ad_reference_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, validationtype, vformat, entitytype, isorderbyvalue, ad_reference_uu, ad_element_id) VALUES ((SELECT MAX(ad_reference_id) + 1 FROM ad_reference), 0, 0, 'Y', '2021-05-05 15:33:16.543000', 100, '2021-05-05 15:33:16.543000', 100, 'Charge Info Data Type', null, null, 'L', null, 'U', 'N', '15b3f5d7-205a-4d91-84c0-5e38ec36b6c6', null) ON CONFLICT DO NOTHING;
+
+-- Insert the new reference list values
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-05-05 15:11:58.103000', 100, '2021-05-05 15:11:58.103000', 100, 'I', 'Insurance', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = 'b313a870-0826-4c1d-a9af-f9ec990b4375'), null, null, 'U', 'd8547f6d-5ad0-4025-b8f8-0f4796cf9d0f', null, null) ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-05-05 15:12:04.235000', 100, '2021-05-05 15:12:04.235000', 100, 'W', 'Waiver', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = 'b313a870-0826-4c1d-a9af-f9ec990b4375'), null, null, 'U', '406d22a4-b3ee-48e4-9bba-7031f653aa06', null, null) ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-05-05 15:12:10.450000', 100, '2021-05-05 15:12:10.450000', 100, 'D', 'Donation', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = 'b313a870-0826-4c1d-a9af-f9ec990b4375'), null, null, 'U', '4782b135-a84e-4eb9-ae3d-88c872a030ce', null, null) ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-05-05 15:33:46.220000', 100, '2021-05-05 15:33:46.220000', 100, 'T', 'Text', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '15b3f5d7-205a-4d91-84c0-5e38ec36b6c6'), null, null, 'U', '30c39cd3-e132-4b80-811e-74c5e06f8fae', null, null) ON CONFLICT DO NOTHING;
+INSERT INTO ad_ref_list (ad_ref_list_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, ad_reference_id, validfrom, validto, entitytype, ad_ref_list_uu, bh_update_existing, bh_add_all) VALUES ((SELECT MAX(ad_ref_list_id) + 1 FROM ad_ref_list), 0, 0, 'Y', '2021-05-05 15:33:50.594000', 100, '2021-05-05 15:33:50.594000', 100, 'L', 'List', null, (SELECT ad_reference_id FROM ad_reference WHERE ad_reference_uu = '15b3f5d7-205a-4d91-84c0-5e38ec36b6c6'), null, null, 'U', '5be1018a-8aa1-4f9a-8ec9-a022fa3675b9', null, null) ON CONFLICT DO NOTHING;
+
+-- Migrate the payment types to deactivate some and rename others
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = 'ade64e84-cd1b-43bc-a85c-c17a14963305'; -- Bill Waiver
+UPDATE ad_ref_list SET name = 'Credit or Debit Card' WHERE ad_ref_list_uu = 'd3874573-b7bf-4556-9b9c-3644698c959e';
+UPDATE ad_ref_list SET name = 'Bank Transfer' WHERE ad_ref_list_uu = '487227e8-c88e-45ef-8e6d-c0a480fdd0de';
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = 'e24511d1-9180-491c-9cc6-354b8a08e1ff'; -- Donor Fund
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = 'bb077404-71a4-4348-9afa-2b99ae9e1381'; -- CCC
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '55df64a7-1c7f-43f2-846b-f542c9cafa45'; -- MCH
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '5b4b4fcf-85c0-4d7c-851d-ab0db2e84b6d'; -- Linda Mama
+UPDATE ad_ref_list SET name = 'Cheque' WHERE ad_ref_list_uu = '900adbf9-5069-4f56-9d97-0313c6372af3';
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '7449ae78-c7d3-463b-921e-62a82a5e1a59'; -- M-TIBA
+UPDATE ad_ref_list SET name = 'Mobile Money' WHERE ad_ref_list_uu = '7a78334e-3494-4d40-a718-c42cb053eea6';
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '28617687-cb93-494a-8f03-bc453da32658'; -- NHIF
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '4caa3109-804f-4773-8115-9bdb116f329b'; -- Outreach
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '64e8ad21-7c9d-442b-9655-f5223d76140c'; -- PesaPal
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = 'bd6f5227-483d-4bcf-b1fe-a840a3142327'; -- Account
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '97e54f17-fbae-40de-8dbd-e8ad7f884732'; -- Jubilee insurance
+UPDATE ad_ref_list SET isactive = 'N' WHERE ad_ref_list_uu = '52fc8585-3c61-45b8-a0dd-db10c1e7d79c'; -- Liason insurance
+
+-- Insert the new window
+INSERT INTO ad_window (ad_window_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, windowtype, issotrx, entitytype, processing, ad_image_id, ad_color_id, isdefault, winheight, winwidth, isbetafunctionality, ad_window_uu, titlelogic) VALUES ((SELECT MAX(ad_window_id) + 1 FROM ad_window), 0, 0, 'Y', '2021-05-14 13:15:17.874000', 100, '2021-05-14 13:15:17.874000', 100, 'Non-Patient Payments', 'This records has no associated tabs or tables as all that will be handled in GL. It''s only here for access assingnment and it''s UUID.', null, 'M', 'Y', 'U', 'N', null, null, 'N', 0, 0, 'N', 'ab23d5c5-19ce-4c46-a17a-5ae2c37dd89d', null) ON CONFLICT DO NOTHING;
+
+-- Insert a new row for the menu item
+INSERT INTO bh_dbrdbtngrp_btn (bh_dbrdbtngrp_btn_id, ad_client_id, ad_infowindow_id, ad_org_id, ad_window_id, bh_dbrdbtngrp_btn_uu, buttonclassname, buttonhelptext, buttontext, created, createdby, description, iconclassname, isactive, lineno, name, updated, updatedby, bh_dbrdbtngrp_id, ad_process_id, ad_form_id, included_role_id) VALUES ((SELECT MAX(bh_dbrdbtngrp_btn_id) + 1 FROM bh_dbrdbtngrp_btn), 0, null, 0, (SELECT ad_window_id FROM ad_window WHERE ad_window_uu = 'ab23d5c5-19ce-4c46-a17a-5ae2c37dd89d'), '6a79762d-b480-4fa0-9e2e-67278fa8ee94', 'button app big', 'Non-Patient Payments', 'Non-Patient Payments', '2021-05-14 13:19:25.102000', 100, '/nonpatientpayments', 'fas fa-hand-holding-usd', 'Y', 110, 'Non-Patient Payments', '2021-05-14 13:22:16.230000', 100, (SELECT bh_dbrdbtngrp_id FROM bh_dbrdbtngrp WHERE bh_dbrdbtngrp_uu = 'bdd761f1-7979-4d87-9c5e-137c6210e9a1'), null, null, null) ON CONFLICT DO NOTHING;
 
 SELECT register_migration_script('202105051351_GO-1650.sql') FROM dual;
