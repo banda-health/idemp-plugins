1e2011d49621d33e01a4838ece41b398eb4376c8
Kevin <kwburnett@gmail.com>
2021-07-08 16:12:47 +0100

    fix: ensure coded diagonsis save works on 8.2

--- WEB-INF/src/org/bandahealth/idempiere/rest/service/db/CodedDiagnosisDBService.java
+++ WEB-INF/src/org/bandahealth/idempiere/rest/service/db/CodedDiagnosisDBService.java
@@ -11,6 +11,7 @@ import org.bandahealth.idempiere.rest.model.CodedDiagnosis;
 import org.bandahealth.idempiere.rest.model.Paging;
 import org.bandahealth.idempiere.rest.utils.QueryUtil;
 import org.bandahealth.idempiere.rest.utils.StringUtil;
+import org.compiere.model.PO;
 import org.compiere.model.Query;
 import org.compiere.util.Env;
 
@@ -75,7 +76,9 @@ public class CodedDiagnosisDBService extends BaseDBService<CodedDiagnosis, MBHCo
 
 		mCodedDiagnosis.setIsActive(entity.getIsActive());
 
+		PO.setCrossTenantSafe();
 		mCodedDiagnosis.saveEx();
+		PO.clearCrossTenantSafe();
 
 		return createInstanceWithAllFields(getEntityByUuidFromDB(mCodedDiagnosis.getBH_CodedDiagnosis_UU()));
 	}
