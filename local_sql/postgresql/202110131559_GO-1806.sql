55b64a1f0a9b672ecd6b8d693a6cd1a7e2478a55
Andrew Moko <amoko@bandahealth.org>
2021-11-04 20:59:51 +0300

    add bh_role_warehouseaccess table

--- local_sql/postgresql/202110131559_GO-1806.sql
+++ local_sql/postgresql/202110131559_GO-1806.sql
@@ -37,4 +37,28 @@ INSERT INTO adempiere.bh_dbrdbtngrp_btn (bh_dbrdbtngrp_btn_id, ad_client_id, ad_
 -- give inventory/pharmacy write access
 INSERT INTO adempiere.ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, isreadwrite, ad_window_access_uu, bh_candeactivate) VALUES ((SELECT ad_window_id FROM ad_window where ad_window_uu = 'd3c84cad-7306-464d-85da-7e629846f8c0'), (select ad_role_id FROM ad_role where ad_role_uu = 'ec17fee0-a53a-4dbb-b946-423ce14880eb'), 0, 0, 'Y', '2021-10-20 10:36:40.358000', 100, '2021-10-20 10:36:40.358000', 100, 'Y', '0bb84633-d507-43f3-95d4-3397a438b786', 'N') ON CONFLICT DO NOTHING;
 
+
+-- warehouse access window
+CREATE TABLE IF NOT EXISTS adempiere.bh_role_warehouseaccess (
+    ad_client_id numeric(10,0) NOT NULL,
+    ad_org_id numeric(10,0) NOT NULL,
+    m_warehouse_id numeric(10,0) NOT NULL,
+    bh_role_warehouseaccess_uu character varying(36) DEFAULT NULL::character varying,
+    created timestamp without time zone DEFAULT statement_timestamp() NOT NULL,
+    createdby numeric(10,0) NOT NULL,
+    isactive character(1) DEFAULT 'Y'::bpchar NOT NULL,
+    isreadonly character(1) NOT NULL,
+    updated timestamp without time zone DEFAULT statement_timestamp() NOT NULL,
+    updatedby numeric(10,0) NOT NULL,
+    bh_role_warehouseaccess_id numeric(10,0) NOT NULL,
+    ad_role_id numeric(10,0) DEFAULT NULL::numeric,
+    CONSTRAINT bh_role_warehouseaccess_isactive_check CHECK ((isactive = ANY (ARRAY['Y'::bpchar, 'N'::bpchar]))),
+    CONSTRAINT bh_role_warehouseaccess_isreadonly_check CHECK ((isreadonly = ANY (ARRAY['Y'::bpchar, 'N'::bpchar]))),
+    CONSTRAINT bh_role_warehouseaccess_uu_idx UNIQUE (bh_role_warehouseaccess_uu),
+    CONSTRAINT ad_functaccess_client FOREIGN KEY (ad_client_id) REFERENCES adempiere.ad_client(ad_client_id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT ad_functaccessorg FOREIGN KEY (ad_org_id) REFERENCES adempiere.ad_org(ad_org_id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT adrole_bhrolewarehouseaccess FOREIGN KEY (ad_role_id) REFERENCES adempiere.ad_role(ad_role_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT mwarehouse_bhrolewarehouseacce FOREIGN KEY (m_warehouse_id) REFERENCES adempiere.m_warehouse(m_warehouse_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED    
+);
+
 SELECT register_migration_script('202110131559_GO-1806.sql') FROM dual;
