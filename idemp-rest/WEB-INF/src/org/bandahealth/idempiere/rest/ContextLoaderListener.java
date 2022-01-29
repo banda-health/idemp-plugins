54d6bd0f61bddb972cd4f3fddd78165012496660
Kelly <muhindikn@gmai.com>
2022-01-29 13:20:54 +0300

    add prometheus instrumentation to rest plugin

--- idemp-rest/WEB-INF/src/org/bandahealth/idempiere/rest/ContextLoaderListener.java
+++ idemp-rest/WEB-INF/src/org/bandahealth/idempiere/rest/ContextLoaderListener.java
@@ -3,7 +3,11 @@
  */
 package org.bandahealth.idempiere.rest;
 
+import java.io.IOException;
+
 import javax.servlet.ServletContextEvent;
+
+import io.prometheus.client.exporter.HTTPServer;
 
 /**
  * @author hengsin
@@ -23,7 +27,12 @@ public class ContextLoaderListener extends
 		ClassLoader cl = Thread.currentThread().getContextClassLoader();
 		try {
 			Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+			HTTPServer server = new HTTPServer(9200);
+			System.out.println("Started Prometheus server!");
 			super.contextInitialized(event);
+		} catch (IOException e) {
+			System.out.println("Failed to start prometheus server");
+			e.printStackTrace();
 		} finally {
 			Thread.currentThread().setContextClassLoader(cl);
 		}
