f3d89026d400c2da6d21c2b21de81c15331f1d79
Kevin <kwburnett@gmail.com>
2022-03-09 07:53:16 -0600

    fix: update price lists to be valid from a date in the past

--- idemp-base/src/org/bandahealth/idempiere/base/model/MBandaSetup.java
+++ idemp-base/src/org/bandahealth/idempiere/base/model/MBandaSetup.java
@@ -1,6 +1,9 @@
 package org.bandahealth.idempiere.base.model;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -446,7 +449,8 @@ public class MBandaSetup {
 
 			// We need to get all charge info values mapped for this charge info from the
 			// map.
-			for (MBHChargeInfoValue defaultChargeInformationValue : defaultChargeInformationValuesForDefaultChargeInformation) {
+			for (MBHChargeInfoValue defaultChargeInformationValue :
+					defaultChargeInformationValuesForDefaultChargeInformation) {
 				MBHChargeInfoValue chargeInfoValue = new MBHChargeInfoValue(context, 0, getTransactionName());
 				chargeInfoValue.setName(defaultChargeInformationValue.getName());
 				chargeInfoValue.setBH_Charge_Info_ID(chargeInfo.getBH_Charge_Info_ID());
@@ -673,9 +677,9 @@ public class MBandaSetup {
 				.filter(docType -> docType.getAD_Client_ID() == 0)
 				.collect(Collectors.toMap(MDocType::getC_DocType_ID,
 						systemDocType -> docTypesForSystemAndClient.stream()
-						.filter(docType -> docType.getAD_Client_ID() != 0
-						&& docType.getName().equals(systemDocType.getName()))
-						.findFirst().map(MDocType::getC_DocType_ID).orElse(0)));
+								.filter(docType -> docType.getAD_Client_ID() != 0
+										&& docType.getName().equals(systemDocType.getName()))
+								.findFirst().map(MDocType::getC_DocType_ID).orElse(0)));
 		//		PO.clearCrossTenantSafe(); // disable what was done previously // <- uncomment for iDempiere-8.2+
 
 		// Get all access for the roles we'll configure
@@ -1174,6 +1178,12 @@ public class MBandaSetup {
 		priceListVersion.setIsActive(true);
 		priceListVersion.setM_PriceList_ID(bandaPriceList.get_ID());
 		priceListVersion.setM_DiscountSchema_ID(discountSchema.get_ID());
+		// GO-2240 Make sure the price list is valid from a date in the past because usually clients want to enter stuff
+		// they've recently received before today
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(Timestamp.from(Instant.now()));
+		calendar.add(Calendar.YEAR, -1);
+		priceListVersion.setValidFrom(new Timestamp(calendar.getTime().getTime()));
 		if (!priceListVersion.save()) {
 			log.log(Level.SEVERE, "Price-list version not saved");
 			transaction.rollback();
