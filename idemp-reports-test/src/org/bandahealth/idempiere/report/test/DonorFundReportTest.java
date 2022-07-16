af40ea8780012b1d325dd21aa52c8c137a35c731
Kevin <kwburnett@gmail.com>
2022-07-16 13:25:53 -0500

    test: adding more initial report tests

--- idemp-reports-test/src/org/bandahealth/idempiere/report/test/DonorFundReportTest.java
+++ idemp-reports-test/src/org/bandahealth/idempiere/report/test/DonorFundReportTest.java
@@ -2,13 +2,40 @@ package org.bandahealth.idempiere.report.test;
 
 import com.chuboe.test.populate.ChuBoePopulateFactoryVO;
 import com.chuboe.test.populate.IPopulateAnnotation;
+import org.bandahealth.idempiere.base.test.BandaCreateEntity;
+import org.bandahealth.idempiere.base.test.BandaValueObjectWrapper;
+import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
+import org.compiere.process.ProcessInfoParameter;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DonorFundReportTest extends ChuBoePopulateFactoryVO {
 
 	@IPopulateAnnotation.CanRun
 	public void canRunReport() throws SQLException, IOException {
+		BandaValueObjectWrapper valueObject = new BandaValueObjectWrapper();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMsg(), is(nullValue()));
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcess_UU("3478d341-c6d9-4f52-a865-5bf0ba8a7607");
+		valueObject.setProcessRecord_ID(0);
+		valueObject.setProcessTable_ID(0);
+		valueObject.setProcessInfoParams(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", TimestampUtils.yesterday(), null, null, null),
+				new ProcessInfoParameter("End Date", TimestampUtils.tomorrow(), null, null, null)
+		));
+		BandaCreateEntity.runReport(valueObject);
+		commitEx();
+
+		// TODO: Implement more criteria when we know what this report is supposed to do
+		assertNotNull(valueObject.getReport(), "Report was generated successfully");
 	}
 }
