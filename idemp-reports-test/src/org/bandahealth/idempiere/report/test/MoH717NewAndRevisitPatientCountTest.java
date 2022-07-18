1908f728497c8c2c28fecf976b2785f9f6f1ff50
Kevin <kwburnett@gmail.com>
2022-07-18 10:48:10 -0500

    test: MoH reports & open balance reports

--- idemp-reports-test/src/org/bandahealth/idempiere/report/test/MoH717NewAndRevisitPatientCountTest.java
+++ idemp-reports-test/src/org/bandahealth/idempiere/report/test/MoH717NewAndRevisitPatientCountTest.java
@@ -2,13 +2,44 @@ package org.bandahealth.idempiere.report.test;
 
 import com.chuboe.test.populate.ChuBoePopulateFactoryVO;
 import com.chuboe.test.populate.IPopulateAnnotation;
+import org.bandahealth.idempiere.base.test.BandaCreateEntity;
+import org.bandahealth.idempiere.base.test.BandaValueObjectWrapper;
+import org.bandahealth.idempiere.report.test.utils.PDFUtils;
+import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
+import org.compiere.process.ProcessInfoParameter;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MoH717NewAndRevisitPatientCountTest extends ChuBoePopulateFactoryVO {
 
 	@IPopulateAnnotation.CanRun
 	public void canRunReport() throws SQLException, IOException {
+		BandaValueObjectWrapper valueObject = new BandaValueObjectWrapper();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMsg(), is(nullValue()));
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcess_UU("742f515a-81c7-4690-8d35-2c6f1252ad5b");
+		valueObject.setProcessRecord_ID(0);
+		valueObject.setProcessTable_ID(0);
+		Timestamp startOfMonth = TimestampUtils.startOfMonth();
+		Timestamp endOfMonth = TimestampUtils.endOfMonth();
+		valueObject.setProcessInfoParams(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", startOfMonth, null, null, null),
+				new ProcessInfoParameter("End Date", endOfMonth, null, null, null)
+		));
+		BandaCreateEntity.runReport(valueObject);
+
+		String reportContent = PDFUtils.readPdfContent(valueObject.getReport(), true);
+		assertThat("Report contains General Outpatients title", reportContent,
+				containsStringIgnoringCase("General Outpatients"));
 	}
 }
--- idemp-reports-test/src/org/bandahealth/idempiere/report/test/OpenBalanceListTest.java
+++ idemp-reports-test/src/org/bandahealth/idempiere/report/test/OpenBalanceListTest.java
@@ -2,13 +2,71 @@ package org.bandahealth.idempiere.report.test;
 
 import com.chuboe.test.populate.ChuBoePopulateFactoryVO;
 import com.chuboe.test.populate.IPopulateAnnotation;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.bandahealth.idempiere.base.model.MDocType_BH;
+import org.bandahealth.idempiere.base.test.BandaCreateEntity;
+import org.bandahealth.idempiere.base.test.BandaValueObjectWrapper;
+import org.compiere.process.DocumentEngine;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OpenBalanceListTest extends ChuBoePopulateFactoryVO {
 
 	@IPopulateAnnotation.CanRun
-	public void canRunReport() throws SQLException, IOException {
+	public void canRunReport() throws SQLException, IOException, ParseException {
+		BandaValueObjectWrapper valueObject = new BandaValueObjectWrapper();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMsg(), is(nullValue()));
+
+		valueObject.setStepName("Create business partner");
+		BandaCreateEntity.createBusinessPartner(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create product");
+		BandaCreateEntity.createProduct(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create order");
+		valueObject.setDocAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		valueObject.setQty(new BigDecimal(100));
+		BandaCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcess_UU("b4f11e14-b9d8-4f6c-aa46-adfd77c4f773");
+		valueObject.setProcessRecord_ID(0);
+		valueObject.setProcessTable_ID(0);
+		valueObject.setReportType("xlsx");
+		BandaCreateEntity.runReport(valueObject);
+		valueObject.refresh();
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			Optional<Row> patientRow =
+					StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(0) != null &&
+							row.getCell(1).getStringCellValue().equalsIgnoreCase(valueObject.getBP().getName())).findFirst();
+			assertTrue(patientRow.isPresent(), "Report contains patient");
+			assertThat("Patient's open balance is correct",
+					NumberFormat.getInstance().parse(patientRow.get().getCell(9).getStringCellValue().trim()),
+					is(valueObject.getOrder().getGrandTotal().longValue()));
+		}
 	}
 }
