79aa6f958b28741c481fd430a11195da7d475d38
Andrew Moko <amoko@bandahealth.org>
2022-07-18 16:48:43 +0300

    go-1335 Create default charges and types

--- local_sql/postgresql/202207151225_GO-1335.sql
+++ local_sql/postgresql/202207151225_GO-1335.sql
@@ -1,27 +1,100 @@
--- Add default income type to configuration client.
-INSERT INTO adempiere.c_chargetype (ad_client_id, ad_org_id, c_chargetype_id, created, createdby, description, help, isactive, name, updated, updatedby, value, c_chargetype_uu) VALUES (2, 0, (SELECT MAX(c_chargetype_id) +1 FROM c_chargetype), '2022-07-15 12:20:22.290000', 100, 'For an income category added by default', null, 'Y', 'Default Income Category - DO NOT CHANGE', '2022-07-15 12:20:22.290000', 100, '1000002', 'f6875b52-1c08-4c9d-80ab-06e486f0e398') ON CONFLICT DO NOTHING;
+-- Add default income type to existing clients.
+DROP TABLE IF EXISTS tmp_c_chargetype;
+
+CREATE TEMP TABLE tmp_c_chargetype
+(
+    ad_client_id    numeric(10) default NULL::numeric         not null,
+    ad_org_id       numeric(10) default 0         		not null,
+    c_chargetype_id serial 					not null,
+    created         timestamp   default now()                 not null,
+    createdby       numeric(10) default 100                   not null,
+    description     varchar(255),
+    help            varchar(2000) default null,
+    isactive        char        default 'Y'::bpchar           not null,
+    name            varchar(60)                               not null,
+    updated 	     timestamp   default now() 		not null,
+    updatedby       numeric(10) default 100                   not null,
+    value           varchar(40)                               not null,
+    c_chargetype_uu uuid NOT NULL DEFAULT uuid_generate_v4()
+);
+
+-- SET sequence
+SELECT setval(
+	'tmp_c_chargetype_c_chargetype_id_seq', 
+	(
+		SELECT currentnext 
+		FROM ad_sequence 
+		WHERE name = 'C_ChargeType' 
+		LIMIT 1
+	)::INT, 
+	false
+);
+
+INSERT INTO tmp_c_chargetype (ad_client_id, description, name, value) SELECT c.ad_client_id, 'For an income category added by default', 'Default Income Category - DO NOT CHANGE', 'Default Income Category - DO NOT CHANGE' FROM ad_client c WHERE c.ad_client_id > 999999;
+
+INSERT INTO c_chargetype (ad_client_id, ad_org_id, c_chargetype_id, created, createdby, description, help, isactive, name, updated, updatedby, value, c_chargetype_uu) SELECT ad_client_id, ad_org_id, c_chargetype_id, created, createdby, description, help, isactive, name, updated, updatedby, value, c_chargetype_uu FROM tmp_c_chargetype ON CONFLICT DO NOTHING;
+
 
 -- Add default incomes to configuration client.
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:58:37.173000', 100, '2022-07-15 16:58:37.278000', 100, 'NHIF National Scheme - Income', 'NHIF National Scheme', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '306e0616-20ec-4432-bac9-0c68fba74cd9', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+DROP TABLE IF EXISTS tmp_c_charge;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:58:00.460000', 100, '2022-07-15 16:58:00.522000', 100, 'NHIF Fixed FFS - Income', 'NHIF Fixed FFS', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), 'd30b5094-cfda-498f-a98b-060e3dfdc09f', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+CREATE TEMP TABLE tmp_c_charge
+(
+    c_charge_id  		serial				 not null,
+    ad_client_id               numeric(10)                     not null,
+    ad_org_id                  numeric(10) default 0 	 not null,
+    isactive                   char        default 'Y'::bpchar not null,
+    created                    timestamp   default now()       not null,
+    createdby                  numeric(10) default 100         not null,
+    updated                    timestamp   default now()       not null,
+    updatedby                  numeric(10) default 100         not null,
+    name                       varchar(60)                     not null,
+    description                varchar(255),
+    chargeamt                  numeric     default 0           not null,
+    issametax                  char        default 'N'::bpchar not null,
+    issamecurrency             char        default 'N'::bpchar not null,
+    c_taxcategory_id           numeric(10) default null,
+    istaxincluded              char        default 'N'::bpchar not null,
+    c_bpartner_id              numeric(10) default null,
+    c_chargetype_id            numeric(10),
+    c_charge_uu                varchar(36) not NULL default uuid_generate_v4(),
+    c_elementvalue_id          numeric(10) default NULL,
+    bh_locked                  char        default 'Y'::bpchar,
+    bh_subtype                 varchar(2)  default NULL,
+    bh_needadditionalvisitinfo char        default 'N'::bpchar not null
+);
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:57:23.145000', 100, '2022-07-15 16:57:23.214000', 100, 'NHIF FFS - Income', 'NHIF FFS', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '1cee2276-f73d-4499-bee6-5273a01d7e22', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+SELECT setval(
+	'tmp_c_charge_c_charge_id_seq', 
+	(
+		SELECT currentnext 
+		FROM ad_sequence 
+		WHERE name = 'C_Charge' 
+		LIMIT 1
+	)::INT, 
+	false
+);
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:53:02.697000', 100, '2022-07-15 16:53:02.748000', 100, 'MCH - Income', 'MCH', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '81bd6171-29ab-412c-932c-8c09fe844141', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'NHIF National Scheme - Income', 'NHIF National Scheme - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:52:19.137000', 100, '2022-07-15 16:52:19.235000', 100, 'Linda Mama - Income', 'Linda Mama', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '62bd42c3-2489-459e-87c5-5c5c76369918', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'NHIF Fixed FFS - Income', 'NHIF Fixed FFS - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:51:43.797000', 100, '2022-07-15 16:51:43.901000', 100, 'Liason Insurance - Income', 'Liason Insurance', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), 'fb41ad55-a022-4fe2-adce-455ed2e9de30', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'NHIF FFS - Income', 'NHIF FFS - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:50:57.003000', 100, '2022-07-15 16:50:57.068000', 100, 'EduAfya FFS - Income', 'EduAfya FFS', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), 'd3925c3a-dde4-4b8b-91c8-8cd6cb03a647', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'MCH - Income', 'MCH - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 16:49:19.250000', 100, '2022-07-15 16:49:19.395000', 100, 'Donor Fund - Income', 'Donor Fund', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '09e5a872-fd6f-484e-8aef-dc2e4747576f', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'Linda Mama - Income', 'Linda Mama - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
-INSERT INTO adempiere.c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) VALUES ((SELECT MAX(c_charge_id)+1 FROM c_charge), 2, 0, 'Y', '2022-07-15 13:04:28.409000', 100, '2022-07-15 13:04:28.513000', 100, 'CCC - Income', 'CCC', 0, 'N', 'N', null, 'N', null, (SELECT c_chargetype_id FROM c_chargetype WHERE c_chargetype_uu='f6875b52-1c08-4c9d-80ab-06e486f0e398'), '774f7faa-814b-4d38-b537-8b51ada31b12', (SELECT c_elementvalue_id FROM c_elementvalue where c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c'), 'Y', null, 'N') ON CONFLICT DO NOTHING;
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'Liason Insurance - Income', 'Liason Insurance - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'EduAfya FFS - Income', 'EduAfya FFS - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
--- Update existing clients with the default charges..
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'Donor Fund - Income', 'Donor Fund - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
 
+INSERT INTO tmp_c_charge (ad_client_id, name, description, c_chargetype_id, c_elementvalue_id) SELECT c.ad_client_id, 'CCC - Income', 'CCC - Income', (SELECT c_chargetype_id FROM c_chargetype WHERE name='Default Income Category - DO NOT CHANGE' AND ad_client_id = c.ad_client_id), (SELECT c_elementvalue_id FROM c_elementvalue WHERE c_elementvalue_uu='36f9c73f-921b-4f11-9a49-d2d0f9fc5d9c') FROM ad_client c WHERE c.ad_client_id > 999999;
+
+INSERT INTO c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo) SELECT c_charge_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, chargeamt, issametax, issamecurrency, c_taxcategory_id, istaxincluded, c_bpartner_id, c_chargetype_id, c_charge_uu, c_elementvalue_id, bh_locked, bh_subtype, bh_needadditionalvisitinfo FROM tmp_c_charge ON CONFLICT DO NOTHING;
+
+SELECT update_sequences();
 
 SELECT register_migration_script('202207151225_GO-1335.sql') FROM dual;
