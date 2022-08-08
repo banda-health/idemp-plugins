962b297ec529bcc2bcc9e09ae7cb739154cb6f5e
Andrew Moko <amoko@bandahealth.org>
2022-08-08 16:12:45 +0300

    code review

--- local_sql/postgresql/202207151225_GO-1335.sql
+++ local_sql/postgresql/202207151225_GO-1335.sql
@@ -36,7 +36,7 @@ INSERT INTO c_chargetype (ad_client_id, ad_org_id, c_chargetype_id, created, cre
 
 DROP TABLE tmp_c_chargetype;
 
--- Add default incomes to configuration client.
+-- Add default income to configuration client.
 DROP TABLE IF EXISTS tmp_c_charge;
 
 CREATE TEMP TABLE tmp_c_charge
@@ -99,32 +99,32 @@ INSERT INTO c_charge (c_charge_id, ad_client_id, ad_org_id, isactive, created, c
 DROP TABLE tmp_c_charge;
 
 -- Add Income Category window
-INSERT INTO adempiere.ad_window (ad_window_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, windowtype, issotrx, entitytype, processing, ad_image_id, ad_color_id, isdefault, winheight, winwidth, isbetafunctionality, ad_window_uu, titlelogic) VALUES ((SELECT MAX(ad_window_id)+1 FROM AD_Window), 0, 0, 'Y', '2022-07-18 17:27:54.767000', 100, '2022-07-18 17:27:54.767000', 100, 'Income Categories', 'Maintain Income Categories', 'The Income Categories Window defines the different incomes that may be received.', 'M', 'Y', 'U', 'N', null, null, 'N', 0, 0, 'N', '20639eca-bd84-4ae3-b890-7b32987fcb5e', null) ON CONFLICT DO NOTHING;
+INSERT INTO adempiere.ad_window (ad_window_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, windowtype, issotrx, entitytype, processing, ad_image_id, ad_color_id, isdefault, winheight, winwidth, isbetafunctionality, ad_window_uu, titlelogic) VALUES ((SELECT MAX(ad_window_id)+1 FROM AD_Window), 0, 0, 'Y', '2022-07-18 17:27:54.767000', 100, '2022-07-18 17:27:54.767000', 100, 'Income Categories', 'Maintain Income Categories', 'The Income Categories Window defines the different income that may be received.', 'M', 'Y', 'U', 'N', null, null, 'N', 0, 0, 'N', '20639eca-bd84-4ae3-b890-7b32987fcb5e', null) ON CONFLICT DO NOTHING;
 
 -- Add Income Category Menu
 INSERT INTO adempiere.ad_menu (ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby, description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id, ad_process_id, ad_form_id, ad_workbench_id, entitytype, iscentrallymaintained, ad_menu_uu, ad_infowindow_id, showonuimenu, iconclassname) VALUES ((SELECT MAX(ad_menu_id)+1 FROM AD_Menu), 0, 0, 'Y', '2022-07-18 17:43:26.532000', 100, '2022-07-18 20:19:46.012000', 'Income Categories', 100, 'Income Categories', 'N', 'Y', 'N', 'W', (SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='20639eca-bd84-4ae3-b890-7b32987fcb5e'), null, null, null, null, null, 'U', 'Y', '4844b8ee-8387-40d8-80c8-5e73479b8b61', null, 'Y', 'far fa-credit-card') ON CONFLICT DO NOTHING;
 
--- Add Track Incomes window
-INSERT INTO adempiere.ad_window (ad_window_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, windowtype, issotrx, entitytype, processing, ad_image_id, ad_color_id, isdefault, winheight, winwidth, isbetafunctionality, ad_window_uu, titlelogic) VALUES ((SELECT MAX(ad_window_id)+1 FROM AD_Window), 0, 0, 'Y', '2022-07-18 20:07:09.040000', 100, '2022-07-18 20:07:09.040000', 100, 'Track Incomes', 'Track Incomes', 'Track Incomes', 'T', 'N', 'U', 'N', null, null, 'N', null, null, 'N', '44c02ddc-ef83-4020-8e4c-709d8cbeadc2', null) ON CONFLICT DO NOTHING;
+-- Add Track Income window
+INSERT INTO adempiere.ad_window (ad_window_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, help, windowtype, issotrx, entitytype, processing, ad_image_id, ad_color_id, isdefault, winheight, winwidth, isbetafunctionality, ad_window_uu, titlelogic) VALUES ((SELECT MAX(ad_window_id)+1 FROM AD_Window), 0, 0, 'Y', '2022-07-18 20:07:09.040000', 100, '2022-07-18 20:07:09.040000', 100, 'Track Income', 'Track Income', 'Track Income', 'T', 'N', 'U', 'N', null, null, 'N', null, null, 'N', '44c02ddc-ef83-4020-8e4c-709d8cbeadc2', null) ON CONFLICT DO NOTHING;
 
--- Add Track Incomes menu
-INSERT INTO adempiere.ad_menu (ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby, description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id, ad_process_id, ad_form_id, ad_workbench_id, entitytype, iscentrallymaintained, ad_menu_uu, ad_infowindow_id, showonuimenu, iconclassname) VALUES ((SELECT MAX(ad_menu_id)+1 FROM AD_Menu), 0, 0, 'Y', '2022-07-18 20:13:04.045000', 100, '2022-07-18 20:21:24.840000', 'Track Incomes', 100, 'Track Incomes', 'N', 'Y', 'N', 'W', (SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), null, null, null, null, null, 'U', 'Y', '88d5359a-4130-4863-830e-63a507a41cab', null, 'Y', 'fas fa-wallet') ON CONFLICT DO NOTHING;
+-- Add Track Income menu
+INSERT INTO adempiere.ad_menu (ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby, description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id, ad_process_id, ad_form_id, ad_workbench_id, entitytype, iscentrallymaintained, ad_menu_uu, ad_infowindow_id, showonuimenu, iconclassname) VALUES ((SELECT MAX(ad_menu_id)+1 FROM AD_Menu), 0, 0, 'Y', '2022-07-18 20:13:04.045000', 100, '2022-07-18 20:21:24.840000', 'Track Income', 100, 'Track Income', 'N', 'Y', 'N', 'W', (SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), null, null, null, null, null, 'U', 'Y', '88d5359a-4130-4863-830e-63a507a41cab', null, 'Y', 'fas fa-wallet') ON CONFLICT DO NOTHING;
 
 -- Add menus in Greenlight Menu Tree
 INSERT INTO adempiere.ad_treenodemm (ad_tree_id, node_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, parent_id, seqno, ad_treenodemm_uu) VALUES (10, (SELECT ad_menu_id FROM ad_menu WHERE ad_menu_uu = '88d5359a-4130-4863-830e-63a507a41cab'), 0, 0, 'Y', '2022-07-18 20:13:04.093665', 100, '2022-07-18 20:14:13.528488', 100, (SELECT ad_menu_id FROM ad_menu WHERE ad_menu_uu = '35ce7d6a-cf7d-4962-a748-75e27d0121bf'), 2, 'fb7b43cb-fd96-4732-8c6f-f36adfb7e7d5') ON CONFLICT DO NOTHING;
 
 INSERT INTO adempiere.ad_treenodemm (ad_tree_id, node_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, parent_id, seqno, ad_treenodemm_uu) VALUES (10, (SELECT ad_menu_id FROM ad_menu WHERE ad_menu_uu = '4844b8ee-8387-40d8-80c8-5e73479b8b61'), 0, 0, 'Y', '2022-07-18 17:43:26.608449', 100, '2022-07-18 20:14:13.538705', 100, (SELECT ad_menu_id FROM ad_menu WHERE ad_menu_uu = '35ce7d6a-cf7d-4962-a748-75e27d0121bf'), 5, 'deb2ce60-fef2-465b-ac37-67d173f3bf6d') ON CONFLICT DO NOTHING;
 
--- Add Track Incomes window access to Cashier/Registration Advanced Role
+-- Add Track Income window access to Cashier/Registration Advanced Role
 INSERT INTO adempiere.ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, isreadwrite, ad_window_access_uu, bh_candeactivate) VALUES ((SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), (SELECT ad_role_id FROM AD_Role WHERE ad_role_uu='ee008abc-2c16-4230-b48c-b1f5577ea270'), 0, 0, 'Y', '2022-07-18 21:43:28.659000', 100, '2022-07-18 21:43:28.659000', 100, 'Y', '506bc371-a975-43db-a7f8-6ec2b191ef87', 'N') ON CONFLICT DO NOTHING;
 
--- Add Track Incomes window access to Clinic Admin Role
+-- Add Track Income window access to Clinic Admin Role
 INSERT INTO adempiere.ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, isreadwrite, ad_window_access_uu, bh_candeactivate) VALUES ((SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), (SELECT ad_role_id FROM AD_Role WHERE ad_role_uu='461b31c5-cae2-449d-8a0c-7385b12f4685'), 0, 0, 'Y', '2022-07-18 21:45:58.222000', 100, '2022-07-18 21:45:58.222000', 100, 'Y', 'debd8a8c-69bb-4ebf-9769-1f96d5d0e432', 'Y') ON CONFLICT DO NOTHING;
 
--- Add Track Incomes window access to Cashier/Registration Basic role
+-- Add Track Income window access to Cashier/Registration Basic role
 INSERT INTO adempiere.ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, isreadwrite, ad_window_access_uu, bh_candeactivate) VALUES ((SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), (SELECT ad_role_id FROM AD_Role WHERE ad_role_uu='09eb7fc8-9cc5-44b0-9d14-15258a066038'), 0, 0, 'Y', '2022-07-18 21:45:58.222000', 100, '2022-07-18 21:45:58.222000', 100, 'Y', '006a5bdd-ae86-498a-afad-a29ac41a2598', 'Y') ON CONFLICT DO NOTHING;
 
--- Add Track Incomes window access to Accounting role
+-- Add Track Income window access to Accounting role
 INSERT INTO adempiere.ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, isreadwrite, ad_window_access_uu, bh_candeactivate) VALUES ((SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), (SELECT ad_role_id FROM AD_Role WHERE ad_role_uu='93365778-a2d9-433b-b962-87fb150db4fa'), 0, 0, 'Y', '2022-07-18 21:45:58.222000', 100, '2022-07-18 21:45:58.222000', 100, 'Y', '0eb6fc09-56e2-48e0-89ce-4a6ee39a10ec', 'Y') ON CONFLICT DO NOTHING;
 
 -- Add Income Categories window access to Clinic Admin Role
@@ -152,7 +152,7 @@ CREATE TEMP TABLE tmp_ad_window_access
     bh_candeactivate		char	    default 'N'	 not null
 );
 
--- Track Incomes
+-- Track Income
 INSERT INTO tmp_ad_window_access (ad_window_id, ad_role_id, ad_client_id, ad_org_id) SELECT (SELECT ad_window_id FROM AD_Window WHERE ad_window_uu='44c02ddc-ef83-4020-8e4c-709d8cbeadc2'), ad_role_id, ad_client_id, ad_org_id FROM ad_role WHERE ad_client_id > 999999 and ismanual = 'N' ON CONFLICT DO NOTHING;
 
 -- Income Categories
