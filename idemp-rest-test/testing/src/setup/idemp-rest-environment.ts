cddd1eb8fdfe80c078deb1beea418ca1c2e6ae67
Kevin <kwburnett@gmail.com>
2022-08-11 14:16:04 -0500

    test: ensure value object unique per test

--- idemp-rest-test/testing/src/setup/idemp-rest-environment.ts
+++ idemp-rest-test/testing/src/setup/idemp-rest-environment.ts
@@ -1,12 +1,5 @@
 import { EnvironmentContext, JestEnvironmentConfig } from '@jest/environment';
-import { readFile } from 'fs/promises';
 import NodeEnvironment from 'jest-environment-node';
-import { tmpdir } from 'os';
-import { join } from 'path';
-import { ValueObject } from '../models';
-import { AuthResponse, Client } from '../types/org.bandahealth.idempiere.rest';
-
-const workingDirectory = join(tmpdir(), 'idemp-rest-global-setup');
 
 export default class IDempRestEnvironment extends NodeEnvironment {
 	constructor(config: JestEnvironmentConfig, context: EnvironmentContext) {
@@ -15,16 +8,6 @@ export default class IDempRestEnvironment extends NodeEnvironment {
 
 	async setup() {
 		await super.setup();
-		// get the wsEndpoint
-		const stringifiedLoginInfo = await readFile(join(workingDirectory, 'loginInfo'), 'utf8');
-		if (!stringifiedLoginInfo) {
-			throw new Error('login info not found');
-		}
-
-		// connect to puppeteer
-		this.global.__VALUE_OBJECT__ = new ValueObject(
-			JSON.parse(stringifiedLoginInfo) as AuthResponse & { client: Client },
-		);
 	}
 
 	async teardown() {
