50d61e540d36d00d8ec6304e8b9383372fea5437
Clinton Ingalia <cingalia@bandahealth.org>
2022-09-09 09:17:43 +0300

    Terminated statement with semicolon

--- local_sql/postgresql/202209081408_GO-2376.sql
+++ local_sql/postgresql/202209081408_GO-2376.sql
@@ -1,5 +1,4 @@
 -- Update all isdisallownegativeinv column to yes
-update m_warehouse
-set isdisallownegativeinv = 'Y'
+update m_warehouse set isdisallownegativeinv = 'Y';
 
 SELECT register_migration_script('202209081408_GO-2376.sql') FROM dual;
