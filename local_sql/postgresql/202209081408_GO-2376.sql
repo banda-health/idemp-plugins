a248bc9df9a49eaa11bf1c93ca16f3118410961a
Clinton Ingalia <cingalia@bandahealth.org>
2022-09-09 09:33:43 +0300

    Excluded Garden World and System

--- local_sql/postgresql/202209081408_GO-2376.sql
+++ local_sql/postgresql/202209081408_GO-2376.sql
@@ -1,4 +1,4 @@
--- Update all isdisallownegativeinv column to yes
-update m_warehouse set isdisallownegativeinv = 'Y';
+-- Update all isdisallownegativeinv column to yes except Garden World and System
+update m_warehouse set isdisallownegativeinv = 'Y' where ad_client_id not in (0, 11);
 
 SELECT register_migration_script('202209081408_GO-2376.sql') FROM dual;
