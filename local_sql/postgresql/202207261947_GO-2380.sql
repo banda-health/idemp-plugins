5b62f62cc4ec0064d5d654439c74403a72424afb
Kevin <kwburnett@gmail.com>
2022-09-12 10:05:52 -0500

    feat: add periods and disable the scheduler

--- local_sql/postgresql/202207261947_GO-2380.sql
+++ local_sql/postgresql/202207261947_GO-2380.sql
@@ -9,20 +9,37 @@ WHERE
 -- Get the current orders that are in an error state
 DROP TABLE IF EXISTS tmp_errored_order_ids;
 SELECT
-	o.c_order_id
+	c_order_id
 INTO TEMP TABLE
 	tmp_errored_order_ids
 FROM
-	c_order o
-		LEFT JOIN c_invoice i
-			ON o.c_order_id = i.c_order_id
-		LEFT JOIN c_payment p
-			ON p.bh_c_order_id = o.c_order_id
+	c_order
 WHERE
-	o.docstatus NOT IN ('CO', 'VO', 'CL')
-	AND o.issotrx = 'Y'
-	AND (i.docstatus != 'RE' OR
-	     ((p.bh_processing = 'Y' AND p.docstatus != 'RE') OR p.docstatus = 'CO'));
+		c_order_id IN (
+		SELECT
+			o.c_order_id
+		FROM
+			c_order o
+				LEFT JOIN c_invoice i
+					ON o.c_order_id = i.c_order_id AND i.docstatus NOT IN ('RE', 'RA', 'VO')
+				LEFT JOIN c_allocationline al
+					ON i.c_invoice_id = al.c_invoice_id
+				LEFT JOIN c_allocationhdr ah
+					ON al.c_allocationhdr_id = ah.c_allocationhdr_id
+				LEFT JOIN c_payment p
+					ON (p.bh_c_order_id = o.c_order_id OR p.c_payment_id = al.c_payment_id) AND p.docstatus != 'RE'
+		WHERE
+			o.issotrx = 'Y'
+			AND (ah.docstatus IS NULL OR ah.docstatus NOT IN ('RA', 'RC'))
+			AND (
+				(
+							o.docstatus IN ('CO', 'CL') AND (
+								i.docstatus NOT IN ('CO', 'CL') OR
+								(p.docstatus IS NOT NULL AND p.docstatus NOT IN ('CO', 'CL'))
+						)
+					)
+				OR (o.docstatus NOT IN ('CO', 'VO', 'CL', 'IN') AND (p.bh_processing = 'Y' OR p.docstatus = 'CO')))
+	);
 
 -- Make sure the errored orders now have the correct payment rule
 UPDATE c_order o
@@ -82,7 +99,532 @@ WHERE
 			tmp_orders_with_no_lines
 	);
 
--- Reset all open balances to match what the system calculates
--- For any open balances that differ from what the system calculates, we need to figure out why and correct stuff
+-- Update payments on bad orders with wrong transaction dates to have the correct transaction date
+UPDATE c_payment p
+SET
+	dateacct = i.dateacct
+FROM
+	c_invoice i
+WHERE
+	i.c_order_id = p.bh_c_order_id
+	AND i.dateacct > p.dateacct
+	AND bh_c_order_id IN (
+	SELECT
+		o.c_order_id
+	FROM
+		c_order o
+			LEFT JOIN c_invoice i
+				ON o.c_order_id = i.c_order_id AND i.docstatus != 'RE'
+			LEFT JOIN c_allocationline al
+				ON i.c_invoice_id = al.c_invoice_id
+			LEFT JOIN c_payment p
+				ON (p.bh_c_order_id = o.c_order_id OR p.c_payment_id = al.c_payment_id) AND p.docstatus != 'RE'
+	WHERE
+		o.issotrx = 'Y'
+		AND (
+			(
+						o.docstatus IN ('CO', 'CL') AND (
+							i.docstatus NOT IN ('CO', 'CL') OR
+							(p.docstatus IS NOT NULL AND p.docstatus NOT IN ('CO', 'CL'))
+					)
+				)
+			OR (o.docstatus NOT IN ('CO', 'VO', 'CL', 'IN') AND (p.bh_processing = 'Y' OR p.docstatus = 'CO')))
+);
 
-SELECT register_migration_script('202207261947_GO-2380.sql') FROM dual;
+-- Update any invoices with no lines, but are linked to completed orders and are an allocation line
+DROP TABLE IF EXISTS tmp_bad_invoice_ids;
+
+SELECT
+	i.c_invoice_id
+INTO TEMP TABLE
+	tmp_bad_invoice_ids
+FROM
+	c_invoice i
+		JOIN c_order o
+			ON i.c_order_id = o.c_order_id
+		LEFT JOIN c_invoiceline il
+			ON i.c_invoice_id = il.c_invoice_id
+		JOIN c_allocationline al
+			ON i.c_invoice_id = al.c_invoice_id
+WHERE
+	o.docstatus = 'CO'
+	AND i.docstatus NOT IN ('CO', 'VO', 'CL', 'IN')
+	AND il.c_invoice_id IS NULL;
+
+UPDATE c_invoice i
+SET
+	issotrx     = o.issotrx,
+	paymentrule = o.paymentrule,
+	grandtotal  = o.grandtotal
+FROM
+	c_order o
+WHERE
+	o.c_order_id = i.c_order_id
+	AND i.c_invoice_id IN (
+	SELECT
+		c_invoice_id
+	FROM
+		tmp_bad_invoice_ids
+);
+
+DROP TABLE IF EXISTS tmp_c_invoiceline;
+CREATE TEMP TABLE tmp_c_invoiceline
+(
+	c_invoiceline_id          serial                          NOT NULL,
+	ad_client_id              numeric(10)                     NOT NULL,
+	ad_org_id                 numeric(10)                     NOT NULL,
+	isactive                  char        DEFAULT 'Y'::bpchar NOT NULL,
+	created                   timestamp   DEFAULT NOW()       NOT NULL,
+	createdby                 numeric(10)                     NOT NULL,
+	updated                   timestamp   DEFAULT NOW()       NOT NULL,
+	updatedby                 numeric(10)                     NOT NULL,
+	c_invoice_id              numeric(10)                     NOT NULL,
+	c_orderline_id            numeric(10),
+	m_inoutline_id            numeric(10),
+	line                      numeric(10)                     NOT NULL,
+	description               varchar(255),
+	m_product_id              numeric(10),
+	qtyinvoiced               numeric     DEFAULT 0           NOT NULL,
+	pricelist                 numeric     DEFAULT 0           NOT NULL,
+	priceactual               numeric     DEFAULT 0           NOT NULL,
+	pricelimit                numeric     DEFAULT 0           NOT NULL,
+	linenetamt                numeric     DEFAULT 0           NOT NULL,
+	c_charge_id               numeric(10),
+	c_uom_id                  numeric(10),
+	c_tax_id                  numeric(10),
+	s_resourceassignment_id   numeric(10),
+	a_asset_id                numeric(10),
+	taxamt                    numeric     DEFAULT 0,
+	m_attributesetinstance_id numeric(10) DEFAULT 0,
+	isdescription             char        DEFAULT 'N'::bpchar NOT NULL,
+	isprinted                 char        DEFAULT 'Y'::bpchar NOT NULL,
+	linetotalamt              numeric     DEFAULT 0,
+	ref_invoiceline_id        numeric(10),
+	processed                 char        DEFAULT 'N'::bpchar NOT NULL,
+	qtyentered                numeric                         NOT NULL,
+	priceentered              numeric                         NOT NULL,
+	c_project_id              numeric(10),
+	c_projectphase_id         numeric(10),
+	c_projecttask_id          numeric(10),
+	rrstartdate               timestamp,
+	rramt                     numeric,
+	c_campaign_id             numeric(10),
+	c_activity_id             numeric(10),
+	user1_id                  numeric(10),
+	user2_id                  numeric(10),
+	ad_orgtrx_id              numeric(10),
+	m_rmaline_id              numeric(10),
+	a_createasset             char        DEFAULT 'N'::bpchar,
+	a_processed               char        DEFAULT 'N'::bpchar,
+	a_capvsexp                varchar(3),
+	a_asset_group_id          numeric(10),
+	c_invoiceline_uu          uuid        DEFAULT uuid_generate_v4(),
+	isfixedassetinvoice       char,
+	c_1099box_id              numeric(10) DEFAULT NULL::numeric
+);
+
+SELECT
+	SETVAL(
+			'tmp_c_invoiceline_c_invoiceline_id_seq',
+			(
+				SELECT
+					currentnext
+				FROM
+					ad_sequence
+				WHERE
+					name = 'C_InvoiceLine'
+				LIMIT 1
+			)::INT,
+			FALSE
+		);
+
+INSERT INTO
+	tmp_c_invoiceline (ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, c_invoice_id,
+	                   m_inoutline_id, line, description, m_product_id, qtyinvoiced, pricelist,
+	                   priceactual, pricelimit, linenetamt, c_charge_id, c_uom_id, c_tax_id, s_resourceassignment_id,
+	                   a_asset_id, taxamt, m_attributesetinstance_id, isdescription, isprinted, linetotalamt,
+	                   ref_invoiceline_id, processed, qtyentered, priceentered, c_project_id, c_projectphase_id,
+	                   c_projecttask_id, rrstartdate, rramt, c_campaign_id, c_activity_id, user1_id, user2_id,
+	                   ad_orgtrx_id, m_rmaline_id, a_createasset, a_processed, a_capvsexp, a_asset_group_id,
+	                   isfixedassetinvoice, c_1099box_id)
+SELECT
+	ol.ad_client_id,
+	ol.ad_org_id,
+	ol.isactive,
+	ol.created,
+	ol.createdby,
+	ol.updated,
+	ol.updatedby,
+	i.c_invoice_id,
+	iol.m_inoutline_id,
+	ol.line,
+	ol.description,
+	ol.m_product_id,
+	qtyinvoiced,
+	pricelist,
+	priceactual,
+	pricelimit,
+	linenetamt,
+	ol.c_charge_id,
+	ol.c_uom_id,
+	c_tax_id,
+	s_resourceassignment_id,
+	NULL,          -- a_asset_id
+	0,             -- taxamt
+	ol.m_attributesetinstance_id,
+	ol.isdescription,
+	i.isprinted,
+	ol.linenetamt, -- linetotalamt
+	NULL,          -- ref_invoiceline_id
+	'N',           -- processed
+	ol.qtyentered,
+	priceentered,
+	ol.c_project_id,
+	ol.c_projectphase_id,
+	ol.c_projecttask_id,
+	rrstartdate,
+	rramt,
+	ol.c_campaign_id,
+	ol.c_activity_id,
+	ol.user1_id,
+	ol.user2_id,
+	ol.ad_orgtrx_id,
+	m_rmaline_id,
+	'N',           -- a_createasset
+	'N',           -- a_processed
+	NULL,          -- a_capvsexp
+	NULL,          -- a_asset_group_id
+	isfixedassetinvoice,
+	NULL           -- c_1099box_id
+FROM
+	c_order o
+		JOIN c_invoice i
+			ON o.c_order_id = i.c_order_id
+		JOIN tmp_bad_invoice_ids bii
+			ON i.c_invoice_id = bii.c_invoice_id
+		JOIN c_orderline ol
+			ON o.c_order_id = ol.c_order_id
+		LEFT JOIN m_inoutline iol
+			ON ol.c_orderline_id = iol.c_orderline_id;
+
+INSERT INTO
+	c_invoiceline (c_invoiceline_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby,
+	               c_invoice_id, c_orderline_id, m_inoutline_id, line, description, m_product_id, qtyinvoiced, pricelist,
+	               priceactual, pricelimit, linenetamt, c_charge_id, c_uom_id, c_tax_id, s_resourceassignment_id,
+	               a_asset_id, taxamt, m_attributesetinstance_id, isdescription, isprinted, linetotalamt,
+	               ref_invoiceline_id, processed, qtyentered, priceentered, c_project_id, c_projectphase_id,
+	               c_projecttask_id, rrstartdate, rramt, c_campaign_id, c_activity_id, user1_id, user2_id, ad_orgtrx_id,
+	               m_rmaline_id, a_createasset, a_processed, a_capvsexp, a_asset_group_id, c_invoiceline_uu,
+	               isfixedassetinvoice, c_1099box_id)
+SELECT
+	c_invoiceline_id,
+	ad_client_id,
+	ad_org_id,
+	isactive,
+	created,
+	createdby,
+	updated,
+	updatedby,
+	c_invoice_id,
+	c_orderline_id,
+	m_inoutline_id,
+	line,
+	description,
+	m_product_id,
+	qtyinvoiced,
+	pricelist,
+	priceactual,
+	pricelimit,
+	linenetamt,
+	c_charge_id,
+	c_uom_id,
+	c_tax_id,
+	s_resourceassignment_id,
+	a_asset_id,
+	taxamt,
+	m_attributesetinstance_id,
+	isdescription,
+	isprinted,
+	linetotalamt,
+	ref_invoiceline_id,
+	processed,
+	qtyentered,
+	priceentered,
+	c_project_id,
+	c_projectphase_id,
+	c_projecttask_id,
+	rrstartdate,
+	rramt,
+	c_campaign_id,
+	c_activity_id,
+	user1_id,
+	user2_id,
+	ad_orgtrx_id,
+	m_rmaline_id,
+	a_createasset,
+	a_processed,
+	a_capvsexp,
+	a_asset_group_id,
+	c_invoiceline_uu,
+	isfixedassetinvoice,
+	c_1099box_id
+FROM
+	tmp_c_invoiceline;
+
+-- Make sure periods exist for any errored order (or associated invoice/payment)
+-- Get all the accounting date information we'll need for the various tables
+DROP TABLE IF EXISTS tmp_account_dates;
+SELECT DISTINCT
+	ad_client_id,
+	EXTRACT(YEAR FROM dateacct)                                                 AS calendar_year,
+	EXTRACT(MONTH FROM dateacct)                                                AS calendar_month,
+	TO_CHAR(dateacct, 'Mon-YY')                                                 AS name,
+	DATE_TRUNC('month', dateacct)                                               AS startdate,
+			DATE_TRUNC('month', dateacct) + '1 month'::interval - '1 day'::interval AS enddate
+INTO TEMP TABLE
+	tmp_account_dates
+FROM
+	(
+-- Insert years for all errored orders
+		SELECT
+			o.ad_client_id,
+			o.dateacct
+		FROM
+			c_order o
+				JOIN tmp_errored_order_ids teoi
+					ON teoi.c_order_id = o.c_order_id
+				JOIN c_calendar c
+					ON c.ad_client_id = o.ad_client_id
+		UNION
+-- Insert years for all invoices associated with errored orders
+		SELECT
+			i.ad_client_id,
+			i.dateacct
+		FROM
+			c_invoice i
+				JOIN tmp_errored_order_ids teoi
+					ON teoi.c_order_id = i.c_order_id
+				JOIN c_calendar c
+					ON c.ad_client_id = i.ad_client_id
+		UNION
+-- Insert years for all payments associated with errored orders
+		SELECT
+			p.ad_client_id,
+			p.dateacct
+		FROM
+			c_order o
+				JOIN tmp_errored_order_ids teoi
+					ON teoi.c_order_id = o.c_order_id
+				JOIN c_calendar c
+					ON c.ad_client_id = o.ad_client_id
+				LEFT JOIN c_invoice i
+					ON o.c_order_id = i.c_order_id
+				LEFT JOIN c_allocationline al
+					ON i.c_invoice_id = al.c_invoice_id
+				LEFT JOIN c_payment p
+					ON al.c_payment_id = p.c_payment_id OR p.bh_c_order_id = o.c_order_id
+	) p;
+
+-- First create any years that don't yet exist
+DROP TABLE IF EXISTS tmp_c_year;
+CREATE TEMP TABLE tmp_c_year
+(
+	c_year_id     serial                                  NOT NULL,
+	ad_client_id  numeric(10)                             NOT NULL,
+	ad_org_id     numeric(10)  DEFAULT 0                  NOT NULL,
+	isactive      char         DEFAULT 'Y'::bpchar        NOT NULL,
+	created       timestamp    DEFAULT NOW()              NOT NULL,
+	createdby     numeric(10)  DEFAULT 100                NOT NULL,
+	updated       timestamp    DEFAULT NOW()              NOT NULL,
+	updatedby     numeric(10)  DEFAULT 100                NOT NULL,
+	fiscalyear    varchar(10)                             NOT NULL,
+	description   varchar(255) DEFAULT 'N',
+	c_calendar_id numeric(10)                             NOT NULL,
+	processing    char         DEFAULT 'N',
+	c_year_uu     uuid         DEFAULT uuid_generate_v4() NOT NULL
+);
+
+SELECT
+	SETVAL(
+			'tmp_c_year_c_year_id_seq',
+			(
+				SELECT
+					currentnext
+				FROM
+					ad_sequence
+				WHERE
+					name = 'C_Year'
+				LIMIT 1
+			)::INT,
+			FALSE
+		);
+
+-- Insert years for all errored orders
+INSERT INTO
+	tmp_c_year (ad_client_id, fiscalyear, c_calendar_id)
+SELECT
+	ad.ad_client_id,
+	ad.calendar_year,
+	c.c_calendar_id
+FROM
+	tmp_account_dates ad
+		JOIN c_calendar c
+			ON c.ad_client_id = ad.ad_client_id
+		LEFT JOIN c_year y
+			ON c.c_calendar_id = y.c_calendar_id AND y.fiscalyear = ad.calendar_year::varchar
+WHERE
+	y.c_year_id IS NULL;
+
+INSERT INTO
+	c_year (c_year_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, fiscalyear, description,
+	        c_calendar_id, processing, c_year_uu)
+SELECT
+	c_year_id,
+	ad_client_id,
+	ad_org_id,
+	isactive,
+	created,
+	createdby,
+	updated,
+	updatedby,
+	fiscalyear,
+	description,
+	c_calendar_id,
+	processing,
+	c_year_uu
+FROM
+	tmp_c_year
+ON CONFLICT DO NOTHING;
+
+-- Now handle the periods
+DROP TABLE IF EXISTS tmp_c_period;
+CREATE TEMP TABLE IF NOT EXISTS tmp_c_period
+(
+	c_period_id  serial                          NOT NULL,
+	ad_client_id numeric(10)                     NOT NULL,
+	ad_org_id    numeric(10) DEFAULT 0           NOT NULL,
+	isactive     char        DEFAULT 'Y'::bpchar NOT NULL,
+	created      timestamp   DEFAULT NOW()       NOT NULL,
+	createdby    numeric(10) DEFAULT 100         NOT NULL,
+	updated      timestamp   DEFAULT NOW()       NOT NULL,
+	updatedby    numeric(10) DEFAULT 100         NOT NULL,
+	name         varchar(60)                     NOT NULL,
+	periodno     numeric(10)                     NOT NULL,
+	c_year_id    numeric(10)                     NOT NULL,
+	startdate    timestamp                       NOT NULL,
+	enddate      timestamp,
+	periodtype   char        DEFAULT 'S'         NOT NULL,
+	processing   char        DEFAULT 'N',
+	c_period_uu  uuid        DEFAULT uuid_generate_v4()
+);
+
+INSERT INTO
+	tmp_c_period (ad_client_id, name, periodno, c_year_id, startdate, enddate)
+SELECT
+	ad.ad_client_id,
+	ad.name,
+	ad.calendar_month,
+	ty.c_year_id,
+	ad.startdate,
+	ad.enddate
+FROM
+	tmp_account_dates ad
+		JOIN tmp_c_year ty
+			ON ty.fiscalyear = ad.calendar_year::varchar AND ty.ad_client_id = ad.ad_client_id;
+
+INSERT INTO
+	c_period (c_period_id, ad_client_id, ad_org_id, createdby, updatedby, name, periodno, c_year_id, startdate, enddate,
+	          periodtype, processing)
+SELECT
+	c_period_id,
+	ad_client_id,
+	ad_org_id,
+	createdby,
+	updatedby,
+	name,
+	periodno,
+	c_year_id,
+	startdate,
+	enddate,
+	periodtype,
+	processing
+FROM
+	tmp_c_period;
+
+-- Lastly, insert the period controls
+DROP TABLE IF EXISTS tmp_c_periodcontrol;
+CREATE TEMP TABLE tmp_c_periodcontrol
+(
+	c_periodcontrol_id serial                          NOT NULL,
+	ad_client_id       numeric(10)                     NOT NULL,
+	ad_org_id          numeric(10) DEFAULT 0           NOT NULL,
+	isactive           char        DEFAULT 'Y'::bpchar NOT NULL,
+	created            timestamp   DEFAULT NOW()       NOT NULL,
+	createdby          numeric(10) DEFAULT 100         NOT NULL,
+	updated            timestamp   DEFAULT NOW()       NOT NULL,
+	updatedby          numeric(10) DEFAULT 100         NOT NULL,
+	c_period_id        numeric(10)                     NOT NULL,
+	docbasetype        char(3)                         NOT NULL,
+	periodstatus       char        DEFAULT 'O',
+	periodaction       char        DEFAULT 'N'         NOT NULL,
+	processing         char        DEFAULT 'N',
+	c_periodcontrol_uu uuid        DEFAULT uuid_generate_v4()
+);
+
+INSERT INTO
+	tmp_c_periodcontrol (ad_client_id, c_period_id, docbasetype)
+SELECT
+	tp.ad_client_id,
+	tp.c_period_id,
+	dt.docbasetype
+FROM
+	tmp_c_period tp
+		JOIN (
+		SELECT DISTINCT
+			ad_client_id,
+			docbasetype
+		FROM
+			c_doctype
+	) dt
+			ON tp.ad_client_id = dt.ad_client_id;
+
+INSERT INTO
+	c_periodcontrol (c_periodcontrol_id, ad_client_id, ad_org_id, createdby, updatedby, c_period_id, docbasetype,
+	                 periodstatus, periodaction, processing)
+SELECT
+	c_periodcontrol_id,
+	ad_client_id,
+	ad_org_id,
+	createdby,
+	updatedby,
+	c_period_id,
+	docbasetype,
+	periodstatus,
+	periodaction,
+	processing
+FROM
+	tmp_c_periodcontrol;
+
+-- Set the compoletion process to not run at the same time
+UPDATE ad_process
+SET
+	allowmultipleexecution = 'N'
+WHERE
+	ad_process_uu = '1d5191dd-4792-464f-94c5-5b4d652e5fe5';
+
+-- Disable the completion scheduler so we can run it manually (to update a lot of old data)
+UPDATE ad_scheduler
+SET
+	isactive = 'N'
+WHERE
+		ad_process_id = (
+		SELECT ad_process_id FROM ad_process WHERE ad_process_uu = '1d5191dd-4792-464f-94c5-5b4d652e5fe5'
+	);
+
+SELECT
+	update_sequences();
+
+SELECT
+	register_migration_script('202207261947_GO-2380.sql')
+FROM
+	dual;
