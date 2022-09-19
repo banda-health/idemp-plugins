19e35223787d8fac9b298c7da4211e509e3855bc
Kevin <kwburnett@gmail.com>
2022-09-19 10:56:56 -0500

    test: report changes

--- idemp-reports-test/src/org/bandahealth/idempiere/report/test/InventoryQuantityReportTest.java
+++ idemp-reports-test/src/org/bandahealth/idempiere/report/test/InventoryQuantityReportTest.java
@@ -20,15 +20,20 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InventoryQuantityReportTest extends ChuBoePopulateFactoryVO {
+	private final String reportUuid = "93d7c1bc-2885-43f4-985f-90f57a414e5f";
+
 	@IPopulateAnnotation.CanRunBeforeClass
 	public void prepareIt() throws Exception {
 		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
@@ -77,7 +82,7 @@ public class InventoryQuantityReportTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Generate the report");
-		valueObject.setProcessUuid("93d7c1bc-2885-43f4-985f-90f57a414e5f");
+		valueObject.setProcessUuid(reportUuid);
 		valueObject.setProcessRecordId(0);
 		valueObject.setProcessTableId(0);
 		valueObject.setProcessInformationParameters(Arrays.asList(
@@ -104,6 +109,79 @@ public class InventoryQuantityReportTest extends ChuBoePopulateFactoryVO {
 					is(40.0D));
 			assertThat("Balanced stock for this product is correct", productRow.get().getCell(5).getNumericCellValue(),
 					is(30.0D));
+		}
+	}
+
+	@IPopulateAnnotation.CanRun
+	public void duplicateProductQuantityDoesntAppearWhenSellingOnDifferentDays() throws SQLException, IOException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		valueObject.setStepName("Create business partner");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create product");
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create purchase order");
+		valueObject.setDateOffset(-1);
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		valueObject.setQuantity(new BigDecimal(10));
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Adjust inventory");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setQuantity(new BigDecimal(50));
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_MaterialPhysicalInventory, null, false, false, false);
+		ChuBoeCreateEntity.createInventory(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create first sales order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		valueObject.setQuantity(new BigDecimal(10));
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create second sales order");
+		valueObject.setDateOffset(1);
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		valueObject.setQuantity(new BigDecimal(10));
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid(reportUuid);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", TimestampUtils.lastMonth(), null, null, null),
+				new ProcessInfoParameter("End Date", new Timestamp(System.currentTimeMillis()), null, null, null)
+		));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			List<Row> productRows = StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(0) != null &&
+							row.getCell(0).getStringCellValue().equalsIgnoreCase(valueObject.getProduct().getName()))
+					.collect(Collectors.toList());
+			assertEquals(1, productRows.size(), "Only one row is returned");
+			Row productRow = productRows.get(0);
+			assertThat("Opening stock for this product is correct", productRow.getCell(1).getNumericCellValue(), is(0.0D));
+			assertThat("Received stock for this product is correct", productRow.getCell(2).getNumericCellValue(), is(10.0D));
+			assertThat("Sold stock for this product is correct", productRow.getCell(3).getNumericCellValue(), is(20.0D));
+			assertThat("Balanced stock for this product is correct", productRow.getCell(4).getNumericCellValue(), is(40.0D));
+			assertThat("Balanced stock for this product is correct", productRow.getCell(5).getNumericCellValue(), is(30.0D));
 		}
 	}
 }
--- idemp-reports-test/src/org/bandahealth/idempiere/report/test/InventorySoldReportTest.java
+++ idemp-reports-test/src/org/bandahealth/idempiere/report/test/InventorySoldReportTest.java
@@ -20,15 +20,20 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InventorySoldReportTest extends ChuBoePopulateFactoryVO {
+	private final String reportUuid = "1211e173-6f12-4e2f-bfcc-d43d48af51c3";
+
 	@IPopulateAnnotation.CanRunBeforeClass
 	public void prepareIt() throws Exception {
 		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
@@ -71,7 +76,7 @@ public class InventorySoldReportTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Generate the report");
-		valueObject.setProcessUuid("1211e173-6f12-4e2f-bfcc-d43d48af51c3");
+		valueObject.setProcessUuid(reportUuid);
 		valueObject.setProcessRecordId(0);
 		valueObject.setProcessTableId(0);
 		valueObject.setProcessInformationParameters(Arrays.asList(
@@ -99,8 +104,84 @@ public class InventorySoldReportTest extends ChuBoePopulateFactoryVO {
 			assertThat("COGS for this product is correct", productRow.get().getCell(5).getNumericCellValue(),
 					is(valueObject.getPurchaseStandardPrice().doubleValue() * quantitySold.doubleValue()));
 			assertThat("Gross profit for this product is correct", productRow.get().getCell(6).getNumericCellValue(),
-					is((valueObject.getSalesStandardPrice().doubleValue() - valueObject.getPurchaseStandardPrice().doubleValue()) *
+					is((valueObject.getSalesStandardPrice().doubleValue() -
+							valueObject.getPurchaseStandardPrice().doubleValue()) *
 							quantitySold.doubleValue()));
+		}
+	}
+
+	@IPopulateAnnotation.CanRun
+	public void duplicateProductQuantityDoesntAppearWhenSellingOnDifferentDays() throws SQLException, IOException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		valueObject.setStepName("Create business partner");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create product");
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create purchase order");
+		valueObject.setDateOffset(-1);
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		valueObject.setQuantity(new BigDecimal(30));
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create first sales order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		BigDecimal quantitySold = new BigDecimal(5);
+		valueObject.setQuantity(quantitySold);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create second sales order");
+		valueObject.setDateOffset(1);
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		valueObject.setQuantity(quantitySold);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid(reportUuid);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", TimestampUtils.lastMonth(), null, null, null),
+				new ProcessInfoParameter("End Date", new Timestamp(System.currentTimeMillis()), null, null, null)
+		));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			List<Row> productRows = StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(0) != null &&
+							row.getCell(0).getStringCellValue().equalsIgnoreCase(valueObject.getProduct().getName()))
+					.collect(Collectors.toList());
+			double totalQuantitySold = quantitySold.doubleValue() * 2;
+			assertEquals(1, productRows.size(), "Only one row is returned");
+			assertThat("Quantity sold for this product is correct", productRows.get(0).getCell(1).getNumericCellValue(),
+					is(totalQuantitySold));
+			assertThat("Selling price for this product is correct", productRows.get(0).getCell(2).getNumericCellValue(),
+					is(valueObject.getSalesStandardPrice().doubleValue()));
+			assertThat("Income for this product is correct", productRows.get(0).getCell(3).getNumericCellValue(),
+					is(totalQuantitySold * valueObject.getSalesStandardPrice().doubleValue()));
+			assertThat("Purchase price for this product is correct", productRows.get(0).getCell(4).getNumericCellValue(),
+					is(valueObject.getSalesStandardPrice().doubleValue()));
+			assertThat("COGS for this product is correct", productRows.get(0).getCell(5).getNumericCellValue(),
+					is(valueObject.getPurchaseStandardPrice().doubleValue() * totalQuantitySold));
+			assertThat("Gross profit for this product is correct", productRows.get(0).getCell(6).getNumericCellValue(),
+					is((valueObject.getSalesStandardPrice().doubleValue() -
+							valueObject.getPurchaseStandardPrice().doubleValue()) * totalQuantitySold));
 		}
 	}
 }
