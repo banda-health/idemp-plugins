33dbb0363104f057adc3a15d8bf5ca6c1f6dd648
Kevin <kwburnett@gmail.com>
2022-09-26 19:39:20 -0500

    test: ensure tests pass

--- idemp-base/src/org/bandahealth/idempiere/base/model/MRole_BH.java
+++ idemp-base/src/org/bandahealth/idempiere/base/model/MRole_BH.java
@@ -5,5 +5,5 @@ public class MRole_BH {
 	public static Integer SYSTEM_ROLE_ID = 0;
 	public static final String CLINICIAN_ADVANCED_UU = "c54253cf-c86b-4aaa-b472-ed8880635c62";
 	public static final String CLINICIAN_BASIC_UU = "98617c31-55ff-48f9-bd44-253ef323d960";
-
+	public static final String MUST_HAVES_UU = "baec9412-d994-4313-815c-31332357863a";
 }
