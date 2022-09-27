6d337c5ce6ebab546b356eb228316823b1706c34
Clinton Ingalia <cingalia@bandahealth.org>
2022-09-27 12:37:34 +0300

    Inventory Test

--- idemp-rest-test/testing/src/tests/inventory.test.ts
+++ idemp-rest-test/testing/src/tests/inventory.test.ts
@@ -1,2 +1,17 @@
-import { productApi } from '../api';
-import { visitApi } from '../api';
+import { patientApi, referenceListApi, visitApi, productApi } from '../api';
+import { referenceUuid, tenderTypeName } from '../models';
+import { Payment, PaymentType, Visit } from '../types/org.bandahealth.idempiere.rest';
+import { createPatient, createProduct, createVisit, waitForVisitToComplete, createProductInventory } from '../utils';
+
+test(`Product Created with negative value`, async () => {
+	const valueObject = globalThis.__VALUE_OBJECT__;
+	await valueObject.login();
+
+	valueObject.stepName = 'Create product with negative value';
+	valueObject.salesStandardPrice = 100;
+    valueObject.quantity = -100;
+	await createProductInventory(valueObject);
+
+	expect(valueObject.product?.totalQuantity).toBe(0);
+
+});
