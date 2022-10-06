8dc0e7c6773845eae4959267edb5a48d95e02496
Clinton Ingalia <cingalia@bandahealth.org>
2022-10-06 09:15:20 +0300

    Added UpdateExpiredStockProcess class

---
+++ idemp-base/src/org/bandahealth/idempiere/base/process/UpdateExpiredStockProcess.java
@@ -0,0 +1,32 @@
+package org.bandahealth.idempiere.base.process;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.compiere.model.MStorageOnHand;
+import org.compiere.model.Query;
+import org.compiere.process.SvrProcess;
+
+/**
+ * This process resets expired stocks to zero for a given client
+ * 
+ * @author clinton
+ *
+ */
+
+public class UpdateExpiredStockProcess extends SvrProcess {
+
+    @Override
+    protected void prepare() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected String doIt() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
