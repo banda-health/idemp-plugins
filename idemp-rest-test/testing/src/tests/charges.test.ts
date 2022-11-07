38b5592ef3c827da48bcec6590e0e457245e9ac6
Andrew Moko <amoko@bandahealth.org>
2022-11-07 11:31:10 +0300

    code review - add test

--- idemp-rest-test/testing/src/tests/charges.test.ts
+++ idemp-rest-test/testing/src/tests/charges.test.ts
@@ -1,5 +1,6 @@
+import { expenseCategoryApi } from 'src/api/expenseCategories';
 import { accountApi, chargeApi, chargeTypeApi } from '../api';
-import { Account, Charge } from '../types/org.bandahealth.idempiere.rest';
+import { Account, Charge, ExpenseCategory } from '../types/org.bandahealth.idempiere.rest';
 import { createCharge } from '../utils';
 
 test('charge creation', async () => {
@@ -42,4 +43,26 @@ test('account mapping can be set up through a charge', async () => {
 
 	expect(savedCharge.chargeType.uuid).toBe(charge.chargeType?.uuid);
 	expect(savedCharge.account.uuid).toBe(charge.account?.uuid);
+});
+
+test('save expense category', async () => {
+	const valueObject = globalThis.__VALUE_OBJECT__;
+	await valueObject.login();
+
+	const doNotChangeAccount = (
+		await accountApi.get(valueObject, undefined, undefined, undefined, JSON.stringify({ value: '99999' }))
+	).results[0];
+	expect(doNotChangeAccount).toBeTruthy();
+
+	const expenseCategory: Partial<ExpenseCategory> = {
+		orgId: 0,
+		description: valueObject.getStepMessageLong(),
+		name: `${valueObject.random}_${valueObject.scenarioName}`,
+		accountUuid: doNotChangeAccount.uuid,
+	};
+	
+	const savedExpenseCategory = await expenseCategoryApi.save(valueObject, expenseCategory as ExpenseCategory);
+
+	expect(savedExpenseCategory.name).toBe(expenseCategory.name);
+	expect(savedExpenseCategory.accountUuid).toBe(expenseCategory.accountUuid);
 });
