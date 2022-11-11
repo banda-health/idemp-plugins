f0df5f7bb309ccbb33e71a25e58b2a4c1cb2b203
Clinton Ingalia <cingalia@bandahealth.org>
2022-11-11 13:10:04 +0300

    Updated process

--- idemp-base/src/org/bandahealth/idempiere/base/process/UpdateExpiredStockProcess.java
+++ idemp-base/src/org/bandahealth/idempiere/base/process/UpdateExpiredStockProcess.java
@@ -30,6 +30,7 @@ public class UpdateExpiredStockProcess extends SvrProcess {
 
     private final String PROCESS_NAME = this.getClass().getName();
     public static final String PARAMETERNAME_C_DocType_ID = "C_DocType_ID";
+    private static final String STORAGE_CLEANUP_PROCESS_STRING = "8e270648-1d54-46d9-9161-2d0300dd80ff";
     
     @Override
     protected void prepare() {
@@ -43,23 +44,20 @@ public class UpdateExpiredStockProcess extends SvrProcess {
         
         List<MStorageOnHand> expiredStocks = new Query(Env.getCtx(), MStorageOnHand.Table_Name,
                 MAttributeSetInstance.Table_Name + "." + MAttributeSetInstance.COLUMNNAME_GuaranteeDate +
-                        " < now()", get_TrxName()).addJoinClause(
+                        " < now() AND " + MStorageOnHand.Table_Name + "." + MStorageOnHand.COLUMNNAME_QtyOnHand + " NOT IN (0)", get_TrxName()).addJoinClause(
                 "JOIN " + MAttributeSetInstance.Table_Name + " ON " + MAttributeSetInstance.Table_Name + "." +
                         MAttributeSetInstance.COLUMNNAME_M_AttributeSetInstance_ID + "=" + MStorageOnHand.Table_Name + "." +
-                        MStorageOnHand.COLUMNNAME_M_AttributeSetInstance_ID +
-                        "WHERE " + MStorageOnHand.COLUMNNAME_QtyOnHand + " NOT IN (0)")
+                        MStorageOnHand.COLUMNNAME_M_AttributeSetInstance_ID)
                 .setClient_ID()
                 .setOnlyActiveRecords(true)
                 .list();
 
-        int count = 0;
         for (MStorageOnHand expiredStock : expiredStocks) {
             UpdateStock.updateStock(expiredStock, BigDecimal.ZERO);
-            count++;
         }
         
         MProcess mprocess = new Query(Env.getCtx(), MProcess.Table_Name, MProcess.COLUMNNAME_AD_Process_UU + "=?",
-                get_TrxName()).setOnlyActiveRecords(true).setParameters("8e270648-1d54-46d9-9161-2d0300dd80ff").first();
+                get_TrxName()).setOnlyActiveRecords(true).setParameters(STORAGE_CLEANUP_PROCESS_STRING).first();
 
         MPInstance mpInstance = new MPInstance(mprocess, 0);
 
@@ -74,44 +72,6 @@ public class UpdateExpiredStockProcess extends SvrProcess {
 
         return processInfo.getSummary();
         
-        // Kick off the storage cleanup process
-        //StorageCleanup storageCleanupProcess = new StorageCleanup();
-        //Get C_DocType_ID for this client before clearing parameters
-        //String whereClause  = "DocBaseType=?";
-       /** MDocType mDocType = new Query(getCtx(), MDocType.Table_Name, whereClause, null)
-                .setClient_ID()
-                .setParameters(MDocType.DOCBASETYPE_MaterialMovement)
-                .setOnlyActiveRecords(true)
-                .first();
-        /**
-         if (mDocType != null) {
-         
-            //Clear Parameters
-            clearParameters();
-            // Add Doc Type Parameter.
-            addParameter(new ProcessInfoParameter(PARAMETERNAME_C_DocType_ID, mDocType.getC_DocType_ID(), null, null, null));
-            storageCleanupProcess.startProcess(getCtx(), getProcessInfo(), null);
-        }**/
-        
-
-        //String msg = "STOP " + PROCESS_NAME + ". Processed " + count + " records(s).";
-        //log.log(Level.INFO, msg);
-
-        //return msg;
-    }
-    
-    private void clearParameters() {
-        List<ProcessInfoParameter> parameters = new ArrayList<ProcessInfoParameter>(Arrays.asList(getParameter()));
-        parameters.clear();
-        // Set the parameters so they can be accessed by everyone
-        getProcessInfo().setParameter(parameters.toArray(ProcessInfoParameter[]::new));
-    }
-    
-    private void addParameter(ProcessInfoParameter parameter) {
-        List<ProcessInfoParameter> parameters = new ArrayList<ProcessInfoParameter>(Arrays.asList(getParameter()));
-        parameters.add(parameter);
-        // Set the parameters so they can be accessed by everyone
-        getProcessInfo().setParameter(parameters.toArray(ProcessInfoParameter[]::new));
     }
 
 }
