b8a27ebe84761ef2384edeef784a15af4fc52ac2
andrewmoko <andrewm@openhmisafrica.org>
2023-02-07 12:10:20 +0300

    Revert "go-2584 close process visit transaction"

--- rest/WEB-INF/src/org/bandahealth/idempiere/rest/service/db/VisitDBService.java
+++ rest/WEB-INF/src/org/bandahealth/idempiere/rest/service/db/VisitDBService.java
@@ -187,13 +187,11 @@ public class VisitDBService extends BaseOrderDBService<Visit> {
 				}
 			}
 			processVisitTransaction.commit();
-			processVisitTransaction.close();
 			Visit visit = createInstanceWithAllFields(order);
 			visit.setPayments(paymentDBService.getPaymentsByOrderId(visit.getId()));
 			return visit;
 		} catch (Exception exception) {
 			processVisitTransaction.rollback();
-			processVisitTransaction.close();
 			throw exception;
 		}
 	}
