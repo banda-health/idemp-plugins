5c647cf1e5f17fa75db8bdb6eeaa913c34b3a617
Andrew Moko <amoko@bandahealth.org>
2023-02-08 11:17:37 +0300

    increase jest timeout

--- rest-test/testing/src/setup/globalConfig.ts
+++ rest-test/testing/src/setup/globalConfig.ts
@@ -10,7 +10,7 @@ const workingDirectory = join(tmpdir(), 'rest-global-setup');
 
 // Since we're dealing with APIs and processing documents, calls may take a while
 // depending on the DB. So, increase the test timeout to handle it
-jest.setTimeout(30000); // 30 seconds
+jest.setTimeout(50000); // 50 seconds
 
 // Set the scenario name for each test since it's used when creating entities
 beforeEach(async () => {
