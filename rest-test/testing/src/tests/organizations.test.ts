a89a597f85e041e1454b3e6b0ce11c52a3043508
Kevin <kwburnett@gmail.com>
2023-04-26 15:47:37 -0500

    test: adjust org to send UUIDs

--- rest-test/testing/src/tests/organizations.test.ts
+++ rest-test/testing/src/tests/organizations.test.ts
@@ -5,27 +5,29 @@ test('save organization information', async () => {
 	const valueObject = globalThis.__VALUE_OBJECT__;
 	await valueObject.login();
 
-	let organization = (
-		await organizationApi.get(valueObject, 0, 1, undefined, undefined)
-	).results[0];
+	const organization = (await organizationApi.get(valueObject, 0, 1, undefined, undefined)).results[0];
 	expect(organization).toBeTruthy();
-	
-	const organizationInformation: Partial<OrganizationInformation> = {
-		facilityNumber: 'facility Number',
-		headerMessage: 'header',
-		receiptFooterMessage: 'footer message',
+
+	const organizationToSave: Partial<Organization> = {
+		...organization,
+		organizationInformation: {
+			...organization.organizationInformation,
+			facilityNumber: 'facility Number',
+			headerMessage: 'header',
+			receiptFooterMessage: 'footer message',
+		} as OrganizationInformation,
 	};
 
-	const saveOrganization: Partial<Organization> = {
-		name: organization.name,
-		description: organization.description,
-		organizationInformation: organizationInformation as OrganizationInformation,
-	};
-	
-	const savedOrganization = await organizationApi.save(valueObject, saveOrganization as Organization);
+	const savedOrganization = await organizationApi.save(valueObject, organizationToSave as Organization);
 
-	expect(saveOrganization.name).toBe(saveOrganization.name);
-	expect(saveOrganization.organizationInformation!.facilityNumber).toBe(savedOrganization.organizationInformation.facilityNumber);
-	expect(saveOrganization.organizationInformation!.headerMessage).toBe(savedOrganization.organizationInformation.headerMessage);
-	expect(saveOrganization.organizationInformation!.receiptFooterMessage).toBe(savedOrganization.organizationInformation.receiptFooterMessage);
+	expect(savedOrganization.name).toBe(organizationToSave.name);
+	expect(savedOrganization.organizationInformation.facilityNumber).toBe(
+		organizationToSave.organizationInformation!.facilityNumber,
+	);
+	expect(savedOrganization.organizationInformation.headerMessage).toBe(
+		organizationToSave.organizationInformation!.headerMessage,
+	);
+	expect(savedOrganization.organizationInformation.receiptFooterMessage).toBe(
+		organizationToSave.organizationInformation!.receiptFooterMessage,
+	);
 });
