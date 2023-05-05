e86e7c1de08e6f87ce3fab9939c8765fe6fd2ac4
NivekC <kchemjor@bandahealth.org>
2023-05-05 14:19:23 +0300

    Deletion from ad_process

---
+++ migration/local_sql/postgresql/202305041300_GO-2694.sql
@@ -0,0 +1,64 @@
+-- Deleting from ad_process_para table
+DELETE
+FROM
+	ad_process_para
+WHERE
+	ad_process_id = 1000033;
+
+-- Deleting from ad_process_access table
+DELETE
+FROM
+	ad_process_access
+WHERE
+	ad_process_id = 1000033;
+
+-- Deleting from ad_process_trl table
+DELETE
+FROM
+	ad_process_trl
+WHERE
+	ad_process_id = 1000033;
+
+-- Deleting from ad_pinstance and ad_pinstance_para table
+DELETE
+FROM
+	ad_pinstance ap
+WHERE
+	EXISTS(
+			SELECT
+				1
+			FROM
+				ad_pinstance_para app
+			WHERE
+				ap.ad_pinstance_id = app.ad_pinstance_id
+				AND ap.ad_process_id = 1000033
+		);
+
+-- Deleting from ad_package_exp_detail table
+DELETE
+FROM
+	ad_package_exp_detail ad
+WHERE
+	EXISTS(
+			SELECT
+				1
+			FROM
+				ad_menu am
+			WHERE
+				am.ad_menu_id = ad.ad_menu_id
+				AND am.ad_process_id = 1000033
+		);
+
+-- Deleting from ad_menu
+DELETE
+FROM
+	ad_menu
+WHERE
+	ad_process_id = 1000033;
+
+-- Deleting from ad_process table
+DELETE
+FROM
+	ad_process
+WHERE
+	ad_process_uu = 'ad3da529-459a-4804-8020-5f192360fed0';
