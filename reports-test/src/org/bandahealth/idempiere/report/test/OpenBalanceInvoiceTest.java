0d2cd1731731a1a873c036566411ea3ae215fceb
Kevin <kwburnett@gmail.com>
2023-06-09 14:16:26 -0500

    test: phone numbers are correct

--- reports-test/src/org/bandahealth/idempiere/report/test/OpenBalanceInvoiceTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/OpenBalanceInvoiceTest.java
@@ -11,6 +11,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bandahealth.idempiere.base.model.MDocType_BH;
 import org.bandahealth.idempiere.base.model.MInvoice_BH;
+import org.bandahealth.idempiere.base.model.MOrgInfo_BH;
 import org.bandahealth.idempiere.base.model.MPayment_BH;
 import org.bandahealth.idempiere.report.test.utils.TableUtils;
 import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
@@ -27,11 +28,14 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OpenBalanceInvoiceTest extends ChuBoePopulateFactoryVO {
 	private final String reportUuid = "199f56a6-8e1f-47b4-8f22-e2bdb8da7505";
@@ -191,6 +195,82 @@ public class OpenBalanceInvoiceTest extends ChuBoePopulateFactoryVO {
 			valueObject.refresh();
 			assertEquals(valueObject.getBusinessPartner().getTotalOpenBalance().doubleValue(), totalOpenBalance,
 					"Total open balance matches what's on the business partner");
+		}
+	}
+
+	@IPopulateAnnotation.CanRun
+	public void invoiceShowsTheCorrectClinicAndPatientInformation() throws SQLException, IOException, ParseException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		ChuBoeCreateEntity.changeOrganization(valueObject);
+
+		valueObject.setStepName("Create business partner");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		valueObject.getBusinessPartner().setAD_Org_ID(valueObject.getOrg().getAD_Org_ID());
+		valueObject.getBusinessPartner().setBH_Phone(String.valueOf(valueObject.getRandomNumber()));
+		valueObject.getBusinessPartner().saveEx();
+		commitEx();
+
+		valueObject.setStepName("Update clinic phone");
+		valueObject.setRandom();
+		MOrgInfo_BH clinicInfo =
+				new Query(valueObject.getContext(), MOrgInfo_BH.Table_Name, MOrgInfo_BH.COLUMNNAME_AD_Org_ID + "=?",
+						valueObject.getTransactionName()).setParameters(valueObject.getOrg().get_ID()).first();
+		clinicInfo.setPhone(String.valueOf(valueObject.getRandomNumber()));
+		clinicInfo.saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create product");
+		valueObject.setRandom();
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create purchase order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create visit");
+		ChuBoeCreateEntity.createVisit(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create sales order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid(reportUuid);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Collections.singletonList(
+				new ProcessInfoParameter("c_bpartner_uu", valueObject.getBusinessPartner().getC_BPartner_UU(), null, null,
+						null)
+		));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+
+			Optional<Row> clinicPhoneRow = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
+							cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
+									cell.getStringCellValue().equals("TEL:  " + clinicInfo.getPhone()))).findFirst();
+			Optional<Row> patientPhoneRow = StreamSupport.stream(sheet.spliterator(), false).filter(
+							row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
+									cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
+											cell.getStringCellValue().equals("Phone:  " + valueObject.getBusinessPartner().getBH_Phone())))
+					.findFirst();
+
+			assertTrue(clinicPhoneRow.isPresent(), "Clinic phone number is displayed");
+			assertTrue(patientPhoneRow.isPresent(), "Patient phone number is displayed");
 		}
 	}
 }
