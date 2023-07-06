ab48e14d370cf2723faf3c32b29a83f2276cdc54
Kevin <kwburnett@gmail.com>
2023-07-06 13:02:18 -0500

    fix: formatting and where clause

--- migration/local_sql/postgresql/202307061620_GO-XXXX.sql
+++ migration/local_sql/postgresql/202307061620_GO-XXXX.sql
@@ -2,21 +2,13 @@
 -- Solution: Update all the ad_client_id columns to match the ad_org_id column on m_transaction where the two do not match
 UPDATE m_transaction
 SET
-    ad_client_id = ad_org_id
+	ad_client_id = ad_org_id
 WHERE
-        ad_client_id IN (
-        SELECT DISTINCT
-            ad_client_id
-        FROM
-            m_transaction
-        WHERE
-            ad_client_id <> ad_org_id
-    );
+	ad_client_id != ad_org_id;
 
 -- Finishing Up
 -- Named as go-XXXX for now since YouTrack is down and we don't have an issue created for this
 SELECT
-    register_migration_script('202307061620_GO-XXXX.sql')
+	register_migration_script('202307061620_GO-XXXX.sql')
 FROM
-    dual;
-    
+	dual;
