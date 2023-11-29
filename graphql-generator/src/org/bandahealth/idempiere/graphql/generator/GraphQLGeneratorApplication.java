b25d0f7ce44b8b641e3dc114a1c41a48c32ec434
Kevin <kwburnett@gmail.com>
2023-11-29 09:28:12 +0100

    feat: initial GraphQL generator

--- graphql-generator/src/org/bandahealth/idempiere/graphql/generator/GraphQLGeneratorApplication.java
+++ graphql-generator/src/org/bandahealth/idempiere/graphql/generator/GraphQLGeneratorApplication.java
@@ -13,27 +13,25 @@
  *****************************************************************************/
 package org.bandahealth.idempiere.graphql.generator;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.Map;
-
-import javax.swing.JFrame;
-
 import org.adempiere.util.ModelClassGenerator;
-import org.adempiere.util.ModelGeneratorDialog;
 import org.adempiere.util.ModelInterfaceGenerator;
+import org.bandahealth.idempiere.graphql.generator.util.GraphQLGeneratorDialog;
 import org.compiere.Adempiere;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.FrameworkUtil;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Map;
+
 /**
  * @author hengsin
  * @author tbayen - command line start
- *
  */
-public class ModelGeneratorApplication implements IApplication {
+public class GraphQLGeneratorApplication implements IApplication {
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
@@ -50,36 +48,35 @@ public class ModelGeneratorApplication implements IApplication {
 			String entityType = commandlineArgs[2];
 			String tableName = commandlineArgs[3];
 			String columnEntityType = null;
-			if (commandlineArgs.length >= 5) 
+			if (commandlineArgs.length >= 5)
 				columnEntityType = commandlineArgs[4];
 			ModelInterfaceGenerator.generateSource(folder, packageName, entityType, tableName, columnEntityType);
 			ModelClassGenerator.generateSource(folder, packageName, entityType, tableName, columnEntityType);
 		} else if (commandlineArgs.length != 0) {
 			System.out.println("usage: ModelGenerator folder packageName tableEntityType tableName columnEntityType");
 		} else {
-			ModelGeneratorDialog dialog = new ModelGeneratorDialog();
+			GraphQLGeneratorDialog dialog = new GraphQLGeneratorDialog();
 			dialog.addWindowListener(new WindowAdapter() {
 				@Override
 				public void windowClosed(WindowEvent e) {
-					context.setResult(IApplication.EXIT_OK, ModelGeneratorApplication.this);
+					context.setResult(IApplication.EXIT_OK, GraphQLGeneratorApplication.this);
 					try {
 						// async stop https://www.eclipse.org/forums/index.php?t=msg&th=31999&goto=103832&#msg_103832
 						// can cast getBundle(0) to org.osgi.framework.launch.Framework in case want more
-						FrameworkUtil.getBundle(ModelGeneratorApplication.class).getBundleContext().getBundle(0).stop();
+						FrameworkUtil.getBundle(GraphQLGeneratorApplication.class).getBundleContext().getBundle(0).stop();
 					} catch (BundleException be) {
 						System.exit(0);
 					}
 				}
-				
+
 			});
 			//dialog.setModal(true);
 			dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			dialog.pack();
 			dialog.setLocationRelativeTo(null);
 			dialog.setVisible(true);
-			
 		}
-		
+
 		// async stop on close window
 		return IApplicationContext.EXIT_ASYNC_RESULT;
 	}
