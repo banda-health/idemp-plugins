3d620b7dd7f9ff8803bec82186a51c77921e501b
Kevin <kwburnett@gmail.com>
2023-12-05 12:18:31 +0100

    fix: column ordering

--- migration/local_sql/postgresql/202311061144_GO-2357.sql
+++ migration/local_sql/postgresql/202311061144_GO-2357.sql
@@ -249,7 +249,7 @@ VALUES
 	 ), 0, 0, 'Y', '2023-11-23 08:26:24.728000', '2023-11-23 08:26:24.728000', 100, 100, 'BMI (kg/m²)', NULL, NULL, 0,
 	 'U', 'BH_BMI', (
 		 SELECT ad_table_id FROM ad_table WHERE ad_table_uu = 'f472818e-6071-4dcf-b705-4020ad79c154'
-	 ), 12, NULL, NULL, 14, '@BH_Height@*100*100/@BH_Weight@/@BH_Weight@', 'N', 'N', 'N', 'Y', '1=1', 'N', 25, 'N', 'N',
+	 ), 12, NULL, NULL, 14, '@BH_Height@*100*100/@BH_Weight@/@BH_Weight@', 'N', 'N', 'N', 'Y', '1=1', 'N', 0, 'N', 'N',
 	 NULL, NULL, NULL, NULL, 'N', (
 		 SELECT ad_element_id FROM ad_element WHERE ad_element_uu = 'd8826d20-904b-496c-88c6-850c35237365'
 	 ), NULL, 'N', 'N', NULL, NULL, NULL, 'N', 'Y', '0.00', '850c652d-3cdb-46fa-b40d-f30321dcc35b', 'Y', 0, 'N', 'N',
@@ -327,8 +327,8 @@ VALUES
 		 SELECT ad_tab_id FROM ad_tab WHERE ad_tab_uu = '789a08af-2015-4469-b2ef-d4ca55e6f2e7'
 	 ), (
 		 SELECT ad_column_id FROM ad_column WHERE ad_column_uu = '850c652d-3cdb-46fa-b40d-f30321dcc35b'
-	 ), NULL, 'Y', NULL, 0, 'N', 100, 0, 'N', 'N', 'N', 'N', 'U', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-	 '70b5bfa1-9c75-4fea-bf7e-076f4f4163fb', NULL, 100, 'Y', 1, 1, 1, 'N', NULL, NULL, NULL, NULL, NULL, 'N', 'N', NULL,
+	 ), NULL, 'Y', NULL, 0, 'N', 25, 0, 'N', 'N', 'N', 'N', 'U', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	 '70b5bfa1-9c75-4fea-bf7e-076f4f4163fb', NULL, 25, 'Y', 1, 1, 1, 'N', NULL, NULL, NULL, NULL, NULL, 'N', 'N', NULL,
 	 NULL, NULL, NULL, 'N');
 INSERT INTO
 	ad_field (ad_field_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description,
