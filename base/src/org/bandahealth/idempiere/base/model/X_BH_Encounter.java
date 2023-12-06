538c439d74b7ab5e9fa90364c7d4b8ee5b746cec
Andrew Moko <amoko@bandahealth.org>
2023-12-06 15:45:52 +0300

    code review fixes

--- base/src/org/bandahealth/idempiere/base/model/X_BH_Encounter.java
+++ base/src/org/bandahealth/idempiere/base/model/X_BH_Encounter.java
@@ -30,7 +30,7 @@ public class X_BH_Encounter extends PO implements I_BH_Encounter, I_Persistent
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20231121L;
+	private static final long serialVersionUID = 20231206L;
 
     /** Standard Constructor */
     public X_BH_Encounter (Properties ctx, int BH_Encounter_ID, String trxName)
@@ -90,14 +90,14 @@ public class X_BH_Encounter extends PO implements I_BH_Encounter, I_Persistent
 		return ii.intValue();
 	}
 
-	/** Immunizations = i */
-	public static final String BH_ENCOUNTER_TYPE_Immunizations = "i";
-	/** Capture Vitals = l */
-	public static final String BH_ENCOUNTER_TYPE_CaptureVitals = "l";
+	/** Immunizations = I */
+	public static final String BH_ENCOUNTER_TYPE_Immunizations = "I";
+	/** Capture Vitals = V */
+	public static final String BH_ENCOUNTER_TYPE_CaptureVitals = "V";
 	/** Diagnosis = m */
 	public static final String BH_ENCOUNTER_TYPE_Diagnosis = "m";
-	/** Clinical Details = n */
-	public static final String BH_ENCOUNTER_TYPE_ClinicalDetails = "n";
+	/** Clinical Details = D */
+	public static final String BH_ENCOUNTER_TYPE_ClinicalDetails = "D";
 	/** Set Encounter Type.
 		@param BH_Encounter_Type Encounter Type	  */
 	public void setBH_Encounter_Type (String BH_Encounter_Type)
