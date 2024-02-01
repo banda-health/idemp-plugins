8c8cf280707422dab296e0138df5acd5c6b9c757
Kevin <kwburnett@gmail.com>
2024-02-01 19:15:25 +0100

    test: service creation

--- graphql-test/testing/src/tests/services.test.ts
+++ graphql-test/testing/src/tests/services.test.ts
@@ -1,25 +1,36 @@
-// import { serviceApi } from '../api';
-// import { Service } from '../types/org.bandahealth.idempiere.rest';
+import { mutate, query } from '../api';
+import { getDefaultProductCategory, getDefaultTaxCategory } from '../utils';
+import { C_UomGetDefaultDocument, M_ProductGetDocument, M_ProductSaveDocument } from '../__generated__/graphql';
 
-// test('can create and fetch services', async () => {
-// 	const valueObject = globalThis.__VALUE_OBJECT__;
-// 	await valueObject.login();
+test('can create and fetch services', async () => {
+	const valueObject = globalThis.__VALUE_OBJECT__;
+	await valueObject.login();
 
-// 	let service: Partial<Service> = {
-// 		orgId: 0,
-// 		description: valueObject.getStepMessageLong(),
-// 		name: valueObject.getDynamicScenarioName(),
-// 		productCategoryUuid: '',
-// 		sellingPrice: 200,
-// 	};
-// 	service = await serviceApi.save(valueObject, service as Service);
-// 	expect(service).toBeTruthy();
+	const service = (
+		await mutate(valueObject)({
+			mutation: M_ProductSaveDocument,
+			variables: {
+				entity: {
+					AD_Org: { UUID: valueObject.organization!.UUID },
+					Description: valueObject.getStepMessageLong(),
+					Name: valueObject.getDynamicScenarioName(),
+					C_TaxCategory: { UUID: (await getDefaultTaxCategory(valueObject)).UUID },
+					M_Product_Category: { UUID: (await getDefaultProductCategory(valueObject)).UUID },
+					C_UOM: { UUID: (await query(valueObject)({ query: C_UomGetDefaultDocument })).data.C_UOMGetDefault.UUID },
+					ProductType: { UUID: '265e0369-47e4-4be9-b6d5-e344230f5588' }, // Service
+					BH_SellPrice: 200,
+				},
+			},
+		})
+	).data!.M_ProductSave!;
+	expect(service).toBeTruthy();
 
-// 	const services = (
-// 		await serviceApi.get(valueObject, undefined, undefined, undefined, JSON.stringify({ name: service.name! }))
-// 	).results;
-// 	expect(services).toHaveLength(1);
-// 	expect(services[0].name).toEqual(service.name);
-// });
-
-export {};
+	const services = (
+		await query(valueObject)({
+			query: M_ProductGetDocument,
+			variables: { filter: JSON.stringify({ name: service.Name! }) },
+		})
+	).data.M_ProductGet.results;
+	expect(services).toHaveLength(1);
+	expect(services[0].Name).toEqual(service.Name);
+});
