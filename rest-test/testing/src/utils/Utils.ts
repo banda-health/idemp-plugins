1293e9a1a1c052776bb4a45e601ba1f46a965d89
brianmituka <mitukab@outlook.com>
2024-04-02 09:38:23 +0300

    GO-2592 Added utility method for creating invoices

--- rest-test/testing/src/utils/Utils.ts
+++ rest-test/testing/src/utils/Utils.ts
@@ -461,3 +461,42 @@ export async function createInventory(valueObject: ValueObject) {
 		}
 	}
 }
+
+
+export async function createInvoiceWithPaymentType(valueObject: ValueObject) {
+	valueObject.validate();
+
+ if (!valueObject.businessPartner) {
+		throw new Error('Business Partner is Null');
+	} 
+
+	const invoice: Partial<Invoice> = {
+		orgId: 0,
+		description: valueObject.getStepMessageLong(),
+		businessPartner: valueObject.businessPartner,
+		dateInvoiced: valueObject.date?.toISOString(),
+		invoiceLines: [],
+		documentTypeTarget: valueObject.documentType,
+		paymentRule : valueObject.paymentRule,
+		grandTotal: valueObject.paymentAmount
+	};
+	const invoiceLine: Partial<InvoiceLine> = {
+		description: valueObject.getStepMessageLong(),
+		quantity: valueObject.quantity || 1,
+	};
+	if (valueObject.product) {
+		invoiceLine.product = valueObject.product;
+	} else if (valueObject.charge) {
+		invoiceLine.charge = valueObject.charge;
+	}
+	invoiceLine.price =
+		valueObject.salesStandardPrice || (invoiceLine.quantity || 0) * (invoiceLine.product?.sellPrice || 0);
+	invoice.invoiceLines?.push(invoiceLine as unknown as InvoiceLine);
+
+	valueObject.invoice = await invoiceApi.save(valueObject, invoice as Invoice);
+	if (!valueObject.invoice) {
+		throw new Error('Invoice not created');
+	}
+	valueObject.invoiceLine = valueObject.invoice!.invoiceLines[0];
+	valueObject.visit?.invoices?.push(valueObject.invoice!);
+}
