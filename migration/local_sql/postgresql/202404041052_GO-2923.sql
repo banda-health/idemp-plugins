28f681370fe6943a52eb9395c859e4d3374ca95e
Andrew Moko <amoko@bandahealth.org>
2024-04-25 15:35:56 +0300

    update coded diagnoses tab

--- migration/local_sql/postgresql/202404041052_GO-2923.sql
+++ migration/local_sql/postgresql/202404041052_GO-2923.sql
@@ -138,21 +138,12 @@ WHERE
 	);
 DELETE
 FROM
-	ad_tab
-WHERE
-		ad_table_id IN (
-		SELECT
-			ad_table_id
-		FROM
-			ad_table
-		WHERE
-			LOWER(tablename) = 'bh_coded_diagnosis'
-	);
-DELETE
-FROM
 	ad_table
 WHERE
 	LOWER(tablename) = 'bh_coded_diagnosis';
+
+-- update coded diagnoses tab to point to bh_concept table
+UPDATE  AD_Tab SET AD_Table_ID = (SELECT AD_Table_ID FROM AD_Table WHERE AD_Table_uu = '2dcec3ca-58e7-4f5e-86b9-90465b99a581') WHERE AD_Tab_UU = 'd25b4199-f9f8-482a-b4a7-cdc7a628bf10';	
 
 -- Step 6:
 UPDATE AD_Process SET classname = 'org.bandahealth.idempiere.base.process.ConceptSyncProcess' WHERE AD_Process_UU='dc0a5369-1478-46ff-aef4-8bac662132b7';
