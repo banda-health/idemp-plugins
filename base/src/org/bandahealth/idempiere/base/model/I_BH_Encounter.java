effe9620df0db83c730de639d4d73eeaa3024559
brianmituka <mitukab@outlook.com>
2024-05-03 14:47:44 +0300

    Added encounter to db

--- base/src/org/bandahealth/idempiere/base/model/I_BH_Encounter.java
+++ base/src/org/bandahealth/idempiere/base/model/I_BH_Encounter.java
@@ -23,7 +23,7 @@ import org.compiere.util.KeyNamePair;
 
 /** Generated Interface for BH_Encounter
  *  @author iDempiere (generated) 
- *  @version Release 7.1
+ *  @version Release 11
  */
 @SuppressWarnings("all")
 public interface I_BH_Encounter 
@@ -32,7 +32,7 @@ public interface I_BH_Encounter
     /** TableName=BH_Encounter */
     public static final String Table_Name = "BH_Encounter";
 
-    /** AD_Table_ID=1000040 */
+    /** AD_Table_ID=1000041 */
     public static final int Table_ID = MTable.getTable_ID(Table_Name);
 
     KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
@@ -46,8 +46,8 @@ public interface I_BH_Encounter
     /** Column name AD_Client_ID */
     public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
 
-	/** Get Client.
-	  * Client/Tenant for this installation.
+	/** Get Tenant.
+	  * Tenant for this installation.
 	  */
 	public int getAD_Client_ID();
 
@@ -55,14 +55,27 @@ public interface I_BH_Encounter
     public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
 
 	/** Set Organization.
-	  * Organizational entity within client
+	  * Organizational entity within tenant
 	  */
 	public void setAD_Org_ID (int AD_Org_ID);
 
 	/** Get Organization.
-	  * Organizational entity within client
+	  * Organizational entity within tenant
 	  */
 	public int getAD_Org_ID();
+
+    /** Column name BH_Encounter_Date */
+    public static final String COLUMNNAME_BH_Encounter_Date = "BH_Encounter_Date";
+
+	/** Set Encounter Date.
+	  * the time a vital was taken
+	  */
+	public void setBH_Encounter_Date (Timestamp BH_Encounter_Date);
+
+	/** Get Encounter Date.
+	  * the time a vital was taken
+	  */
+	public Timestamp getBH_Encounter_Date();
 
     /** Column name BH_Encounter_ID */
     public static final String COLUMNNAME_BH_Encounter_ID = "BH_Encounter_ID";
