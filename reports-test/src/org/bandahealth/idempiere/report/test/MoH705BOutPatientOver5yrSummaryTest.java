a1b3db7e17647e0d790d7900904245e992bdc3e6
Andrew Moko <amoko@bandahealth.org>
2024-05-06 14:52:06 +0300

    add burns concept

--- reports-test/src/org/bandahealth/idempiere/report/test/MoH705BOutPatientOver5yrSummaryTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/MoH705BOutPatientOver5yrSummaryTest.java
@@ -83,6 +83,32 @@ public class MoH705BOutPatientOver5yrSummaryTest extends ChuBoePopulateFactoryVO
 			}
 
 			commitEx();
+			
+			// verify second diagnosis exists
+			MBHConcept codedDiagnosis2 = new Query(valueObject.getContext(), MBHConcept.Table_Name,
+					MBHConcept.COLUMNNAME_BH_Display_Name + "=?", valueObject.getTransactionName())
+							.setParameters(diagnosisAfterDiagnosisToSearchForOnReport).first();
+			if (codedDiagnosis2 == null) {
+				valueObject.setStepName("Create the burns coded diagnosis");
+				codedDiagnosis2 = new MBHConcept(valueObject.getContext(), 0, valueObject.getTransactionName());
+				codedDiagnosis2.setBH_Display_Name(diagnosisAfterDiagnosisToSearchForOnReport);
+			}
+			codedDiagnosis2.saveEx();
+
+			MBHConceptExtra extra2 = new Query(valueObject.getContext(), MBHConceptExtra.Table_Name,
+					MBHConceptExtra.COLUMNNAME_BH_Value + "=? AND " + MBHConceptExtra.COLUMNNAME_BH_Concept_ID + "=? AND "
+							+ MBHConceptExtra.COLUMNNAME_BH_Key + "=?",
+					valueObject.getTransactionName())
+					.setParameters(diagnosisAfterDiagnosisToSearchForOnReport, codedDiagnosis.getBH_Concept_ID(), MOH705BGREATERTHAN5).first();
+			if (extra2 == null) {
+				extra2 = new MBHConceptExtra(valueObject.getContext(), 0, valueObject.getTransactionName());
+				extra2.setBH_Key(MOH705BGREATERTHAN5);
+				extra2.setBH_Value(diagnosisAfterDiagnosisToSearchForOnReport);
+				extra2.setBH_Concept_ID(codedDiagnosis2.getBH_Concept_ID());
+				extra2.saveEx();
+			}
+
+			commitEx();
 		} finally {
 			Env.setContext(valueObject.getContext(), Env.AD_CLIENT_ID, currentClientId);
 		}
@@ -232,6 +258,31 @@ public class MoH705BOutPatientOver5yrSummaryTest extends ChuBoePopulateFactoryVO
 				extra.setBH_Value(diagnosisToSearchFor);
 				extra.setBH_Concept_ID(codedDiagnosis.getBH_Concept_ID());
 				extra.saveEx();
+			}
+
+			commitEx();
+			// verify second diagnosis exists
+			MBHConcept codedDiagnosis2 = new Query(valueObject.getContext(), MBHConcept.Table_Name,
+					MBHConcept.COLUMNNAME_BH_Display_Name + "=?", valueObject.getTransactionName())
+							.setParameters(diagnosisAfterDiagnosisToSearchForOnReport).first();
+			if (codedDiagnosis2 == null) {
+				valueObject.setStepName("Create the burns coded diagnosis");
+				codedDiagnosis2 = new MBHConcept(valueObject.getContext(), 0, valueObject.getTransactionName());
+				codedDiagnosis2.setBH_Display_Name(diagnosisAfterDiagnosisToSearchForOnReport);
+			}
+			codedDiagnosis2.saveEx();
+
+			MBHConceptExtra extra2 = new Query(valueObject.getContext(), MBHConceptExtra.Table_Name,
+					MBHConceptExtra.COLUMNNAME_BH_Value + "=? AND " + MBHConceptExtra.COLUMNNAME_BH_Concept_ID + "=? AND "
+							+ MBHConceptExtra.COLUMNNAME_BH_Key + "=?",
+					valueObject.getTransactionName())
+					.setParameters(diagnosisAfterDiagnosisToSearchForOnReport, codedDiagnosis.getBH_Concept_ID(), MOH705BGREATERTHAN5).first();
+			if (extra2 == null) {
+				extra2 = new MBHConceptExtra(valueObject.getContext(), 0, valueObject.getTransactionName());
+				extra2.setBH_Key(MOH705BGREATERTHAN5);
+				extra2.setBH_Value(diagnosisAfterDiagnosisToSearchForOnReport);
+				extra2.setBH_Concept_ID(codedDiagnosis2.getBH_Concept_ID());
+				extra2.saveEx();
 			}
 
 			commitEx();
