5aa0c9dfb5325702b8852b796b4ebcb6acadc73c
jasonbrink <jasonb1@gmail.com>
2024-05-22 09:39:49 -0400

    fix: re-generate GraphQL concept models after update to generator

--- graphql/src/org/bandahealth/idempiere/graphql/resolver/query/X_BH_Concept_DescriptionQuery.java
+++ graphql/src/org/bandahealth/idempiere/graphql/resolver/query/X_BH_Concept_DescriptionQuery.java
@@ -3,7 +3,11 @@ package org.bandahealth.idempiere.graphql.resolver.query;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
 import org.bandahealth.idempiere.base.model.MBHConceptDescription;
+import org.bandahealth.idempiere.graphql.dataloader.impl.X_BH_Concept_DescriptionDataLoader;
 import org.bandahealth.idempiere.graphql.model.Connection;
+import org.dataloader.DataLoader;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Generated Query Resolver for BH_Concept_Description - DO NOT CHANGE
@@ -15,6 +19,12 @@ public class X_BH_Concept_DescriptionQuery extends POQuery<MBHConceptDescription
 	@Override
 	protected String getTableName() {
 		return MBHConceptDescription.Table_Name;
+	}
+
+	public CompletableFuture<MBHConceptDescription> BH_Concept_Description(String UU, DataFetchingEnvironment environment) {
+		DataLoader<String, MBHConceptDescription> dataLoader = environment.getDataLoaderRegistry()
+				.getDataLoader(X_BH_Concept_DescriptionDataLoader.DATALOADER_BH_Concept_Description_BY_UUID);
+		return dataLoader.load(UU);
 	}
 
 	public Connection<MBHConceptDescription> BH_Concept_DescriptionGet(int Page, int PageSize, String Sort, String Filter,
