eceb6e87bf778cb935a86677df3c731a45dd7482
brianmituka <mitukab@outlook.com>
2024-05-23 12:23:08 +0300

    Removed unnecessary imports

--- rest/WEB-INF/src/org/bandahealth/idempiere/rest/service/db/EncounterDBService.java
+++ rest/WEB-INF/src/org/bandahealth/idempiere/rest/service/db/EncounterDBService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.sql.Timestamp;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.bandahealth.idempiere.base.model.MBHEncounter;
