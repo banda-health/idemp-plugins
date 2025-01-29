6f82068e9e0617af4edc56fae5be1bb53c6059a6
Kevin <kwburnett@gmail.com>
2025-01-29 08:09:19 -0600

    test: the price list on the report

--- reports-test/src/org/bandahealth/idempiere/report/test/ProductsAndPricesTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/ProductsAndPricesTest.java
@@ -15,6 +15,7 @@ import org.bandahealth.idempiere.base.model.MDocType_BH;
 import org.bandahealth.idempiere.base.model.MPayment_BH;
 import org.bandahealth.idempiere.base.model.MProduct_BH;
 import org.bandahealth.idempiere.report.test.utils.TableUtils;
+import org.compiere.model.MPriceList;
 import org.compiere.model.Query;
 import org.compiere.process.DocumentEngine;
 import org.hamcrest.Matchers;
@@ -105,14 +106,24 @@ public class ProductsAndPricesTest extends ChuBoePopulateFactoryVO {
 		FileInputStream file = new FileInputStream(valueObject.getReport());
 		try (Workbook workbook = new XSSFWorkbook(file)) {
 			Sheet sheet = workbook.getSheetAt(0);
-			Optional<Row> productRow =
-					StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(0) != null &&
-							row.getCell(0).getStringCellValue().equalsIgnoreCase(valueObject.getProduct().getName())).findFirst();
+
+			Row headerRow = TableUtils.getHeaderRow(sheet, "Name");
+			int productNameColumnIndex = TableUtils.getColumnIndex(headerRow, "Name");
+			int lastBuyingPriceColumnIndex = TableUtils.getColumnIndex(headerRow, "Last Buying Price");
+			int sellingPriceColumnIndex = TableUtils.getColumnIndex(headerRow, "Selling Price");
+			int totalQuantityColumnIndex = TableUtils.getColumnIndex(headerRow, "Total Quantity");
+
+			Optional<Row> productRow = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> row.getCell(productNameColumnIndex) != null && row.getCell(productNameColumnIndex).getStringCellValue()
+							.equalsIgnoreCase(valueObject.getProduct().getName())).findFirst();
 
 			assertTrue(productRow.isPresent(), "Product row exists");
-			assertThat("Last Buying Price is correct", productRow.get().getCell(1).getNumericCellValue(), is(20D));
-			assertThat("Selling Price is correct", productRow.get().getCell(2).getNumericCellValue(), is(50D));
-			assertThat("Selling Price is correct", productRow.get().getCell(3).getNumericCellValue(), is(1D));
+			assertThat("Last Buying Price is correct",
+					productRow.get().getCell(lastBuyingPriceColumnIndex).getNumericCellValue(), is(20D));
+			assertThat("Selling Price is correct", productRow.get().getCell(sellingPriceColumnIndex).getNumericCellValue(),
+					is(50D));
+			assertThat("Selling Price is correct", productRow.get().getCell(totalQuantityColumnIndex).getNumericCellValue(),
+					is(1D));
 		}
 	}
 
@@ -188,13 +199,14 @@ public class ProductsAndPricesTest extends ChuBoePopulateFactoryVO {
 			assertEquals(productCount, reportProductCount, "All active products returned on the report");
 		}
 	}
+
 	@IPopulateAnnotation.CanRun
 	public void priceListAreDisplayed() throws SQLException, IOException, ParseException {
 		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
 		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
 		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
 
-		valueObject.setStepName("Create business partner");
+		valueObject.setStepName("Create business partner 1");
 		ChuBoeCreateEntity.createBusinessPartner(valueObject);
 		commitEx();
 
@@ -206,17 +218,17 @@ public class ProductsAndPricesTest extends ChuBoePopulateFactoryVO {
 		attributeSet.setDescription(valueObject.getScenarioName());
 		attributeSet.saveEx();
 		commitEx();
-		
 
-		valueObject.setStepName("Create product");
+		valueObject.setStepName("Create product 1");
 		valueObject.setSalesStandardPrice(new BigDecimal(50));
 		ChuBoeCreateEntity.createProduct(valueObject);
 		valueObject.getProduct().setM_AttributeSet_ID(attributeSet.get_ID());
-		valueObject.getProduct().setName("Product with priceList");
 		valueObject.getProduct().saveEx();
 		commitEx();
+		MProduct_BH product1 = valueObject.getProduct();
+		MPriceList priceList1 = valueObject.getSalesPriceList();
 
-		valueObject.setStepName("Create valid attribute set instance");
+		valueObject.setStepName("Create valid attribute set instance 1");
 		MAttributeSetInstance_BH
 				validAttributeSetInstance =
 				new MAttributeSetInstance_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
@@ -226,7 +238,39 @@ public class ProductsAndPricesTest extends ChuBoePopulateFactoryVO {
 		validAttributeSetInstance.saveEx();
 		commitEx();
 
-		valueObject.setStepName("Create order");
+		valueObject.setStepName("Create order 1");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		valueObject.setAttributeSetInstance(validAttributeSetInstance);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create business partner 2");
+		valueObject.clearBusinessPartner();
+		valueObject.clearProduct();
+		valueObject.clearPriceLists();
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create product 2");
+		valueObject.setSalesStandardPrice(new BigDecimal(50));
+		ChuBoeCreateEntity.createProduct(valueObject);
+		valueObject.getProduct().setM_AttributeSet_ID(attributeSet.get_ID());
+		valueObject.getProduct().saveEx();
+		commitEx();
+		MProduct_BH product2 = valueObject.getProduct();
+		MPriceList priceList2 = valueObject.getSalesPriceList();
+
+		valueObject.setStepName("Create valid attribute set instance 2");
+		validAttributeSetInstance =
+				new MAttributeSetInstance_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
+		validAttributeSetInstance.setM_AttributeSet_ID(attributeSet.get_ID());
+		validAttributeSetInstance.setAD_Org_ID(valueObject.getOrg().getAD_Org_ID());
+		validAttributeSetInstance.setDescription(valueObject.getScenarioName());
+		validAttributeSetInstance.saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create order 2");
 		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
 		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
 		valueObject.setAttributeSetInstance(validAttributeSetInstance);
@@ -240,34 +284,29 @@ public class ProductsAndPricesTest extends ChuBoePopulateFactoryVO {
 		valueObject.setReportType("xlsx");
 		ChuBoeCreateEntity.runReport(valueObject);
 
-		// Get the number of products we should see on the report
-		int productCount = new Query(valueObject.getContext(), MProduct_BH.Table_Name,
-				MProduct_BH.COLUMNNAME_IsActive + "=? AND " + MProduct_BH.COLUMNNAME_ProductType + "=?",
-				valueObject.getTransactionName()).setParameters("Y", "I").setClient_ID().count();
-
 		FileInputStream file = new FileInputStream(valueObject.getReport());
 		try (Workbook workbook = new XSSFWorkbook(file)) {
 			Sheet sheet = workbook.getSheetAt(0);
 
-			int reportProductCount = 0;
 			Row headerRow = TableUtils.getHeaderRow(sheet, "Name");
-			int headerRowIndex = TableUtils.getIndexOfRow(sheet, headerRow);
-			int productNameColumnIndex = TableUtils.getColumnIndex(headerRow, "Name");
+			int productColumnIndex = TableUtils.getColumnIndex(headerRow, "Name");
 			int priceListColumnIndex = TableUtils.getColumnIndex(headerRow, "Price List");
-			for (int i = headerRowIndex + 1; i <= sheet.getLastRowNum(); i++) {
-				if (sheet.getRow(i).getCell(productNameColumnIndex).getCellType().equals(CellType.STRING) &&
-						!sheet.getRow(i).getCell(productNameColumnIndex).getStringCellValue().isEmpty()) {
-					reportProductCount++;
-				}
-				if (sheet.getRow(i).getCell(priceListColumnIndex).getCellType().equals(CellType.STRING) &&
-						!sheet.getRow(i).getCell(priceListColumnIndex).getStringCellValue().isEmpty()) {
-					
-				}
-				
-				
-			}
-			assertEquals(sheet.getRow(0).getCell(priceListColumnIndex).getStringCellValue(), "Price List");
-			assertEquals(productCount, reportProductCount, "All active products returned on the report");
+
+			Optional<Row> product1Row =
+					StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(productColumnIndex) != null &&
+							row.getCell(productColumnIndex).getCellType().equals(CellType.STRING) &&
+							row.getCell(productColumnIndex).getStringCellValue().equals(product1.getName())).findFirst();
+			assertTrue(product1Row.isPresent(), "First product appears on the report");
+			assertEquals(product1Row.get().getCell(priceListColumnIndex).getStringCellValue(), priceList1.getName(),
+					"First products shows the correct price list");
+
+			Optional<Row> product2Row =
+					StreamSupport.stream(sheet.spliterator(), false).filter(row -> row.getCell(productColumnIndex) != null &&
+							row.getCell(productColumnIndex).getCellType().equals(CellType.STRING) &&
+							row.getCell(productColumnIndex).getStringCellValue().equalsIgnoreCase(product2.getName())).findFirst();
+			assertTrue(product2Row.isPresent(), "Second product appears on the report");
+			assertEquals(product2Row.get().getCell(priceListColumnIndex).getStringCellValue(), priceList2.getName(),
+					"Second products shows the correct price list");
 		}
 	}
 }
