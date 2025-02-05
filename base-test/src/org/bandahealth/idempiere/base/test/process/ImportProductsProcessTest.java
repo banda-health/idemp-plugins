7035a2a440a91c4bfd951423afe7bfaaa42f2511
brianmituka <mitukab@outlook.com>
2025-02-05 18:43:47 +0300

    Add tests

--- base-test/src/org/bandahealth/idempiere/base/test/process/ImportProductsProcessTest.java
+++ base-test/src/org/bandahealth/idempiere/base/test/process/ImportProductsProcessTest.java
@@ -11,8 +11,15 @@ import org.bandahealth.idempiere.base.model.MSerNoCtl_BH;
 import org.bandahealth.idempiere.base.model.MWarehouse_BH;
 import org.bandahealth.idempiere.base.model.X_BH_I_Product_Quantity;
 import org.bandahealth.idempiere.base.process.ImportProductsProcess;
+import org.compiere.model.I_M_PriceList;
+import org.compiere.model.MDiscountSchema;
+import org.compiere.model.MPriceList;
+import org.compiere.model.MPriceListVersion;
+import org.compiere.model.MProductPrice;
 import org.compiere.model.PO;
 import org.compiere.model.Query;
+import org.compiere.model.X_M_DiscountSchema;
+import org.compiere.model.X_M_PriceList;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
@@ -21,6 +28,7 @@ import org.hamcrest.Matchers;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,9 +55,10 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		if (!valueObject.getWarehouse().isBH_DefaultWarehouse()) {
 			// See if any warehouse in the DB is default for this client
 			List<MWarehouse_BH> clientWarehouses = new Query(valueObject.getContext(), MWarehouse_BH.Table_Name,
-					MWarehouse_BH.COLUMNNAME_AD_Client_ID + "=? AND " + MWarehouse_BH.COLUMNNAME_BH_DefaultWarehouse + "=?",
-					get_TrxName()).setParameters(valueObject.getClient().getAD_Client_ID(), "Y").setOnlyActiveRecords(true)
-					.list();
+					MWarehouse_BH.COLUMNNAME_AD_Client_ID + "=? AND " + MWarehouse_BH.COLUMNNAME_BH_DefaultWarehouse
+							+ "=?",
+					get_TrxName()).setParameters(valueObject.getClient().getAD_Client_ID(), "Y")
+					.setOnlyActiveRecords(true).list();
 			if (clientWarehouses.isEmpty()) {
 				valueObject.getWarehouse().setBH_DefaultWarehouse(true);
 				valueObject.getWarehouse().saveEx();
@@ -59,13 +68,12 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		}
 
 		// Check the default serial number control
-		List<MSerNoCtl_BH> serialNumberControls =
-				new Query(valueObject.getContext(), MSerNoCtl_BH.Table_Name, MSerNoCtl_BH.COLUMNNAME_AD_Client_ID + "=?",
-						valueObject.getTransactionName()).setParameters(valueObject.getClient().getAD_Client_ID())
-						.setOnlyActiveRecords(true).list();
+		List<MSerNoCtl_BH> serialNumberControls = new Query(valueObject.getContext(), MSerNoCtl_BH.Table_Name,
+				MSerNoCtl_BH.COLUMNNAME_AD_Client_ID + "=?", valueObject.getTransactionName())
+				.setParameters(valueObject.getClient().getAD_Client_ID()).setOnlyActiveRecords(true).list();
 		if (serialNumberControls.isEmpty()) {
-			MSerNoCtl_BH serialNumberControl =
-					new MSerNoCtl_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
+			MSerNoCtl_BH serialNumberControl = new MSerNoCtl_BH(valueObject.getContext(), 0,
+					valueObject.getTransactionName());
 			serialNumberControl.setName("Default Serial Counter");
 			serialNumberControl.setStartNo(100);
 			serialNumberControl.setIncrementNo(1);
@@ -77,7 +85,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		// Ensure the attribute sets exist
 		MAttributeSet_BH expiringAttributeSet = new Query(valueObject.getContext(), MAttributeSet_BH.Table_Name,
 				MAttributeSet_BH.COLUMNNAME_BH_Locked + "=? AND " + MAttributeSet_BH.COLUMNNAME_IsGuaranteeDate + "=?",
-				valueObject.getTransactionName()).setParameters(true, true).setClient_ID().setOnlyActiveRecords(true).first();
+				valueObject.getTransactionName()).setParameters(true, true).setClient_ID().setOnlyActiveRecords(true)
+				.first();
 		if (expiringAttributeSet == null) {
 			expiringAttributeSet = new MAttributeSet_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
 			expiringAttributeSet.setName("With Expiry");
@@ -88,9 +97,11 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		}
 		MAttributeSet_BH nonExpiringAttributeSet = new Query(valueObject.getContext(), MAttributeSet_BH.Table_Name,
 				MAttributeSet_BH.COLUMNNAME_BH_Locked + "=? AND " + MAttributeSet_BH.COLUMNNAME_IsGuaranteeDate + "=?",
-				valueObject.getTransactionName()).setParameters(true, false).setClient_ID().setOnlyActiveRecords(true).first();
+				valueObject.getTransactionName()).setParameters(true, false).setClient_ID().setOnlyActiveRecords(true)
+				.first();
 		if (nonExpiringAttributeSet == null) {
-			nonExpiringAttributeSet = new MAttributeSet_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
+			nonExpiringAttributeSet = new MAttributeSet_BH(valueObject.getContext(), 0,
+					valueObject.getTransactionName());
 			nonExpiringAttributeSet.setName("Without Expiry");
 			nonExpiringAttributeSet.setIsGuaranteeDate(false);
 			nonExpiringAttributeSet.setBH_Locked(true);
@@ -122,15 +133,15 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
 
 		valueObject.setStepName("Create duplicate products");
-		X_BH_I_Product_Quantity duplicateProduct1 =
-				new X_BH_I_Product_Quantity(valueObject.getContext(), 0, valueObject.getTransactionName());
+		X_BH_I_Product_Quantity duplicateProduct1 = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
 		duplicateProduct1.setName(valueObject.getScenarioName());
 		duplicateProduct1.setCategoryName("Standard");
 		duplicateProduct1.setBH_BuyPrice(new BigDecimal(5));
 		duplicateProduct1.setBH_SellPrice(new BigDecimal(25));
 		duplicateProduct1.saveEx();
-		X_BH_I_Product_Quantity duplicateProduct2 =
-				new X_BH_I_Product_Quantity(valueObject.getContext(), 0, valueObject.getTransactionName());
+		X_BH_I_Product_Quantity duplicateProduct2 = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
 		duplicateProduct2.setName(valueObject.getScenarioName());
 		duplicateProduct2.setCategoryName("Standard");
 		duplicateProduct2.setBH_BuyPrice(new BigDecimal(5));
@@ -139,8 +150,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 
 		valueObject.setStepName("Create product that expires but has no expiration date");
 		valueObject.setRandom();
-		X_BH_I_Product_Quantity expiresWithoutExpiration =
-				new X_BH_I_Product_Quantity(valueObject.getContext(), 0, valueObject.getTransactionName());
+		X_BH_I_Product_Quantity expiresWithoutExpiration = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
 		expiresWithoutExpiration.setName(valueObject.getScenarioName());
 		expiresWithoutExpiration.setCategoryName("Standard");
 		expiresWithoutExpiration.setBH_HasExpiration(true);
@@ -152,8 +163,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 
 		valueObject.setStepName("Create normal product");
 		valueObject.setRandom();
-		X_BH_I_Product_Quantity normalProduct =
-				new X_BH_I_Product_Quantity(valueObject.getContext(), 0, valueObject.getTransactionName());
+		X_BH_I_Product_Quantity normalProduct = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
 		normalProduct.setName(valueObject.getScenarioName());
 		normalProduct.setCategoryName("Standard");
 		normalProduct.setBH_BuyPrice(new BigDecimal("63.875"));
@@ -162,8 +173,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 
 		commitEx();
 
-		int numberOfProducts =
-				new Query(valueObject.getContext(), MProduct_BH.Table_Name, null, valueObject.getTransactionName()).count();
+		int numberOfProducts = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
 		valueObject.setStepName("Run the product import process");
 		valueObject.setProcessUuid(processUuid);
 		valueObject.setProcessRecordId(0);
@@ -173,27 +184,20 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 						new BigDecimal(valueObject.getClient().getAD_Client_ID()), null, null, null),
 				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_HANDLE_EXISTING_PRODUCTS,
 						ImportProductsProcess.HANDLE_EXISTING_PRODUCTS_ERROR, null, null, null),
-				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_DELETE_OLD_IMPORTED, "Y", null, null, null)
-		));
+				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_DELETE_OLD_IMPORTED, "Y", null, null,
+						null)));
 		ChuBoeCreateEntity.runProcess(valueObject);
 		assertThat("Process ran successfully", valueObject.getErrorMessage(), Matchers.is(Matchers.nullValue()));
 		commitEx();
 
-		int numberOfProductsAfterImport =
-				new Query(valueObject.getContext(), MProduct_BH.Table_Name, null, valueObject.getTransactionName()).count();
+		int numberOfProductsAfterImport = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
 		assertEquals(numberOfProducts, numberOfProductsAfterImport, "No new products were imported");
-		addAssertionSQL(
-				"WITH products_to_import as (" +
-						"	SELECT *" +
-						"	FROM bh_i_product_quantity" +
-						"	WHERE ad_client_id = " + valueObject.getClient().getAD_Client_ID() +
-						") " +
-						"SELECT " +
-						"	'Assert all import records are still editable', " +
-						"	(" +
-						"		SELECT (SELECT COUNT(*) FROM products_to_import) = " +
-						"     (SELECT COUNT(*) FROM products_to_import WHERE processing = 'N') " +
-						"	)");
+		addAssertionSQL("WITH products_to_import as (" + "	SELECT *" + "	FROM bh_i_product_quantity"
+				+ "	WHERE ad_client_id = " + valueObject.getClient().getAD_Client_ID() + ") " + "SELECT "
+				+ "	'Assert all import records are still editable', " + "	("
+				+ "		SELECT (SELECT COUNT(*) FROM products_to_import) = "
+				+ "     (SELECT COUNT(*) FROM products_to_import WHERE processing = 'N') " + "	)");
 
 		ChuBoeAssert.executeSQLAsserts(getAssertionSQL(), valueObject.getContext(), valueObject.getTransactionName());
 	}
@@ -206,8 +210,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 
 		valueObject.setStepName("Create product that expires with an expiration date too far into the future");
 		valueObject.setRandom();
-		X_BH_I_Product_Quantity tooFarExpirationProduct =
-				new X_BH_I_Product_Quantity(valueObject.getContext(), 0, valueObject.getTransactionName());
+		X_BH_I_Product_Quantity tooFarExpirationProduct = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
 		tooFarExpirationProduct.setName(valueObject.getRandomNumber() + valueObject.getScenarioName());
 		tooFarExpirationProduct.setCategoryName("Standard");
 		tooFarExpirationProduct.setBH_HasExpiration(true);
@@ -220,8 +224,8 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 
 		commitEx();
 
-		int numberOfProducts =
-				new Query(valueObject.getContext(), MProduct_BH.Table_Name, null, valueObject.getTransactionName()).count();
+		int numberOfProducts = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
 		valueObject.setStepName("Run the product import process");
 		valueObject.setProcessUuid(processUuid);
 		valueObject.setProcessRecordId(0);
@@ -231,14 +235,14 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 						new BigDecimal(valueObject.getClient().getAD_Client_ID()), null, null, null),
 				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_HANDLE_EXISTING_PRODUCTS,
 						ImportProductsProcess.HANDLE_EXISTING_PRODUCTS_ERROR, null, null, null),
-				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_DELETE_OLD_IMPORTED, "Y", null, null, null)
-		));
+				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_DELETE_OLD_IMPORTED, "Y", null, null,
+						null)));
 		ChuBoeCreateEntity.runProcess(valueObject);
 		assertThat("Process ran successfully", valueObject.getErrorMessage(), Matchers.is(Matchers.nullValue()));
 		commitEx();
 
-		int numberOfProductsAfterImport =
-				new Query(valueObject.getContext(), MProduct_BH.Table_Name, null, valueObject.getTransactionName()).count();
+		int numberOfProductsAfterImport = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
 		assertEquals(numberOfProducts + 1, numberOfProductsAfterImport, "Product was imported");
 	}
 
@@ -248,11 +252,108 @@ public class ImportProductsProcessTest extends ChuBoePopulateFactoryVO {
 		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
 		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
 
-		List<PO> productQuantityImportRecords =
-				new Query(valueObject.getContext(), X_BH_I_Product_Quantity.Table_Name,
-						X_BH_I_Product_Quantity.COLUMNNAME_AD_Client_ID + "=?", valueObject.getTransactionName()).setParameters(
-						valueObject.getClient().getAD_Client_ID()).list();
+		List<PO> productQuantityImportRecords = new Query(valueObject.getContext(), X_BH_I_Product_Quantity.Table_Name,
+				X_BH_I_Product_Quantity.COLUMNNAME_AD_Client_ID + "=?", valueObject.getTransactionName())
+				.setParameters(valueObject.getClient().getAD_Client_ID()).list();
 		productQuantityImportRecords.forEach(productQuantityImportRecord -> productQuantityImportRecord.deleteEx(true));
 		commitEx();
+	}
+
+	@IPopulateAnnotation.CanRun
+	public void canImportProductsWithDifferentPriceLists() throws SQLException, IOException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		valueObject.setStepName("Create products with different price lists");
+		MPriceList priceList1= new MPriceList(valueObject.getContext(), 0, valueObject.getTransactionName());
+		priceList1.setName(valueObject.getScenarioName());
+		priceList1.setIsSOPriceList(true);
+		priceList1.saveEx();
+		
+		Timestamp datePriceList = valueObject.getDatePriceList();
+		if (datePriceList == null) {
+			datePriceList = ChuBoeCreateEntity.getDateOffset(valueObject.getDate(), -365);
+		}
+		
+		MDiscountSchema schema = new Query(valueObject.getContext(),
+				X_M_DiscountSchema.Table_Name,
+				"discounttype = '" + X_M_DiscountSchema.DISCOUNTTYPE_Pricelist + "'",
+				valueObject.getTransactionName())
+				.setClient_ID()
+				.first();
+
+		MPriceListVersion priceListVersion1 = new MPriceListVersion(valueObject.getContext(), 0, valueObject.getTransactionName());
+		priceListVersion1.setAD_Org_ID(0);
+		priceListVersion1.setName(datePriceList + "; IsSOTrx=Y; " + valueObject.getRandomNumber());
+		priceListVersion1.setDescription(valueObject.getStepMessageLong());
+		priceListVersion1.setM_PriceList_ID(priceList1.get_ID());
+		priceListVersion1.setValidFrom(datePriceList);
+		priceListVersion1.setM_DiscountSchema_ID(schema.get_ID());
+		priceListVersion1.saveEx(); 
+		
+		X_BH_I_Product_Quantity product1 = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
+		product1.setName(valueObject.getScenarioName());
+		product1.setCategoryName("Product1");
+		product1.setBH_BuyPrice(new BigDecimal(5));
+		product1.setBH_SellPrice(new BigDecimal(25));
+		
+		product1.saveEx();
+		
+		MPriceList priceList2= new MPriceList(valueObject.getContext(), 0, valueObject.getTransactionName());
+		priceList2.setName(valueObject.getScenarioName());
+		priceList2.setIsSOPriceList(true);
+		priceList2.saveEx();
+		
+		MPriceListVersion priceListVersion2 = new MPriceListVersion(valueObject.getContext(), 0, valueObject.getTransactionName());
+		priceListVersion2.setAD_Org_ID(0);
+		priceListVersion2.setName(datePriceList + "; IsSOTrx=Y; " + valueObject.getRandomNumber());
+		priceListVersion2.setDescription(valueObject.getStepMessageLong());
+		priceListVersion2.setM_PriceList_ID(priceListVersion2.get_ID());
+		priceListVersion2.setValidFrom(datePriceList);
+		priceListVersion2.setM_DiscountSchema_ID(schema.get_ID());
+		priceListVersion2.saveEx(); 
+	
+		
+		X_BH_I_Product_Quantity product2 = new X_BH_I_Product_Quantity(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
+		product2.setName(valueObject.getScenarioName());
+		product2.setCategoryName("Product2");
+		product2.setBH_BuyPrice(new BigDecimal(5));
+		product2.setBH_SellPrice(new BigDecimal(30));
+		product2.saveEx();
+
+		int numberOfProducts = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
+		valueObject.setStepName("Run the product import process");
+		valueObject.setProcessUuid(processUuid);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Arrays.asList(
+				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_AD_CLIENT_ID,
+						new BigDecimal(valueObject.getClient().getAD_Client_ID()), null, null, null),
+				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_HANDLE_EXISTING_PRODUCTS,
+						ImportProductsProcess.HANDLE_EXISTING_PRODUCTS_ERROR, null, null, null),
+				new ProcessInfoParameter(ImportProductsProcess.PARAMETERNAME_DELETE_OLD_IMPORTED, "Y", null, null,
+						null)));
+		ChuBoeCreateEntity.runProcess(valueObject);
+		assertThat("Process ran successfully", valueObject.getErrorMessage(), Matchers.is(Matchers.nullValue()));
+		commitEx();
+
+		int numberOfProductsAfterImport = new Query(valueObject.getContext(), MProduct_BH.Table_Name, null,
+				valueObject.getTransactionName()).count();
+		assertEquals(numberOfProducts, numberOfProductsAfterImport, "No new products were imported");
+		
+		List<MProductPrice> productPrices = new Query(valueObject.getContext(), MProductPrice.Table_Name, null,
+				valueObject.getTransactionName()).list();	
+		
+		productPrices.stream()		
+		
+	}
+	
+	@IPopulateAnnotation.CanRun
+	public void canNotImportProductsWithInvalidPriceLists() throws SQLException, IOException {
+		
 	}
 }
