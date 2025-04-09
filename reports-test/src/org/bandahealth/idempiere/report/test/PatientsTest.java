4b7a5821bc17599958b51396b070b6da35684d7b
brianmituka <mitukab@outlook.com>
2025-04-09 12:51:44 +0300

    Update tests

--- reports-test/src/org/bandahealth/idempiere/report/test/PatientsTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/PatientsTest.java
@@ -4,11 +4,28 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bandahealth.idempiere.report.test.utils.PDFUtils;
+import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
+import org.compiere.model.MBPGroup;
+import org.compiere.model.Query;
+import org.compiere.process.ProcessInfoParameter;
 import org.hamcrest.Matchers;
 
 import com.chuboe.test.populate.ChuBoeCreateEntity;
@@ -49,5 +66,43 @@ public class PatientsTest extends ChuBoePopulateFactoryVO {
 		String reportContent = PDFUtils.readPdfContent(valueObject.getReport(), true);
 		assertThat("Patient's name is on the report", reportContent,
 				containsString(valueObject.getBusinessPartner().getName().substring(0, 30)));
+	}
+	
+	@IPopulateAnnotation.CanRun
+	public void patientsReportIsGeneratedCorrectly() throws SQLException, IOException{
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		MBPGroup clientBusinessPartnerGroup =
+				new Query(valueObject.getContext(), MBPGroup.Table_Name, MBPGroup.COLUMNNAME_Name + "=?",
+						valueObject.getTransactionName()).setOnlyActiveRecords(true).setParameters("Patients - DO NOT CHANGE").first();
+		
+		valueObject.setStepName("Create business partner 1");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		valueObject.getBusinessPartner().setTotalOpenBalance(new BigDecimal(1000));
+		valueObject.getBusinessPartner().setC_BP_Group_ID(clientBusinessPartnerGroup.get_ID());
+		commitEx();
+		valueObject.setStepName("Create business partner 2 without group");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		valueObject.getBusinessPartner().setTotalOpenBalance(new BigDecimal(1000));
+		commitEx();
+		
+		valueObject.setStepName("Generate the report to get initial data");
+		valueObject.setProcessUuid(patientsReportUuid);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+		
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			Optional<Row> titleRow = StreamSupport
+					.stream(sheet.spliterator(), false).filter(
+							row -> StreamSupport.stream(row.spliterator(), false)
+									.anyMatch(cell -> cell != null && cell.getCellType().equals(CellType.STRING)
+											&& cell.getStringCellValue().contains("Patient Report")))
+					.findFirst();
+			assertTrue(titleRow.isPresent(), "title is present");
+		}
+		
 	}
 }
