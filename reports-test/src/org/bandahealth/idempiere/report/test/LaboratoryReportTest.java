28b530a1f68c23e946a5dcb6a5ac8548960c4991
brianmituka <mitukab@outlook.com>
2025-04-29 16:31:56 +0300

    Updated Report tests

--- reports-test/src/org/bandahealth/idempiere/report/test/LaboratoryReportTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/LaboratoryReportTest.java
@@ -409,4 +409,101 @@ public class LaboratoryReportTest extends ChuBoePopulateFactoryVO {
 					"Status message is correct");
 		}
 	}
+	
+	@IPopulateAnnotation.CanRun
+	public void labNotesAreDisplayedCorrectly() throws SQLException, IOException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		valueObject.setStepName("Create business partner");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create product");
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		valueObject.getOrderLine().setPrice(new BigDecimal(20));
+		valueObject.getOrderLine().setQty(new BigDecimal(10));
+		valueObject.getOrderLine().saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create diagnostic concept");
+		valueObject.setRandom();
+		MBHConcept diagnostic = new MBHConcept(valueObject.getContext(), 0, valueObject.getTransactionName());
+		diagnostic.setBH_Display_Name(String.valueOf(valueObject.getRandomNumber()));
+		diagnostic.setOcl_Uuid(String.valueOf(valueObject.getRandomNumber()));
+		diagnostic.saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create concept extra");
+		valueObject.setRandom();
+		MBHConceptExtra conceptExtra = new MBHConceptExtra(valueObject.getContext(), 0, valueObject.getTransactionName());
+		conceptExtra.setBH_Key("hi_normal");
+		conceptExtra.setBH_Value("5");
+		conceptExtra.setBH_Concept_ID(diagnostic.getBH_Concept_ID());
+		conceptExtra.saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create visit");
+		ChuBoeCreateEntity.createVisit(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create diagnostics");
+		MBHEncounter encounter = new MBHEncounter(valueObject.getContext(), 0, valueObject.getTransactionName());
+		encounter.setBH_Encounter_Type(MBHEncounter.BH_ENCOUNTER_TYPE_ClinicalDetails);
+		encounter.setBH_Visit_ID(valueObject.getVisit().get_ID());
+		encounter.setBH_Encounter_Date(TimestampUtils.today());
+		encounter.saveEx();
+
+		String diagnosticValue = "10";
+		MBHEncounterDiagnostic encounterDiagnostic = new MBHEncounterDiagnostic(valueObject.getContext(), 0,
+				valueObject.getTransactionName());
+		encounterDiagnostic.setBH_Encounter_ID(encounter.getBH_Encounter_ID());
+		encounterDiagnostic.setBH_Concept_ID(diagnostic.get_ID());
+		encounterDiagnostic.setBH_Value(diagnosticValue);
+		encounterDiagnostic.setLineNo(10);
+		encounterDiagnostic.setBH_Diagnostic_Note("Are within range");
+		encounterDiagnostic.setGroup1(String.valueOf(valueObject.getRandomNumber()));
+		encounterDiagnostic.saveEx();
+
+		valueObject.setStepName("Create sales order");
+		valueObject.setRandom();
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true,
+				false, false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		MOrder_BH order = valueObject.getOrder();
+		order.setSalesRep_ID(valueObject.getUser().get_ID());
+		order.saveEx();
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid("1a7175fe-2afe-4404-9c56-58d2fda9bc57");
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(
+				List.of(new ProcessInfoParameter("BH_Visit_UU", valueObject.getVisit().get_UUID(), null, null, null)));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			Row headerRow = TableUtils.getHeaderRow(sheet, "Notes");
+			int statusColumnIndex = TableUtils.getColumnIndex(headerRow, "Notes");
+
+			List<Row> testRows = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> row.getCell(statusColumnIndex) != null &&
+							row.getCell(statusColumnIndex).getCellType().equals(CellType.STRING) &&
+							row.getCell(statusColumnIndex).getStringCellValue().contains("Are within range")).collect(Collectors.toList());
+			assertFalse(testRows.isEmpty(), "Notes Appear");
+		}
+	}
+
 }
