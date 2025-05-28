05601149625280767aec9babb9e179bc34b22311
Andrew <andrewndungu@gmail.com>
2025-05-28 18:37:36 +0300

    add financial stored functions

---
+++ migration/processes_post_migration/postgresql/bh_dashboard_get_financial_historical.sql
@@ -0,0 +1,76 @@
+DROP FUNCTION IF EXISTS bh_dashboard_get_financial_historical(_ad_client_id numeric, _begin_date timestamp, _end_date timestamp);
+CREATE OR REPLACE FUNCTION bh_dashboard_get_financial_historical(_ad_client_id numeric, _begin_date timestamp, _end_date timestamp)
+	RETURNS table
+	        (
+		        bucket_value         timestamp,
+		        total_income         numeric,
+		        total_expenses		 numeric,
+		        net_profit           numeric
+	        )
+	LANGUAGE sql
+	STABLE
+AS
+$$
+WITH expenses AS (
+SELECT
+    COALESCE(SUM(i.grandtotal), 0)      AS total_expenses,
+    i.dateinvoiced                      AS date
+FROM
+    c_invoice i
+WHERE
+    i.ad_client_id = _ad_client_id
+    AND i.docstatus = 'CO'
+    AND i.issotrx = 'N'
+    AND i.bh_visit_id IS NULL
+    AND i.dateinvoiced BETWEEN _begin_date AND _end_date
+    GROUP BY i.dateinvoiced
+),
+payments AS (
+	SELECT
+		COALESCE(SUM(p.payamt), 0)                  AS total_income,
+		p.datetrx                                   AS date
+	FROM
+		bh_get_visit_payments(_ad_client_id, _begin_date, _end_date) p
+	GROUP BY p.datetrx
+),
+payments_expenses AS (
+	SELECT
+		COALESCE(p.date, e.date)                        AS date,
+		COALESCE(p.total_income, 0)                     AS total_income,
+		COALESCE(e.total_expenses, 0)                   AS total_expenses,
+		COALESCE(p.total_income - e.total_expenses, 0)  AS net_profit
+	FROM
+		payments p
+	FULL OUTER JOIN expenses e ON p.date = e.date
+),
+buckets_cte AS (
+	SELECT
+		total_income,
+		total_expenses,
+		net_profit,
+		WIDTH_BUCKET(EXTRACT(EPOCH FROM date), EXTRACT(EPOCH FROM _begin_date),
+		             EXTRACT(EPOCH FROM _end_date), 6)                  						AS bucket_number
+	FROM
+		payments_expenses
+),
+bucket_mapping (bucket_number, bucket_value) AS (
+	VALUES
+		(1, _begin_date),
+		(2, _end_date - (_end_date - _begin_date) * 5 / 6),
+		(3, _end_date - (_end_date - _begin_date) * 4 / 6),
+		(4, _end_date - (_end_date - _begin_date) * 3 / 6),
+		(5, _end_date - (_end_date - _begin_date) * 2 / 6),
+		(6, _end_date - (_end_date - _begin_date) / 6)
+)
+SELECT
+	bm.bucket_value,
+	total_income,
+	total_expenses,
+	net_profit
+FROM
+	bucket_mapping bm
+LEFT JOIN buckets_cte bcte
+ON bcte.bucket_number = bm.bucket_number
+GROUP BY
+	bm.bucket_value, total_income, total_expenses, net_profit;
+$$;
