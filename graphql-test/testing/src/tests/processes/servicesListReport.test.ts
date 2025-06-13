cb1d6e02c6884a45c0cd22926ce9bfdc2409ea8c
Kevin <kwburnett@gmail.com>
2025-06-13 16:28:56 +0100

    fix: go-3316 > correct mistyped test

--- graphql-test/testing/src/tests/processes/servicesListReport.test.ts
+++ graphql-test/testing/src/tests/processes/servicesListReport.test.ts
@@ -1,7 +1,7 @@
 import { PdfData } from 'pdfdataextract';
 import { Ad_ProcessGetDocument } from '../../__generated__/graphql';
 import { query } from '../../api';
-import { runReport, tomorrow, yesterday } from '../../utils';
+import { runReport } from '../../utils';
 
 const reportUuid = 'fd5b6538-760c-4c8f-b943-115c1f3d2287';
 
@@ -15,27 +15,11 @@ test('services charged report is runnable', async () => {
 			variables: { Size: 1, Filter: JSON.stringify({ ad_process_uu: reportUuid }) },
 		})
 	).data.AD_ProcessGet.Results[0];
-	const beginDateParameter = process.AD_Process_ParaList?.find((parameter) => parameter.Name === 'Begin Date');
-	const endDateParameter = process.AD_Process_ParaList?.find((parameter) => parameter.Name === 'End Date');
-
-	expect(beginDateParameter).toBeTruthy();
-	expect(endDateParameter).toBeTruthy();
+	expect(process.AD_Process_ParaList).toBeFalsy();
 
 	valueObject.stepName = 'Run report';
 	valueObject.processUuid = process.UU;
-		valueObject.processInformationParameters = [
-			{
-				AD_Process: { UU: process.UU },
-				ParameterName: beginDateParameter!.Name,
-				Parameter: yesterday().toISOString(),
-			},
-			{
-				AD_Process: { UU: process.UU },
-				ParameterName: endDateParameter!.Name,
-				Parameter: tomorrow().toISOString(),
-			},
-		];
 	await runReport(valueObject);
 
-	expect((await PdfData.extract(valueObject.report!)).text?.join('')).toContain('Stock Transfers Between');
+	expect((await PdfData.extract(valueObject.report!)).text?.join('')).toContain('Service List');
 });
--- graphql-test/testing/src/tests/processes/stockTransferReport.test.ts
+++ graphql-test/testing/src/tests/processes/stockTransferReport.test.ts
@@ -1,9 +1,9 @@
 import { PdfData } from 'pdfdataextract';
 import { Ad_ProcessGetDocument } from '../../__generated__/graphql';
 import { query } from '../../api';
-import { runReport } from '../../utils';
+import { runReport, tomorrow, yesterday } from '../../utils';
 
-const reportUuid = 'fd5b6538-760c-4c8f-b943-115c1f3d2287';
+const reportUuid = '5a666f24-469a-43dc-865f-4053e0dd4fd6';
 
 test('stock transfer report is runnable', async () => {
 	const valueObject = globalThis.__VALUE_OBJECT__;
@@ -15,11 +15,28 @@ test('stock transfer report is runnable', async () => {
 			variables: { Size: 1, Filter: JSON.stringify({ ad_process_uu: reportUuid }) },
 		})
 	).data.AD_ProcessGet.Results[0];
-	expect(process.AD_Process_ParaList).toBeFalsy();
+	const beginDateParameter = process.AD_Process_ParaList?.find((parameter) => parameter.Name === 'Begin Date');
+	const endDateParameter = process.AD_Process_ParaList?.find((parameter) => parameter.Name === 'End Date');
+
+	expect(beginDateParameter).toBeTruthy();
+	expect(endDateParameter).toBeTruthy();
 
 	valueObject.stepName = 'Run report';
 	valueObject.processUuid = process.UU;
+	valueObject.processUuid = process.UU;
+	valueObject.processInformationParameters = [
+		{
+			AD_Process: { UU: process.UU },
+			ParameterName: beginDateParameter!.Name,
+			Parameter: yesterday().toISOString(),
+		},
+		{
+			AD_Process: { UU: process.UU },
+			ParameterName: endDateParameter!.Name,
+			Parameter: tomorrow().toISOString(),
+		},
+	];
 	await runReport(valueObject);
 
-	expect((await PdfData.extract(valueObject.report!)).text?.join('')).toContain('Service List');
+	expect((await PdfData.extract(valueObject.report!)).text?.join('')).toContain('Stock Transfers Between');
 });
