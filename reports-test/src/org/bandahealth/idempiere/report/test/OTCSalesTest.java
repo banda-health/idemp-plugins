7d7d459e12fde437a432afdb8e8953bc9acc8c20
Kevin <kwburnett@gmail.com>
2025-07-01 19:19:00 +0100

    feat: go-3344 > add totals by cashier to OTC sales report

--- reports-test/src/org/bandahealth/idempiere/report/test/OTCSalesTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/OTCSalesTest.java
@@ -4,6 +4,7 @@ import com.chuboe.test.populate.ChuBoeCreateEntity;
 import com.chuboe.test.populate.ChuBoePopulateFactoryVO;
 import com.chuboe.test.populate.ChuBoePopulateVO;
 import com.chuboe.test.populate.IPopulateAnnotation;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -12,11 +13,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.bandahealth.idempiere.base.model.MBPGroup_BH;
 import org.bandahealth.idempiere.base.model.MDocType_BH;
 import org.bandahealth.idempiere.base.model.MOrderLine_BH;
+import org.bandahealth.idempiere.base.model.MOrder_BH;
 import org.bandahealth.idempiere.base.model.MPayment_BH;
 import org.bandahealth.idempiere.base.model.MProduct_BH;
 import org.bandahealth.idempiere.report.test.utils.TableUtils;
 import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
 import org.compiere.model.Query;
+import org.compiere.process.DocAction;
 import org.compiere.process.DocumentEngine;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.util.Env;
@@ -26,8 +29,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -41,7 +42,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OTCSalesTest extends ChuBoePopulateFactoryVO {
-	private static final String patientTransactionReportUuid = "b8508f0a-c66f-4030-a88c-3ae383322ceb";
+	private static final String reportUU = "b8508f0a-c66f-4030-a88c-3ae383322ceb";
+	private MBPGroup_BH otcBusinessPartnerGroup;
 
 	@IPopulateAnnotation.CanRunBeforeClass
 	public void prepareIt() throws Exception {
@@ -52,6 +54,17 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 		valueObject.setStepName("Open needed periods");
 		ChuBoeCreateEntity.createAndOpenAllFiscalYears(valueObject);
 		commitEx();
+
+		otcBusinessPartnerGroup =
+				new Query(valueObject.getContext(), MBPGroup_BH.Table_Name, MBPGroup_BH.COLUMNNAME_Name + "=?",
+						valueObject.getTransactionName()).setParameters(MBPGroup_BH.NAME_OTC).setClient_ID().first();
+		if (otcBusinessPartnerGroup == null) {
+			otcBusinessPartnerGroup = new MBPGroup_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
+			otcBusinessPartnerGroup.setValue(MBPGroup_BH.NAME_OTC);
+			otcBusinessPartnerGroup.setDescription("DO NOT CHANGE");
+			otcBusinessPartnerGroup.setName(MBPGroup_BH.NAME_OTC);
+			otcBusinessPartnerGroup.saveEx();
+		}
 	}
 
 	@IPopulateAnnotation.CanRun
@@ -62,16 +75,6 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 
 		valueObject.setStepName("Create business partner");
 		ChuBoeCreateEntity.createBusinessPartner(valueObject);
-		MBPGroup_BH otcBusinessPartnerGroup =
-				new Query(valueObject.getContext(), MBPGroup_BH.Table_Name, MBPGroup_BH.COLUMNNAME_Name + "=?",
-						valueObject.getTransactionName()).setParameters(MBPGroup_BH.NAME_OTC).setClient_ID().first();
-		if (otcBusinessPartnerGroup == null) {
-			otcBusinessPartnerGroup = new MBPGroup_BH(valueObject.getContext(), 0, valueObject.getTransactionName());
-			otcBusinessPartnerGroup.setValue(MBPGroup_BH.NAME_OTC);
-			otcBusinessPartnerGroup.setDescription("DO NOT CHANGE");
-			otcBusinessPartnerGroup.setName(MBPGroup_BH.NAME_OTC);
-			otcBusinessPartnerGroup.saveEx();
-		}
 		valueObject.getBusinessPartner().setBPGroup(otcBusinessPartnerGroup);
 		valueObject.getBusinessPartner().saveEx();
 		commitEx();
@@ -104,7 +107,7 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Generate the report");
-		valueObject.setProcessUuid(patientTransactionReportUuid);
+		valueObject.setProcessUuid(reportUU);
 		valueObject.setProcessRecordId(0);
 		valueObject.setProcessTableId(0);
 		valueObject.setProcessInformationParameters(Arrays.asList(
@@ -136,9 +139,8 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 
 		valueObject.setStepName("Create business partner");
 		ChuBoeCreateEntity.createBusinessPartner(valueObject);
-		valueObject.getBusinessPartner().setName(valueObject.getBusinessPartner().getName().substring(0, 19));
+		valueObject.getBusinessPartner().setBPGroup(otcBusinessPartnerGroup);
 		valueObject.getBusinessPartner().saveEx();
-		valueObject.setRandom();
 		commitEx();
 
 		valueObject.setStepName("Create product 1");
@@ -166,8 +168,6 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Create visit");
-		Timestamp date = Timestamp.valueOf(LocalDateTime.of(2024, 11, 30, 0, 0));
-		valueObject.setDate(date);
 		ChuBoeCreateEntity.createVisit(valueObject);
 		commitEx();
 
@@ -209,7 +209,7 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Generate the invoice report");
-		valueObject.setProcessUuid(patientTransactionReportUuid);
+		valueObject.setProcessUuid(reportUU);
 		valueObject.setProcessRecordId(0);
 		valueObject.setProcessTableId(0);
 		valueObject.setProcessInformationParameters(Arrays.asList(
@@ -223,11 +223,163 @@ public class OTCSalesTest extends ChuBoePopulateFactoryVO {
 		try (Workbook workbook = new XSSFWorkbook(file)) {
 			Sheet sheet = workbook.getSheetAt(0);
 
-			Optional<Row> includedProductRow = StreamSupport.stream(sheet.spliterator(), false).filter(
+			Optional<Row> productRow = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
+							cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
+									cell.getStringCellValue().contains(valueObject.getProduct().getName()))).findFirst();
+			assertTrue(productRow.isPresent(), "Regular product is on the report");
+			productRow = StreamSupport.stream(sheet.spliterator(), false).filter(
 					row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
 							cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
 									cell.getStringCellValue().contains(product1.getName()))).findFirst();
-			assertTrue(includedProductRow.isEmpty(), "Included product is not on the report");
+			assertTrue(productRow.isEmpty(), "Included product is not on the report");
 		}
+	}
+
+	@IPopulateAnnotation.CanRun
+	public void totalsByCashierWorksCorrectly() throws SQLException, IOException {
+		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
+		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
+		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid(reportUU);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", TimestampUtils.startOfYesterday(), null, null, null),
+				new ProcessInfoParameter("End Date", TimestampUtils.endOfTomorrow(), null, null, null)
+		));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		FileInputStream file = new FileInputStream(valueObject.getReport());
+		double cashTotal = 0;
+		double mobileMoneyTotal = 0;
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			List<Row> totalsRows = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
+							cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
+									cell.getStringCellValue().equals("Total"))).collect(Collectors.toUnmodifiableList());
+			if (totalsRows.size() > 1) {
+				for (Cell cell : totalsRows.get(0)) {
+					if (cell != null && cell.getCellType().equals(CellType.STRING)) {
+						if (cell.getStringCellValue().equals("Cash")) {
+							cashTotal = totalsRows.get(1).getCell(cell.getColumnIndex()).getNumericCellValue();
+						} else if (cell.getStringCellValue().equals("Mobile Money")) {
+							mobileMoneyTotal = totalsRows.get(1).getCell(cell.getColumnIndex()).getNumericCellValue();
+						}
+					}
+				}
+			}
+		}
+
+		valueObject.setStepName("Create business partner");
+		ChuBoeCreateEntity.createBusinessPartner(valueObject);
+		valueObject.getBusinessPartner().setBPGroup(otcBusinessPartnerGroup);
+		valueObject.getBusinessPartner().saveEx();
+		commitEx();
+
+		valueObject.setStepName("Create first product");
+		valueObject.setSalesPrice(BigDecimal.TEN);
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+		MProduct_BH firstProduct = valueObject.getProduct();
+
+		valueObject.setStepName("Create purchase order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create second product");
+		valueObject.clearProduct();
+		valueObject.setSalesPrice(new BigDecimal(5));
+		ChuBoeCreateEntity.createProduct(valueObject);
+		commitEx();
+		MProduct_BH secondProduct = valueObject.getProduct();
+
+		valueObject.setStepName("Create purchase order");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create visit");
+		ChuBoeCreateEntity.createVisit(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create sales order");
+		valueObject.setProduct(firstProduct);
+		valueObject.setDocumentAction(DocAction.ACTION_Prepare);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
+				false);
+		ChuBoeCreateEntity.createOrder(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Add second order line");
+		valueObject.setProduct(secondProduct);
+		MOrder_BH order = valueObject.getOrder();
+		ChuBoeCreateEntity.createOrder(valueObject);
+		valueObject.setOrder(order);
+		valueObject.getOrderLine().setC_Order_ID(order.get_ID());
+		valueObject.getOrderLine().saveEx();
+		commitEx();
+
+		valueObject.setStepName("Complete the order");
+		order.setDocAction(DocAction.ACTION_Complete);
+		assertTrue(order.processIt(DocAction.ACTION_Complete), "Order was completed");
+		commitEx();
+
+		valueObject.setStepName("Create first payment");
+		valueObject.setDocumentAction(DocAction.ACTION_Complete);
+		valueObject.setTenderType(MPayment_BH.TENDERTYPE_Cash);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_ARReceipt, null, true, false, false);
+		valueObject.setPaymentAmount(new BigDecimal(9));
+		ChuBoeCreateEntity.createPayment(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Create second payment");
+		valueObject.setDocumentAction(DocAction.ACTION_Complete);
+		valueObject.setTenderType(MPayment_BH.TENDERTYPE_MPesa);
+		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_ARReceipt, null, true, false, false);
+		valueObject.setPaymentAmount(new BigDecimal(6));
+		ChuBoeCreateEntity.createPayment(valueObject);
+		commitEx();
+
+		valueObject.setStepName("Generate the report");
+		valueObject.setProcessUuid(reportUU);
+		valueObject.setProcessRecordId(0);
+		valueObject.setProcessTableId(0);
+		valueObject.setProcessInformationParameters(Arrays.asList(
+				new ProcessInfoParameter("Begin Date", TimestampUtils.startOfYesterday(), null, null, null),
+				new ProcessInfoParameter("End Date", TimestampUtils.endOfTomorrow(), null, null, null)
+		));
+		valueObject.setReportType("xlsx");
+		ChuBoeCreateEntity.runReport(valueObject);
+
+		file = new FileInputStream(valueObject.getReport());
+		double newCashTotal = 0;
+		double newMobileMoneyTotal = 0;
+		try (Workbook workbook = new XSSFWorkbook(file)) {
+			Sheet sheet = workbook.getSheetAt(0);
+			List<Row> totalsRows = StreamSupport.stream(sheet.spliterator(), false).filter(
+					row -> StreamSupport.stream(row.spliterator(), false).anyMatch(
+							cell -> cell != null && cell.getCellType().equals(CellType.STRING) &&
+									cell.getStringCellValue().equals("Total"))).collect(Collectors.toUnmodifiableList());
+			assertEquals(2, totalsRows.size(), "Totals rows are present");
+			for (Cell cell : totalsRows.get(0)) {
+				if (cell != null && cell.getCellType().equals(CellType.STRING)) {
+					if (cell.getStringCellValue().equals("Cash")) {
+						newCashTotal = totalsRows.get(1).getCell(cell.getColumnIndex()).getNumericCellValue();
+					} else if (cell.getStringCellValue().equals("Mobile Money")) {
+						newMobileMoneyTotal = totalsRows.get(1).getCell(cell.getColumnIndex()).getNumericCellValue();
+					}
+				}
+			}
+		}
+		assertEquals(cashTotal + 9, newCashTotal, "Cash total is correct");
+		assertEquals(mobileMoneyTotal + 6, newMobileMoneyTotal, "Mobile money total is correct");
 	}
 }
