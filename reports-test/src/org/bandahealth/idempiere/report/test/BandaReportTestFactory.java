b11f81e3d89f03de78ca766786f4ee1af8e493e9
brianmituka <mitukab@outlook.com>
2025-07-01 15:37:53 +0300

    Created the reports title and initial tests

--- reports-test/src/org/bandahealth/idempiere/report/test/BandaReportTestFactory.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/BandaReportTestFactory.java
@@ -28,6 +28,7 @@ public class BandaReportTestFactory implements IChuBoePopulateFactory {
 		tests.add(new MoH705BOutPatientOver5yrSummaryTest());
 		tests.add(new MoH706LabTestsSummaryTest());
 		tests.add(new MoH717NewAndRevisitPatientCountTest());
+		tests.add(new MoH747AContraceptivesConsumptionReportTest());
 		tests.add(new OpenBalanceListTest());
 		tests.add(new OpenBalanceInvoiceTest());
 		tests.add(new OpenBalanceReceiptTest());
