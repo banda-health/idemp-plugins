e3c0a1895ed5d211fb1b39884dbc8e3b99f30842
Kevin <kwburnett@gmail.com>
2025-07-10 19:10:48 +0100

    feat: go-3340 > update to use deletion time and order grand total

--- reports-test/src/org/bandahealth/idempiere/report/test/BandaReportTestFactory.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/BandaReportTestFactory.java
@@ -16,8 +16,8 @@ public class BandaReportTestFactory implements IChuBoePopulateFactory {
 		tests.add(new CashierTransactionDifferencesTest());
 		tests.add(new ChangesToInventoryTest());
 		tests.add(new DailyCashierCollectionsTest());
-		tests.add(new DiagnosisReportTest());
 		tests.add(new DeletedDraftedBillsReportTest());
+		tests.add(new DiagnosisReportTest());
 		tests.add(new DonorFundReportTest());
 		tests.add(new ExpensesTest());
 		tests.add(new ExpiredProductListTest());
--- reports-test/src/org/bandahealth/idempiere/report/test/DeletedDraftedBillsReportTest.java
+++ reports-test/src/org/bandahealth/idempiere/report/test/DeletedDraftedBillsReportTest.java
@@ -1,7 +1,6 @@
 package org.bandahealth.idempiere.report.test;
 
 import com.chuboe.test.populate.ChuBoeCreateEntity;
-import com.chuboe.test.populate.ChuBoeDeleteEntity;
 import com.chuboe.test.populate.ChuBoePopulateFactoryVO;
 import com.chuboe.test.populate.ChuBoePopulateVO;
 import com.chuboe.test.populate.IPopulateAnnotation;
@@ -10,14 +9,10 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.bandahealth.idempiere.base.model.MBHVoidedReason;
 import org.bandahealth.idempiere.base.model.MDocType_BH;
-import org.bandahealth.idempiere.base.model.MOrder_BH;
 import org.bandahealth.idempiere.base.model.MPayment_BH;
 import org.bandahealth.idempiere.report.test.utils.TableUtils;
 import org.bandahealth.idempiere.report.test.utils.TimestampUtils;
-import org.compiere.model.PO;
-import org.compiere.model.Query;
 import org.compiere.process.DocumentEngine;
 import org.compiere.process.ProcessInfoParameter;
 import org.hamcrest.Matchers;
@@ -29,7 +24,6 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.StreamSupport;
@@ -85,7 +79,7 @@ public class DeletedDraftedBillsReportTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Create sales order");
-		valueObject.setDocumentAction(null);
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Prepare);
 		valueObject.setQuantity(new BigDecimal(50));
 		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
 				false);
@@ -93,14 +87,17 @@ public class DeletedDraftedBillsReportTest extends ChuBoePopulateFactoryVO {
 		commitEx();
 
 		valueObject.setStepName("Create a payment");
+		valueObject.setDocumentAction(DocumentEngine.ACTION_Prepare);
 		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_ARReceipt, null, true, false, false);
 		valueObject.setTenderType(MPayment_BH.TENDERTYPE_Cash);
 		valueObject.setPaymentAmount(new BigDecimal(20));
 		ChuBoeCreateEntity.createPayment(valueObject);
 		commitEx();
-	    visitsUUs.add(valueObject.getVisit().getBH_Visit_UU());
+
 		valueObject.setStepName("Delete Visit");
-		ChuBoeDeleteEntity.deletevisits(valueObject, visitsUUs);
+		valueObject.getOrderLine().deleteEx(true);
+		valueObject.getOrder().deleteEx(true);
+		valueObject.getVisit().deleteEx(true);
 		commitEx();
 
 		valueObject.setStepName("Generate the report");
@@ -117,7 +114,7 @@ public class DeletedDraftedBillsReportTest extends ChuBoePopulateFactoryVO {
 		FileInputStream file = new FileInputStream(valueObject.getReport());
 		try (Workbook workbook = new XSSFWorkbook(file)) {
 			Sheet sheet = workbook.getSheetAt(0);
-			Row headerRow = TableUtils.getHeaderRow(sheet, "Datetime");
+			Row headerRow = TableUtils.getHeaderRow(sheet, "Deleted At");
 			int patientName = TableUtils.getColumnIndex(headerRow, "Patient Name");
 			int patientAmount = TableUtils.getColumnIndex(headerRow, "Bill Amount");
 			Optional<Row> patientRow = StreamSupport.stream(sheet.spliterator(), false).filter(
@@ -126,79 +123,7 @@ public class DeletedDraftedBillsReportTest extends ChuBoePopulateFactoryVO {
 							row.getCell(patientName).getStringCellValue()
 									.contains(valueObject.getBusinessPartner().getName())).findFirst();
 			assertTrue(patientRow.isPresent(), "Patient is present");
-			assertEquals(20,
-					patientRow.get().getCell(patientAmount).getNumericCellValue(), "Amount is correct");
-		}
-	}
-	@IPopulateAnnotation.CanRun
-	public void visitsWithCompletedOrdersAreNotDeleted() throws SQLException, IOException {
-		ChuBoePopulateVO valueObject = new ChuBoePopulateVO();
-		valueObject.prepareIt(getScenarioName(), true, get_TrxName());
-
-		assertThat("VO validation gives no errors", valueObject.getErrorMessage(), is(nullValue()));
-		Set<String> visitsUUs = new HashSet<>();
-		valueObject.setStepName("Create business partner");
-		ChuBoeCreateEntity.createBusinessPartner(valueObject);
-		valueObject.getBusinessPartner().setName(valueObject.getBusinessPartner().getName().substring(0, 19));
-		valueObject.getBusinessPartner().saveEx();
-		valueObject.setRandom();
-		commitEx();
-
-		valueObject.setStepName("Create product");
-		ChuBoeCreateEntity.createProduct(valueObject);
-		commitEx();
-
-		valueObject.setStepName("Create purchase order");
-		valueObject.setDocumentAction(DocumentEngine.ACTION_Complete);
-		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_PurchaseOrder, null, false, false, false);
-		valueObject.setQuantity(new BigDecimal(100));
-		ChuBoeCreateEntity.createOrder(valueObject);
-		commitEx();
-
-		valueObject.setStepName("Create visit");
-		ChuBoeCreateEntity.createVisit(valueObject);
-		commitEx();
-
-		valueObject.setStepName("Create sales order");
-		valueObject.setQuantity(new BigDecimal(50));
-		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_SalesOrder, MDocType_BH.DOCSUBTYPESO_OnCreditOrder, true, false,
-				false);
-		ChuBoeCreateEntity.createOrder(valueObject);
-		commitEx();
-
-		valueObject.setStepName("Create a payment");
-		valueObject.setDocBaseType(MDocType_BH.DOCBASETYPE_ARReceipt, null, true, false, false);
-		valueObject.setTenderType(MPayment_BH.TENDERTYPE_Cash);
-		valueObject.setPaymentAmount(new BigDecimal(20));
-		ChuBoeCreateEntity.createPayment(valueObject);
-		commitEx();
-		visitsUUs.add(valueObject.getVisit().getBH_Visit_UU());
-		valueObject.setStepName("Delete Visit");
-		ChuBoeDeleteEntity.deletevisits(valueObject, visitsUUs);
-		commitEx();
-
-		valueObject.setStepName("Generate the report");
-		valueObject.setProcessUuid(reportUuid);
-		valueObject.setProcessRecordId(0);
-		valueObject.setProcessTableId(0);
-		valueObject.setProcessInformationParameters(Arrays.asList(
-				new ProcessInfoParameter("Begin Date", TimestampUtils.today(), null, null, null),
-				new ProcessInfoParameter("End Date", TimestampUtils.endOfTomorrow(), null, null, null)
-		));
-		valueObject.setReportType("xlsx");
-		ChuBoeCreateEntity.runReport(valueObject);
-
-		FileInputStream file = new FileInputStream(valueObject.getReport());
-		try (Workbook workbook = new XSSFWorkbook(file)) {
-			Sheet sheet = workbook.getSheetAt(0);
-			Row headerRow = TableUtils.getHeaderRow(sheet, "Datetime");
-			int patientName = TableUtils.getColumnIndex(headerRow, "Patient Name");
-			Optional<Row> patientRow = StreamSupport.stream(sheet.spliterator(), false).filter(
-					row -> row.getCell(patientName) != null &&
-							row.getCell(patientName).getCellType().equals(CellType.STRING) &&
-							row.getCell(patientName).getStringCellValue()
-									.contains(valueObject.getBusinessPartner().getName())).findFirst();
-			assertTrue(patientRow.isEmpty(), "No patient is present");
+			assertEquals(50, patientRow.get().getCell(patientAmount).getNumericCellValue(), "Amount is correct");
 		}
 	}
 }
