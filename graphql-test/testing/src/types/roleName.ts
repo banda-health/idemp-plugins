982220508c1e4dd3d48773fd6c33ba06ecae6093
brianmituka <mitukab@outlook.com>
2025-09-25 17:30:10 +0300

    Clean up rolename file

--- graphql-test/testing/src/types/roleName.ts
+++ graphql-test/testing/src/types/roleName.ts
@@ -8,7 +8,6 @@ export enum RoleName {
 	ClinicianNurseAdvanced = 'Clinician/Nurse Advanced',
 	ClinicianNurseBasic = 'Clinician/Nurse Basic',
 	ClinicUser = 'Clinic User',
-	Implementer = 'Implementer',
 	InventoryPharmacyAdvanced = 'Inventory/Pharmacy Advanced',
 	InventoryPharmacyBasic = 'Inventory/Pharmacy Basic',
 	LabRadiologyAdvanced = 'Lab/Radiology Advanced',
