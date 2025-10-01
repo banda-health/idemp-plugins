84e1de16a3eeee11813c1af3336b90983977158b
Kevin <kwburnett@gmail.com>
2025-10-01 14:23:13 +0100

    feat: go-3373 > ensure dynamic joins used on WHERE API parameter, if need be

--- graphql-test/testing/src/tests/storage-on-hand.test.ts
+++ graphql-test/testing/src/tests/storage-on-hand.test.ts
@@ -1,4 +1,5 @@
 import {
+	Ad_Ref_ListGetDocument,
 	C_OrderLineSaveDocument,
 	C_OrderProcessDocument,
 	M_AttributeSetGetDocument,
@@ -7,7 +8,7 @@ import {
 	M_StorageOnHandGetDocument,
 } from '../__generated__/graphql';
 import { mutate, query } from '../api';
-import { documentAction, documentBaseType } from '../models';
+import { documentAction, documentBaseType, referenceUuid } from '../models';
 import {
 	createBusinessPartner,
 	createInOutFromOrder,
@@ -159,4 +160,209 @@ test('can sort by ASI guarantee date', async () => {
 	expect(sortedStorageOnHand).toHaveLength(2);
 	expect(sortedStorageOnHand[0].M_AttributeSetInstance.UU).toBe(lateExpiringAttributeSetInstance.UU);
 	expect(sortedStorageOnHand[1].M_AttributeSetInstance.UU).toBe(earlyExpiringAttributeSetInstance.UU);
+});
+
+test('dynamic joins are applied to the where clause', async () => {
+	const valueObject = globalThis.__VALUE_OBJECT__;
+	await valueObject.login();
+
+	// Get initial inventory counts for all and expired/soon to expire
+	const initialFullStorageOnHandCount = (
+		await query(valueObject)({
+			fetchPolicy: 'network-only',
+			query: M_StorageOnHandGetDocument,
+		})
+	).data.M_StorageOnHandGet.PagingInfo.TotalCount;
+	const initialExpiredOrSoonToExpireStorageOnHandCount = (
+		await query(valueObject)({
+			fetchPolicy: 'network-only',
+			query: M_StorageOnHandGetDocument,
+			variables: {
+				Where: `m_attributesetinstance.guaranteedate IS NOT NULL
+	AND (m_attributesetinstance.guaranteedate < NOW() OR (m_product.bh_soontoexpiredays IS NOT NULL AND
+	                                                      m_attributesetinstance.guaranteedate <=
+	                                                      NOW() + MAKE_INTERVAL ( days => m_product.bh_soontoexpiredays::integer )))`,
+			},
+		})
+	).data.M_StorageOnHandGet.PagingInfo.TotalCount;
+
+	valueObject.stepName = 'Create business partner';
+	await createBusinessPartner(valueObject);
+
+	valueObject.stepName = 'Create product';
+	const expiringAttributeSet = (
+		await query(valueObject)({
+			query: M_AttributeSetGetDocument,
+			variables: { Filter: JSON.stringify({ isguaranteedate: true }) },
+		})
+	).data.M_AttributeSetGet.Results[0];
+	await createProduct(valueObject);
+	const soonToExpireOptions = (
+		await query(valueObject)({
+			query: Ad_Ref_ListGetDocument,
+			variables: {
+				Filter: JSON.stringify({ ad_reference: { ad_reference_uu: referenceUuid.SOON_TO_EXPIRE_TYPES } }),
+			},
+		})
+	).data.AD_Ref_ListGet.Results;
+	valueObject.product = (
+		await mutate(valueObject)({
+			mutation: M_ProductSaveDocument,
+			variables: {
+				Entity: {
+					UU: valueObject.product!.UU,
+					BH_SoonToExpireDays: {
+						UU: soonToExpireOptions[0].UU,
+					},
+					M_AttributeSet: { UU: expiringAttributeSet.UU },
+				},
+			},
+		})
+	).data?.M_ProductSave;
+
+	valueObject.stepName = 'Create expired attribute set instance';
+	const lateExpiringAttributeSetInstance = (
+		await mutate(valueObject)({
+			mutation: M_AttributeSetInstanceSaveDocument,
+			variables: {
+				Entity: {
+					GuaranteeDate: formatApiDate(getDateOffset(new Date(), -5)),
+					M_AttributeSet: { UU: expiringAttributeSet.UU },
+				},
+			},
+		})
+	).data!.M_AttributeSetInstanceSave!;
+
+	valueObject.stepName = 'Create first purchase order';
+	valueObject.documentAction = undefined;
+	await valueObject.setDocumentBaseType(documentBaseType.PurchaseOrder, null, false, false, false);
+	await createOrder(valueObject);
+
+	valueObject.stepName = 'Add expiration and complete first PO';
+	await mutate(valueObject)({
+		mutation: C_OrderLineSaveDocument,
+		variables: {
+			Entity: {
+				UU: valueObject.orderLine!.UU,
+				M_AttributeSetInstance: { UU: lateExpiringAttributeSetInstance.UU },
+			},
+		},
+	});
+	(
+		await mutate(valueObject)({
+			mutation: C_OrderProcessDocument,
+			variables: { UU: valueObject.order!.UU, DocumentAction: documentAction.Complete },
+		})
+	).data?.C_OrderProcess;
+	await valueObject.refreshOrder();
+
+	valueObject.stepName = 'Create first material receipt';
+	valueObject.documentAction = documentAction.Complete;
+	await valueObject.setDocumentBaseType(documentBaseType.MaterialReceipt, null, false, false, false);
+	await createInOutFromOrder(valueObject);
+
+	valueObject.stepName = 'Create soon to expire attribute set instance';
+	const soonToExpireAttributeSetInstance = (
+		await mutate(valueObject)({
+			mutation: M_AttributeSetInstanceSaveDocument,
+			variables: {
+				Entity: {
+					GuaranteeDate: formatApiDate(getDateOffset(new Date(), parseInt(soonToExpireOptions[0].Value))),
+					M_AttributeSet: { UU: expiringAttributeSet.UU },
+				},
+			},
+		})
+	).data!.M_AttributeSetInstanceSave!;
+
+	valueObject.stepName = 'Create second purchase order';
+	valueObject.documentAction = undefined;
+	await valueObject.setDocumentBaseType(documentBaseType.PurchaseOrder, null, false, false, false);
+	await createOrder(valueObject);
+
+	valueObject.stepName = 'Add expiration and complete second PO';
+	await mutate(valueObject)({
+		mutation: C_OrderLineSaveDocument,
+		variables: {
+			Entity: {
+				UU: valueObject.orderLine!.UU,
+				M_AttributeSetInstance: { UU: soonToExpireAttributeSetInstance.UU },
+			},
+		},
+	});
+	(
+		await mutate(valueObject)({
+			mutation: C_OrderProcessDocument,
+			variables: { UU: valueObject.order!.UU, DocumentAction: documentAction.Complete },
+		})
+	).data?.C_OrderProcess;
+	await valueObject.refreshOrder();
+
+	valueObject.stepName = 'Create second material receipt';
+	valueObject.documentAction = documentAction.Complete;
+	await valueObject.setDocumentBaseType(documentBaseType.MaterialReceipt, null, false, false, false);
+	await createInOutFromOrder(valueObject);
+
+	valueObject.stepName = 'Create unexpired attribute set instance';
+	const unexpiredAttributeSetInstance = (
+		await mutate(valueObject)({
+			mutation: M_AttributeSetInstanceSaveDocument,
+			variables: {
+				Entity: {
+					GuaranteeDate: formatApiDate(getDateOffset(new Date(), 100)),
+					M_AttributeSet: { UU: expiringAttributeSet.UU },
+				},
+			},
+		})
+	).data!.M_AttributeSetInstanceSave!;
+
+	valueObject.stepName = 'Create third purchase order';
+	valueObject.documentAction = undefined;
+	await valueObject.setDocumentBaseType(documentBaseType.PurchaseOrder, null, false, false, false);
+	await createOrder(valueObject);
+
+	valueObject.stepName = 'Add expiration and complete third PO';
+	await mutate(valueObject)({
+		mutation: C_OrderLineSaveDocument,
+		variables: {
+			Entity: {
+				UU: valueObject.orderLine!.UU,
+				M_AttributeSetInstance: { UU: unexpiredAttributeSetInstance.UU },
+			},
+		},
+	});
+	(
+		await mutate(valueObject)({
+			mutation: C_OrderProcessDocument,
+			variables: { UU: valueObject.order!.UU, DocumentAction: documentAction.Complete },
+		})
+	).data?.C_OrderProcess;
+	await valueObject.refreshOrder();
+
+	valueObject.stepName = 'Create third material receipt';
+	valueObject.documentAction = documentAction.Complete;
+	await valueObject.setDocumentBaseType(documentBaseType.MaterialReceipt, null, false, false, false);
+	await createInOutFromOrder(valueObject);
+
+	expect(
+		(
+			await query(valueObject)({
+				fetchPolicy: 'network-only',
+				query: M_StorageOnHandGetDocument,
+			})
+		).data.M_StorageOnHandGet.PagingInfo.TotalCount,
+	).toBe(initialFullStorageOnHandCount + 3);
+	expect(
+		(
+			await query(valueObject)({
+				fetchPolicy: 'network-only',
+				query: M_StorageOnHandGetDocument,
+				variables: {
+					Where: `m_attributesetinstance.guaranteedate IS NOT NULL
+	AND (m_attributesetinstance.guaranteedate < NOW() OR (m_product.bh_soontoexpiredays IS NOT NULL AND
+	                                                      m_attributesetinstance.guaranteedate <=
+	                                                      NOW() + MAKE_INTERVAL ( days => m_product.bh_soontoexpiredays::integer )))`,
+				},
+			})
+		).data.M_StorageOnHandGet.PagingInfo.TotalCount,
+	).toBe(initialExpiredOrSoonToExpireStorageOnHandCount + 2);
 });
