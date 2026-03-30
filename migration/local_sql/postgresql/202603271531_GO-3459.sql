ad26b32cfe9cf774edc45bad5c0480da95403ad3
Andrew <andrewndungu@gmail.com>
2026-03-30 18:24:11 +0300

    create patient summary report

--- migration/local_sql/postgresql/202603271531_GO-3459.sql
+++ migration/local_sql/postgresql/202603271531_GO-3459.sql
@@ -11,9 +11,10 @@ VALUES
 			 MAX(ad_process_id)
 		 FROM
 			 ad_process
-	 ) + 1, 0, 0, 'Y', '2026-03-27 14:59:14.552000', 100, '2026-03-27 14:59:14.552000', 100, '10000000',
-	 'Patient Summary Report', 'Report for patient summary', NULL, '3', 'U', NULL, 'N', 'N', NULL, NULL, 0, 0, NULL, NULL,
-	 NULL, 'N', NULL, 'Y', NULL, NULL, 'N', 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c', NULL, NULL, 'P', NULL);
+	 ) + 1, 0, 0, 'Y', '2026-03-27 14:59:14.552000', 100, '2026-03-27 14:59:14.552000', 100, 'BH Patient Summary Report',
+	 'Patient Summary Report', 'Report for patient summary', NULL, '3', 'U', NULL, 'Y', 'N', NULL, NULL, 0, 0, NULL, NULL,
+	 NULL, 'N', NULL, 'Y', 'Patients Report/PatientSummary.jasper', NULL, 'N', 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c', NULL,
+	 NULL, 'P', NULL);
 
 INSERT INTO
 	ad_process_para (ad_process_para_id, ad_client_id, ad_org_id, isactive, created, createdby, updated,
@@ -35,7 +36,40 @@ VALUES
 	 ), 10, 10, NULL, NULL, 'BH_Visit_UU', 'Y', 36, 'N', 'N', NULL, NULL, NULL, NULL, NULL, 1000276, 'U', NULL,
 	 NULL, 'd147cad4-2aef-4abd-a929-d6bea83c5495', 'N', NULL, NULL, NULL, 'N', NULL, NULL, 'D', 'N');
 
----- TODO add process access
+-- Add process access for same roles as Diagnosis Report
+INSERT INTO
+	ad_process_access (ad_process_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated,
+	                   updatedby, isreadwrite, ad_process_access_uu)
+VALUES
+	((
+		 SELECT ad_process_id FROM ad_process WHERE ad_process_uu = 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c'
+	 ), (
+		 SELECT ad_role_id FROM ad_role WHERE ad_role_uu = '461b31c5-cae2-449d-8a0c-7385b12f4685'
+	 ), 0, 0, 'Y', NOW(), 100, NOW(), 100, 'Y', 'f10eaecf-6417-4b1c-b238-30be74fcd640')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+	ad_process_access (ad_process_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated,
+	                   updatedby, isreadwrite, ad_process_access_uu)
+VALUES
+	((
+		 SELECT ad_process_id FROM ad_process WHERE ad_process_uu = 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c'
+	 ), (
+		 SELECT ad_role_id FROM ad_role WHERE ad_role_uu = '98617c31-55ff-48f9-bd44-253ef323d960'
+	 ), 0, 0, 'Y', NOW(), 100, NOW(), 100, 'Y', '5e2e6349-361f-41bc-a95e-4badf708e72e')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+	ad_process_access (ad_process_id, ad_role_id, ad_client_id, ad_org_id, isactive, created, createdby, updated,
+	                   updatedby, isreadwrite, ad_process_access_uu)
+VALUES
+	((
+		 SELECT ad_process_id FROM ad_process WHERE ad_process_uu = 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c'
+	 ), (
+		 SELECT ad_role_id FROM ad_role WHERE ad_role_uu = 'e1a9a87d-dc61-4d9e-a6c9-f91d5f42e33e'
+	 ), 0, 0, 'Y', NOW(), 100, NOW(), 100, 'Y', '2fea89ec-fecf-4fef-a9fd-cb22f0665994')
+ON CONFLICT DO NOTHING;
+
 INSERT INTO
 	ad_menu (ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby,
 	         description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id,
@@ -48,7 +82,9 @@ VALUES
 		 FROM
 			 ad_menu
 	 ) + 1, 0, 0, 'Y', '2026-03-27 15:06:44.325000', 100, '2026-03-27 15:06:44.325000', 'Patients Summary Report', 100,
-	 'Patient Summary', 'N', 'Y', 'N', 'P', NULL, NULL, NULL, 1000079, NULL, NULL, 'U', 'Y',
+	 'Patient Summary', 'N', 'Y', 'N', 'R', NULL, NULL, NULL, (
+		 SELECT ad_process_id FROM ad_process WHERE ad_process_uu = 'c0e1adfc-f743-49e4-9346-4da29f1e9f6c'
+	 ), NULL, NULL, 'U', 'Y',
 	 '84e85424-8401-4341-a0d8-b1331443f3e7', NULL, NULL, NULL);
 
 
